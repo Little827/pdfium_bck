37ed7a5aacbd5dacbe0773ba41305972b77ee408
Tom Sepez <tsepez@chromium.org>
2018-02-07 15:46:51 -0800

    Make CFXJS_Engine::SetObjectPrivate() static.
    Change-Id: Ia1a1a1fcc085d8657939e6f8c8d34fc511afddfe

--- fxjs/fxjs_v8.h
+++ fxjs/fxjs_v8.h
@@ -14,6 +14,7 @@
 #ifndef FXJS_FXJS_V8_H_
 #define FXJS_FXJS_V8_H_
 
+#include <functional>
 #include <map>
 #include <memory>
 #include <vector>
@@ -131,9 +132,9 @@ class CFXJS_Engine : public CJS_V8 {
   explicit CFXJS_Engine(v8::Isolate* pIsolate);
   ~CFXJS_Engine() override;
 
-  using Constructor = void (*)(CFXJS_Engine* pEngine,
-                               v8::Local<v8::Object> obj);
-  using Destructor = void (*)(CFXJS_Engine* pEngine, v8::Local<v8::Object> obj);
+  using Constructor =
+      std::function<void(CFXJS_Engine* pEngine, v8::Local<v8::Object> obj)>;
+  using Destructor = std::function<void(v8::Local<v8::Object> obj)>;
 
   static CFXJS_Engine* EngineFromIsolateCurrentContext(v8::Isolate* pIsolate);
   static CFXJS_Engine* EngineFromContext(v8::Local<v8::Context> pContext);
@@ -180,9 +181,9 @@ class CFXJS_Engine : public CJS_V8 {
                                            bool bStatic = false);
 
   // Native object binding.
-  void SetObjectPrivate(v8::Local<v8::Object> pObj,
-                        std::unique_ptr<CJS_Object> p);
   CJS_Object* GetObjectPrivate(v8::Local<v8::Object> pObj);
+  static void SetObjectPrivate(v8::Local<v8::Object> pObj,
+                               std::unique_ptr<CJS_Object> p);
   static void FreeObjectPrivate(v8::Local<v8::Object> pObj);
 
   void Error(const WideString& message);
