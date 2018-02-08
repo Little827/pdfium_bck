a9867199c3f74042fe8322c4f8d0ce48426be032
Tom Sepez <tsepez@chromium.org>
2018-02-08 11:59:25 -0800

    Move cjs_v8.cpp to cfx_v8.cpp
    The layering should be CJS => CFXJS => CFX_V8 with the CJS name
    being higher up.
    
    Stick an UnownedPtr into CFX_V8 while we're at it.
    
    Change-Id: Ic130f248906e9c4df641dd508389b0555786b999

--- fxjs/cfx_v8.h
+++ fxjs/cfx_v8.h
@@ -4,24 +4,23 @@
 
 // Original code copyright 2014 Foxit Software Inc. http://www.foxitsoftware.com
 
-#ifndef FXJS_CJS_V8_H_
-#define FXJS_CJS_V8_H_
+#ifndef FXJS_CFX_V8_H_
+#define FXJS_CFX_V8_H_
 
 #include <map>
 #include <vector>
 
-#include "core/fxcrt/bytestring.h"
 #include "core/fxcrt/fx_string.h"
-#include "core/fxcrt/widestring.h"
+#include "core/fxcrt/unowned_ptr.h"
 #include "v8/include/v8-util.h"
 #include "v8/include/v8.h"
 
-class CJS_V8 {
+class CFX_V8 {
  public:
-  explicit CJS_V8(v8::Isolate* pIsolate);
-  virtual ~CJS_V8();
+  explicit CFX_V8(v8::Isolate* pIsolate);
+  virtual ~CFX_V8();
 
-  v8::Isolate* GetIsolate() const { return m_isolate; }
+  v8::Isolate* GetIsolate() const { return m_isolate.Get(); }
 
   v8::Local<v8::Value> NewNull();
   v8::Local<v8::Value> NewUndefined();
@@ -63,7 +62,7 @@ class CJS_V8 {
   void SetIsolate(v8::Isolate* pIsolate) { m_isolate = pIsolate; }
 
  private:
-  v8::Isolate* m_isolate;
+  UnownedPtr<v8::Isolate> m_isolate;
 };
 
-#endif  // FXJS_CJS_V8_H_
+#endif  // FXJS_CFX_V8_H_
