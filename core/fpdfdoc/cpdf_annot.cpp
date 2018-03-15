2b7a718c3aedbb89b14a6c7bf04e52a100586cca
Ralf Sippl <ralf.sippl@gmail.com>
2018-03-15 10:16:05 +0100

    Calculate AP for multi-line markup annotations correctly
    Currently, when constructing AP for multi-line markup annotations, we
    only take into account the first set of quadpoints, resulting in only
    the first line of the annotation being displayed if the annotation spans
    multiple lines.
    
    This CL, initially written by Jane Liu <janeliulwq@google.com>
    (https://pdfium-review.googlesource.com/12010) takes into account all
    the quadpoints, so multi-line annotations can be displayed correctly.
    
    BUG=pdfium:876
    
    Change-Id: I8be10ee38e01eb6525ddef556df1b727189455c7

--- core/fpdfdoc/cpdf_annot.cpp
+++ core/fpdfdoc/cpdf_annot.cpp
@@ -6,6 +6,7 @@
 
 #include "core/fpdfdoc/cpdf_annot.h"
 
+#include <algorithm>
 #include <utility>
 
 #include "core/fpdfapi/page/cpdf_form.h"
@@ -158,7 +159,7 @@ CFX_FloatRect CPDF_Annot::RectForDrawing() const {
   bool bShouldUseQuadPointsCoords =
       m_bIsTextMarkupAnnotation && m_bHasGeneratedAP;
   if (bShouldUseQuadPointsCoords)
-    return RectFromQuadPoints(m_pAnnotDict.Get());
+    return BoundingRectFromQuadPoints(m_pAnnotDict.Get());
 
   return m_pAnnotDict->GetRectFor("Rect");
 }
@@ -205,7 +206,8 @@ CPDF_Form* CPDF_Annot::GetAPForm(const CPDF_Page* pPage, AppearanceMode mode) {
 }
 
 // Static.
-CFX_FloatRect CPDF_Annot::RectFromQuadPoints(CPDF_Dictionary* pAnnotDict) {
+CFX_FloatRect CPDF_Annot::BoundingRectFromQuadPoints(
+    CPDF_Dictionary* pAnnotDict) {
   CPDF_Array* pArray = pAnnotDict->GetArrayFor("QuadPoints");
   if (!pArray)
     return CFX_FloatRect();
@@ -220,8 +222,29 @@ CFX_FloatRect CPDF_Annot::RectFromQuadPoints(CPDF_Dictionary* pAnnotDict) {
   // On the other hand, /Rect is define as 2 pairs [pair0, pair1] where:
   // pair0 = bottom_left
   // pair1 = top_right.
-  return CFX_FloatRect(pArray->GetNumberAt(4), pArray->GetNumberAt(5),
-                       pArray->GetNumberAt(2), pArray->GetNumberAt(3));
+  float left = pArray->GetNumberAt(4);
+  float bottom = pArray->GetNumberAt(5);
+  float right = pArray->GetNumberAt(2);
+  float top = pArray->GetNumberAt(3);
+  for (size_t i = 1; i < QuadPointCount(pArray); ++i) {
+    left = std::min(pArray->GetNumberAt(4 + i * 8), left);
+    bottom = std::min(pArray->GetNumberAt(5 + i * 8), bottom);
+    right = std::max(pArray->GetNumberAt(2 + i * 8), right);
+    top = std::max(pArray->GetNumberAt(3 + i * 8), top);
+  }
+  return CFX_FloatRect(left, bottom, right, top);
+}
+
+// Static.
+CFX_FloatRect CPDF_Annot::RectFromQuadPoints(CPDF_Dictionary* pAnnotDict,
+                                             size_t nIndex) {
+  CPDF_Array* pArray = pAnnotDict->GetArrayFor("QuadPoints");
+  if (!pArray)
+    return CFX_FloatRect();
+
+  return CFX_FloatRect(
+      pArray->GetNumberAt(4 + nIndex * 8), pArray->GetNumberAt(5 + nIndex * 8),
+      pArray->GetNumberAt(2 + nIndex * 8), pArray->GetNumberAt(3 + nIndex * 8));
 }
 
 // Static.
@@ -346,6 +369,11 @@ ByteString CPDF_Annot::AnnotSubtypeToString(CPDF_Annot::Subtype nSubtype) {
   if (nSubtype == CPDF_Annot::Subtype::XFAWIDGET)
     return "XFAWidget";
   return "";
+}
+
+// Static.
+size_t CPDF_Annot::QuadPointCount(const CPDF_Array* pArray) {
+  return pArray->GetCount() / 8;
 }
 
 bool CPDF_Annot::DrawAppearance(CPDF_Page* pPage,
