e392e66d53e8405573e63fa092bc14700b5557ad
Tom Sepez <tsepez@chromium.org>
2018-04-25 12:08:57 -0700

    Use incomplete types for FPDF_{DOCUMENT,PAGE} in fpdfview.h, try #2.
    Stricter checking avoids casting a void* to the wrong type.
    
    Change-Id: Iff1349a9c5acb3fdd18782bf33e95e21e8d5458e

--- core/fpdfapi/parser/cpdf_document.h
+++ core/fpdfapi/parser/cpdf_document.h
@@ -39,7 +39,16 @@ class JBig2_DocumentContext;
 
 #define FPDF_PAGE_MAX_NUM 0xFFFFF
 
-class CPDF_Document : public CPDF_IndirectObjectHolder {
+#ifndef PDF_ENABLE_XFA
+// CPDF_Document is an FPDF API public object when not XFA.
+struct fpdf_document_t__ {};
+#endif
+
+class CPDF_Document :
+#ifndef PDF_ENABLE_XFA
+    public fpdf_document_t__,
+#endif
+    public CPDF_IndirectObjectHolder {
  public:
   explicit CPDF_Document(std::unique_ptr<CPDF_Parser> pParser);
   ~CPDF_Document() override;
