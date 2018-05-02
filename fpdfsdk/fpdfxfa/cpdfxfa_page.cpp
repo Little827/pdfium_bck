c5be370af4bd098ebb24c6aa2d0313673e7ef8b7
Tom Sepez <tsepez@chromium.org>
2018-05-02 13:41:42 -0700

    Ensure that XFA Pages always have a corresponding PDF page
    The PDF page may be blank, un-numbered, or untracked by CPDF, but
    this provides a place for all XFA pages to "extend" from down the
    road.
    
    Change-Id: If1003be0f261154e61e9793ccba7e1f43cd73104

--- fpdfsdk/fpdfxfa/cpdfxfa_page.cpp
+++ fpdfsdk/fpdfxfa/cpdfxfa_page.cpp
@@ -18,32 +18,24 @@
 #include "xfa/fxfa/cxfa_ffpageview.h"
 
 CPDFXFA_Page::CPDFXFA_Page(CPDFXFA_Context* pContext, int page_index)
-    : m_pXFAPageView(nullptr), m_pContext(pContext), m_iPageIndex(page_index) {}
+    : m_pXFAPageView(nullptr), m_pContext(pContext), m_iPageIndex(page_index) {
+  CPDF_Dictionary* pDict = nullptr;
+  CPDF_Document* pPDFDoc = m_pContext->GetPDFDoc();
+  if (pPDFDoc && m_pContext->GetFormType() != FormType::kXFAFull)
+    pDict = pPDFDoc->GetPage(m_iPageIndex);
+  m_pPDFPage = pdfium::MakeUnique<CPDF_Page>(pPDFDoc, pDict, true);
+  if (pDict)
+    m_pPDFPage->ParseContent();
+}
 
 CPDFXFA_Page::~CPDFXFA_Page() {}
 
-bool CPDFXFA_Page::LoadPDFPage() {
-  if (!m_pContext)
+bool CPDFXFA_Page::LoadPage() {
+  if (!m_pContext || m_iPageIndex < 0)
     return false;
 
-  CPDF_Document* pPDFDoc = m_pContext->GetPDFDoc();
-  if (!pPDFDoc)
-    return false;
-
-  CPDF_Dictionary* pDict = pPDFDoc->GetPage(m_iPageIndex);
-  if (!pDict)
-    return false;
-
-  if (!m_pPDFPage || m_pPDFPage->m_pFormDict != pDict) {
-    m_pPDFPage = pdfium::MakeUnique<CPDF_Page>(pPDFDoc, pDict, true);
-    m_pPDFPage->ParseContent();
-  }
-  return true;
-}
-
-bool CPDFXFA_Page::LoadXFAPageView() {
-  if (!m_pContext)
-    return false;
+  if (m_pContext->GetFormType() != FormType::kXFAFull)
+    return true;
 
   CXFA_FFDoc* pXFADoc = m_pContext->GetXFADoc();
   if (!pXFADoc)
@@ -61,30 +53,6 @@ bool CPDFXFA_Page::LoadXFAPageView() {
   return true;
 }
 
-bool CPDFXFA_Page::LoadPage() {
-  if (!m_pContext || m_iPageIndex < 0)
-    return false;
-
-  switch (m_pContext->GetFormType()) {
-    case FormType::kNone:
-    case FormType::kAcroForm:
-    case FormType::kXFAForeground:
-      return LoadPDFPage();
-    case FormType::kXFAFull:
-      return LoadXFAPageView();
-  }
-  return false;
-}
-
-bool CPDFXFA_Page::LoadPDFPage(CPDF_Dictionary* pageDict) {
-  if (!m_pContext || m_iPageIndex < 0 || !pageDict)
-    return false;
-
-  m_pPDFPage =
-      pdfium::MakeUnique<CPDF_Page>(m_pContext->GetPDFDoc(), pageDict, true);
-  m_pPDFPage->ParseContent();
-  return true;
-}
 
 float CPDFXFA_Page::GetPageWidth() const {
   if (!m_pPDFPage && !m_pXFAPageView)
