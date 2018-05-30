90891fcb3b1a58f9af4845cc3feea1dc153633ce
Tom Sepez <tsepez@chromium.org>
2018-05-30 15:04:06 -0700

    Creating XFA context shouldnt be a side-effect of converting to FDPF_ type.
    Change-Id: I42d5a6f33bc32e7fb768bed96aa6d239d133467e

--- fpdfsdk/fpdf_view.cpp
+++ fpdfsdk/fpdf_view.cpp
@@ -151,6 +151,12 @@ FPDF_DOCUMENT LoadDocumentImpl(
     return nullptr;
   }
   CheckUnSupportError(pDocument.get(), error);
+
+#ifdef PDF_ENABLE_XFA
+  pDocument->SetExtension(
+      new CPDFXFA_Context(pdfium::WrapUnique(pDocument.get())));
+#endif  // PDF_ENABLE_XFA
+
   return FPDFDocumentFromCPDFDocument(pDocument.release());
 }
 
@@ -728,7 +734,9 @@ FPDF_EXPORT void FPDF_CALLCONV FPDF_ClosePage(FPDF_PAGE page) {
   RetainPtr<IPDF_Page> pPage;
   pPage.Unleak(IPDFPageFromFPDFPage(page));
 
-#ifndef PDF_ENABLE_XFA
+  if (pPage->AsXFAPage())
+    return;
+
   CPDFSDK_PageView* pPageView =
       static_cast<CPDFSDK_PageView*>(pPage->AsPDFPage()->GetView());
   if (!pPageView || pPageView->IsBeingDestroyed())
@@ -743,19 +751,18 @@ FPDF_EXPORT void FPDF_CALLCONV FPDF_ClosePage(FPDF_PAGE page) {
   // first because it will attempt to reset the View on the |pPage| during
   // destruction.
   pPageView->GetFormFillEnv()->RemovePageView(pPage.Get());
-#endif  // PDF_ENABLE_XFA
 }
 
 FPDF_EXPORT void FPDF_CALLCONV FPDF_CloseDocument(FPDF_DOCUMENT document) {
   auto* pDoc = CPDFDocumentFromFPDFDocument(document);
+  if (!pDoc)
+    return;
 
-#if PDF_ENABLE_XFA
   // Deleting the extension will delete the document
-  if (pDoc && pDoc->GetExtension()) {
+  if (pDoc->GetExtension()) {
     delete pDoc->GetExtension();
     return;
   }
-#endif  // PDF_ENABLE_XFA
 
   delete pDoc;
 }
