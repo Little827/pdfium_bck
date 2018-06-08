31577a66152020932bf20fcc9d7f5d98e0c2cf2b
Henrique Nakashima <hnakashima@chromium.org>
2018-06-08 17:48:45 -0400

    Rewrite stream generation.
    Bug: pdfium:1051
    Change-Id: I837b5a7cd9542b7777e7c7ae7ac9cc75f69f30b5

--- fpdfsdk/fpdf_edit_embeddertest.cpp
+++ fpdfsdk/fpdf_edit_embeddertest.cpp
@@ -428,8 +428,7 @@ TEST_F(FPDFEditEmbeddertest, AddPaths) {
   VerifySavedDocument(612, 792, kLastMD5);
 }
 
-// Fails due to pdfium:1051.
-TEST_F(FPDFEditEmbeddertest, DISABLED_SetText) {
+TEST_F(FPDFEditEmbeddertest, SetText) {
   // Load document with some text.
   EXPECT_TRUE(OpenDocument("hello_world.pdf"));
   FPDF_PAGE page = LoadPage(0);
@@ -456,11 +455,14 @@ TEST_F(FPDFEditEmbeddertest, DISABLED_SetText) {
   {
     ScopedFPDFBitmap page_bitmap = RenderPageWithFlags(page, nullptr, 0);
     CompareBitmap(page_bitmap.get(), 200, 200, kChangedMD5);
+    WriteBitmapToPng(page_bitmap.get(), "Changed1.png");
   }
 
   // Now save the result.
   EXPECT_TRUE(FPDFPage_GenerateContent(page));
+  OpenPDFFileForWrite("SetText.pdf");
   EXPECT_TRUE(FPDF_SaveAsCopy(document(), this, 0));
+  ClosePDFFileForWrite();
 
   UnloadPage(page);
 
@@ -471,6 +473,7 @@ TEST_F(FPDFEditEmbeddertest, DISABLED_SetText) {
   {
     ScopedFPDFBitmap page_bitmap = RenderPageWithFlags(saved_page, nullptr, 0);
     CompareBitmap(page_bitmap.get(), 200, 200, kChangedMD5);
+    WriteBitmapToPng(page_bitmap.get(), "Changed2.png");
   }
 
   CloseSavedPage(saved_page);
@@ -626,8 +629,7 @@ TEST_F(FPDFEditEmbeddertest, RemoveMarkedObjectsPrime) {
   UnloadPage(page);
 }
 
-// Fails due to pdfium:1051.
-TEST_F(FPDFEditEmbeddertest, DISABLED_RemoveExistingPageObject) {
+TEST_F(FPDFEditEmbeddertest, RemoveExistingPageObject) {
   // Load document with some text.
   EXPECT_TRUE(OpenDocument("hello_world.pdf"));
   FPDF_PAGE page = LoadPage(0);
@@ -652,6 +654,68 @@ TEST_F(FPDFEditEmbeddertest, DISABLED_RemoveExistingPageObject) {
   OpenSavedDocument();
   FPDF_PAGE saved_page = LoadSavedPage(0);
   EXPECT_EQ(1, FPDFPage_CountObjects(saved_page));
+  CloseSavedPage(saved_page);
+  CloseSavedDocument();
+}
+
+TEST_F(FPDFEditEmbeddertest, RemoveExistingPageObjectSplitStreams) {
+  // Load document with some text.
+  EXPECT_TRUE(OpenDocument("hello_world_split_streams.pdf"));
+  FPDF_PAGE page = LoadPage(0);
+  ASSERT_TRUE(page);
+
+  // Get the "Hello, world!" text object and remove it.
+  ASSERT_EQ(3, FPDFPage_CountObjects(page));
+  FPDF_PAGEOBJECT page_object = FPDFPage_GetObject(page, 0);
+  ASSERT_TRUE(page_object);
+  EXPECT_TRUE(FPDFPage_RemoveObject(page, page_object));
+
+  // Verify the "Hello, world!" text is gone.
+  ASSERT_EQ(2, FPDFPage_CountObjects(page));
+
+  // Save the file
+  EXPECT_TRUE(FPDFPage_GenerateContent(page));
+  // OpenPDFFileForWrite("RemoveExistingPageObjectSplitStreams.pdf");
+  EXPECT_TRUE(FPDF_SaveAsCopy(document(), this, 0));
+  // ClosePDFFileForWrite();
+  UnloadPage(page);
+  FPDFPageObj_Destroy(page_object);
+
+  // Re-open the file and check the page object count is still 1.
+  OpenSavedDocument();
+  FPDF_PAGE saved_page = LoadSavedPage(0);
+  EXPECT_EQ(2, FPDFPage_CountObjects(saved_page));
+  CloseSavedPage(saved_page);
+  CloseSavedDocument();
+}
+
+TEST_F(FPDFEditEmbeddertest, RemoveExistingPageObjectSplitStreams2) {
+  // Load document with some text.
+  EXPECT_TRUE(OpenDocument("hello_world_split_streams.pdf"));
+  FPDF_PAGE page = LoadPage(0);
+  ASSERT_TRUE(page);
+
+  // Get the "Hello, world!" text object and remove it.
+  ASSERT_EQ(3, FPDFPage_CountObjects(page));
+  FPDF_PAGEOBJECT page_object = FPDFPage_GetObject(page, 2);
+  ASSERT_TRUE(page_object);
+  EXPECT_TRUE(FPDFPage_RemoveObject(page, page_object));
+
+  // Verify the "Hello, world!" text is gone.
+  ASSERT_EQ(2, FPDFPage_CountObjects(page));
+
+  // Save the file
+  EXPECT_TRUE(FPDFPage_GenerateContent(page));
+  // OpenPDFFileForWrite("RemoveExistingPageObjectSplitStreams.pdf");
+  EXPECT_TRUE(FPDF_SaveAsCopy(document(), this, 0));
+  // ClosePDFFileForWrite();
+  UnloadPage(page);
+  FPDFPageObj_Destroy(page_object);
+
+  // Re-open the file and check the page object count is still 1.
+  OpenSavedDocument();
+  FPDF_PAGE saved_page = LoadSavedPage(0);
+  EXPECT_EQ(2, FPDFPage_CountObjects(saved_page));
   CloseSavedPage(saved_page);
   CloseSavedDocument();
 }
