ed04f24c17d64f8f577cc0a34da5585c82a8589c
Henrique Nakashima <hnakashima@chromium.org>
2018-06-11 13:23:05 -0400

    Rewrite content stream regeneration.
    Loop through the dirty page objects and streams and regenerate all
    streams that are dirty.
    
    Bug: pdfium:1051
    Change-Id: I837b5a7cd9542b7777e7c7ae7ac9cc75f69f30b5

--- core/fpdfapi/page/cpdf_pageobject.h
+++ core/fpdfapi/page/cpdf_pageobject.h
@@ -28,7 +28,7 @@ class CPDF_PageObject : public CPDF_GraphicStates {
     FORM,
   };
 
-  CPDF_PageObject();
+  explicit CPDF_PageObject(int32_t content_stream);
   ~CPDF_PageObject() override;
 
   virtual Type GetType() const = 0;
@@ -59,6 +59,13 @@ class CPDF_PageObject : public CPDF_GraphicStates {
   }
   FX_RECT GetBBox(const CFX_Matrix* pMatrix) const;
 
+  // Get what content stream the object was parsed from in its page. This number
+  // is the index of the content stream in the "Contents" array, or 0 if there
+  // is a single content stream. If the object is newly created, -1 is returned.
+  // If the object is spread among more than one content stream, this is the
+  // index of the last one.
+  int32_t GetContentStream() const { return m_ContentStream; }
+
   float m_Left;
   float m_Right;
   float m_Top;
@@ -73,6 +80,7 @@ class CPDF_PageObject : public CPDF_GraphicStates {
   void operator=(const CPDF_PageObject& src) = delete;
 
   bool m_bDirty;
+  int32_t m_ContentStream;
 };
 
 #endif  // CORE_FPDFAPI_PAGE_CPDF_PAGEOBJECT_H_
