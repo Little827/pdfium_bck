93e3f92a10eaba842c856a6a7e9f757023d804ba
Henrique Nakashima <hnakashima@chromium.org>
2018-06-13 13:58:42 -0400

    Rewrite content stream regeneration.
    Loop through the dirty page objects and streams and regenerate all
    streams that are dirty.
    
    Bug: pdfium:1051
    Change-Id: I837b5a7cd9542b7777e7c7ae7ac9cc75f69f30b5

--- core/fpdfapi/edit/cpdf_pagecontentgenerator.h
+++ core/fpdfapi/edit/cpdf_pagecontentgenerator.h
@@ -49,8 +49,8 @@ class CPDF_PageContentGenerator {
   std::map<int32_t, std::unique_ptr<std::ostringstream>>
   GenerateModifiedStreams();
 
-  // Generate new stream data with all dirty page objects.
-  bool GenerateStreamWithNewObjects(std::ostringstream* buf);
+  // Generate new stream data.
+  bool GenerateStream(int32_t dirty_stream, std::ostringstream* buf);
 
   // Add buffer as a stream in page's 'Contents'
   void UpdateContentStreams(
