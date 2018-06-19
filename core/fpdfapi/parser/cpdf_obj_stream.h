2e5630d6e883c9340729842b744ed3c848342614
Artem Strygin <art-snake@yandex-team.ru>
2018-06-19 23:34:34 +0300

    Implement CPDF_ObjStream.
    It is allow do not store raw objects streams within CPDF_Document
    for reduce memory usage.
    
    Change-Id: I4377bd5119d87314e76f14255171618cf6ee533d

---
+++ core/fpdfapi/parser/cpdf_obj_stream.h
@@ -0,0 +1,56 @@
+// Copyright 2018 PDFium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CORE_FPDFAPI_PARSER_CPDF_OBJ_STREAM_H_
+#define CORE_FPDFAPI_PARSER_CPDF_OBJ_STREAM_H_
+
+#include <map>
+#include <memory>
+#include <vector>
+
+#include "core/fpdfapi/parser/cpdf_object.h"
+#include "core/fxcrt/retain_ptr.h"
+
+class IFX_SeekableReadStream;
+class CPDF_Stream;
+class CPDF_StreamAcc;
+class CPDF_IndirectObjectHolder;
+
+// Implementation of logic of PDF "Object Streams".
+// See "PDF 32000-1:2008" Spec. section 7.5.7.
+class CPDF_ObjStream {
+ public:
+  static bool IsObjectsStreamObject(const CPDF_Object* object);
+
+  static std::unique_ptr<CPDF_ObjStream> Create(
+      std::unique_ptr<CPDF_Stream> stream);
+
+  virtual ~CPDF_ObjStream();
+
+  uint32_t obj_num() const { return obj_num_; }
+  uint32_t extends_obj_num() const { return extends_obj_num_; }
+
+  bool HasObject(uint32_t obj_number) const;
+  std::unique_ptr<CPDF_Object> ParseObject(CPDF_IndirectObjectHolder* pObjList,
+                                           uint32_t obj_number) const;
+
+  std::vector<std::unique_ptr<CPDF_Object>> ParseAllObjects(
+      CPDF_IndirectObjectHolder* pObjList) const;
+
+ protected:
+  explicit CPDF_ObjStream(std::unique_ptr<CPDF_Stream> obj_stream);
+
+  void Init(std::unique_ptr<CPDF_Stream> stream);
+  std::unique_ptr<CPDF_Object> ParseObjectAtOffset(
+      CPDF_IndirectObjectHolder* pObjList,
+      uint32_t object_offset) const;
+
+  uint32_t obj_num_ = CPDF_Object::kInvalidObjNum;
+  uint32_t extends_obj_num_ = CPDF_Object::kInvalidObjNum;
+  RetainPtr<IFX_SeekableReadStream> data_stream_;
+  int first_object_offset_ = 0;
+  std::map<uint32_t, uint32_t> objects_offsets_;
+};
+
+#endif  // CORE_FPDFAPI_PARSER_CPDF_OBJ_STREAM_H_
