85fec7b7ddd67e3760fd1cd7a75a1b0f6dbde89d
Lei Zhang <thestig@chromium.org>
2018-06-29 01:46:00 -0700

    Add FPDFFlattenEmbeddertestWithParams.
    Use gtests's parameterized test system to instantiate a set of tests for
    importing and flattening PDFs. Which is similar to how Chromium flatten
    PDFs for printing.
    
    Change-Id: Ibe86a1059fd03e37a7f8ca86acbb874d3ccefd1a

--- fpdfsdk/fpdf_flatten_embeddertest.cpp
+++ fpdfsdk/fpdf_flatten_embeddertest.cpp
@@ -2,7 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <string>
+
 #include "public/fpdf_flatten.h"
+#include "public/fpdf_ppo.h"
 #include "public/fpdfview.h"
 #include "testing/embedder_test.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -11,6 +14,28 @@
 namespace {
 
 class FPDFFlattenEmbeddertest : public EmbedderTest {};
+
+struct FlattenTestData {
+  const char* name;
+  const char* md5;
+  int width;
+  int height;
+} const kOnePageTests[] = {
+    {"combobox_form", "d60d2553718030205f564297c126c83c", 300, 600},
+    {"embedded_images", "a63036771e33c05c711abe8a758d6659", 612, 792},
+    {"rectangles", "0a90de37f52127619c3dfb642b5fa2fe", 200, 300},
+};
+
+struct GetTestNameForFlattenTestData {
+  std::string operator()(
+      const testing::TestParamInfo<FlattenTestData>& info) const {
+    return info.param.name;
+  }
+};
+
+class FPDFFlattenEmbeddertestWithParam
+    : public FPDFFlattenEmbeddertest,
+      public testing::WithParamInterface<FlattenTestData> {};
 
 }  // namespace
 
@@ -37,3 +62,26 @@ TEST_F(FPDFFlattenEmbeddertest, FlatPrint) {
   EXPECT_EQ(FLATTEN_SUCCESS, FPDFPage_Flatten(page, FLAT_PRINT));
   UnloadPage(page);
 }
+
+TEST_P(FPDFFlattenEmbeddertestWithParam, ImportAndFlatPrint) {
+  std::string name = GetParam().name;
+  EXPECT_TRUE(OpenDocument(name + ".pdf"));
+  int page_count = GetPageCount();
+  ASSERT_EQ(1, page_count);
+
+  {
+    ScopedFPDFDocument output_doc(FPDF_CreateNewDocument());
+    ASSERT_TRUE(FPDF_ImportPages(output_doc.get(), document(), "1", 0));
+    EXPECT_EQ(1, FPDF_GetPageCount(output_doc.get()));
+    ScopedFPDFPage page(FPDF_LoadPage(output_doc.get(), 0));
+    EXPECT_NE(FLATTEN_FAIL, FPDFPage_Flatten(page.get(), FLAT_PRINT));
+    EXPECT_TRUE(FPDF_SaveAsCopy(output_doc.get(), this, 0));
+  }
+
+  VerifySavedDocument(GetParam().width, GetParam().height, GetParam().md5);
+}
+
+INSTANTIATE_TEST_CASE_P(,
+                        FPDFFlattenEmbeddertestWithParam,
+                        testing::ValuesIn(kOnePageTests),
+                        GetTestNameForFlattenTestData());
