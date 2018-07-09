1cc6ac7a39324e3e24255dadce42e0ad3dd668fe
Henrique Nakashima <hnakashima@chromium.org>
2018-07-09 13:23:10 -0400

    [debug] mark_api5
    Change-Id: I97db364673ef0eccb60e8726b9b590e9168dd8b8

--- core/fpdfapi/page/cpdf_contentmark.cpp
+++ core/fpdfapi/page/cpdf_contentmark.cpp
@@ -29,7 +29,7 @@ size_t CPDF_ContentMark::CountItems() const {
   return m_pMarkData->CountItems();
 }
 
-const CPDF_ContentMarkItem& CPDF_ContentMark::GetItem(size_t i) const {
+CPDF_ContentMarkItem* CPDF_ContentMark::GetItem(size_t i) {
   ASSERT(i < CountItems());
   return m_pMarkData->GetItem(i);
 }
@@ -42,7 +42,7 @@ int CPDF_ContentMark::GetMarkedContentID() const {
 }
 
 void CPDF_ContentMark::AddMark(ByteString name,
-                               const CPDF_Dictionary* pDict,
+                               CPDF_Dictionary* pDict,
                                bool bDirect) {
   if (!m_pMarkData)
     m_pMarkData.Reset(new MarkData());
@@ -70,9 +70,8 @@ size_t CPDF_ContentMark::MarkData::CountItems() const {
   return m_Marks.size();
 }
 
-const CPDF_ContentMarkItem& CPDF_ContentMark::MarkData::GetItem(
-    size_t index) const {
-  return *m_Marks[index];
+CPDF_ContentMarkItem* CPDF_ContentMark::MarkData::GetItem(size_t index) {
+  return m_Marks[index].Get();
 }
 
 int CPDF_ContentMark::MarkData::GetMarkedContentID() const {
@@ -85,7 +84,7 @@ int CPDF_ContentMark::MarkData::GetMarkedContentID() const {
 }
 
 void CPDF_ContentMark::MarkData::AddMark(ByteString name,
-                                         const CPDF_Dictionary* pDict,
+                                         CPDF_Dictionary* pDict,
                                          bool bDirect) {
   auto pItem = pdfium::MakeRetain<CPDF_ContentMarkItem>();
   pItem->SetName(std::move(name));
