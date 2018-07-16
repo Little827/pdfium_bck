600ecef8f0ff6223565939c3a211a0af65f63932
Tom Sepez <tsepez@chromium.org>
2018-07-16 12:46:00 -0700

    Use UnownedPtr<> to v8::Isolates.
    Isolates are long-lived, but this may catch a few things.
    Fix Dispose()/SetIsolate(nullptr) ordering in cjs_runtime.cpp
    Flip protected -> private in one place.
    
    Change-Id: I26cdd120f799192e93b0d9d04dcde8f348dc21f3

--- fxjs/cfxjse_runtimedata.h
+++ fxjs/cfxjse_runtimedata.h
@@ -9,6 +9,7 @@
 
 #include <memory>
 
+#include "core/fxcrt/unowned_ptr.h"
 #include "v8/include/v8.h"
 
 class CFXJSE_RuntimeList;
@@ -19,7 +20,7 @@ class CFXJSE_RuntimeData {
 
   static CFXJSE_RuntimeData* Get(v8::Isolate* pIsolate);
 
-  v8::Isolate* m_pIsolate;
+  UnownedPtr<v8::Isolate> m_pIsolate;
   v8::Global<v8::FunctionTemplate> m_hRootContextGlobalTemplate;
   v8::Global<v8::Context> m_hRootContext;
 
