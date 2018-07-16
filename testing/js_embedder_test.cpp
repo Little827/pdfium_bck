a763a3b41357e5c4ebb095aa2971cfaafeca4634
Tom Sepez <tsepez@chromium.org>
2018-07-16 12:40:55 -0700

    Use UnownedPtr<> to v8::Isolates.
    Isolates are long-lived, but this may catch a few things.
    Flip protected -> private in one place.
    
    Change-Id: I26cdd120f799192e93b0d9d04dcde8f348dc21f3

--- testing/js_embedder_test.cpp
+++ testing/js_embedder_test.cpp
@@ -17,13 +17,13 @@ void JSEmbedderTest::SetUp() {
   params.array_buffer_allocator = m_pArrayBufferAllocator.get();
   m_pIsolate = v8::Isolate::New(params);
 
-  EmbedderTest::SetExternalIsolate(m_pIsolate);
+  EmbedderTest::SetExternalIsolate(isolate());
   EmbedderTest::SetUp();
 
-  v8::Isolate::Scope isolate_scope(m_pIsolate);
-  v8::HandleScope handle_scope(m_pIsolate);
-  FXJS_PerIsolateData::SetUp(m_pIsolate);
-  m_Engine = pdfium::MakeUnique<CFXJS_Engine>(m_pIsolate);
+  v8::Isolate::Scope isolate_scope(isolate());
+  v8::HandleScope handle_scope(isolate());
+  FXJS_PerIsolateData::SetUp(isolate());
+  m_Engine = pdfium::MakeUnique<CFXJS_Engine>(isolate());
   m_Engine->InitializeEngine();
 }
 
@@ -31,12 +31,7 @@ void JSEmbedderTest::TearDown() {
   m_Engine->ReleaseEngine();
   m_Engine.reset();
   EmbedderTest::TearDown();
-  m_pIsolate->Dispose();
-  m_pIsolate = nullptr;
-}
-
-v8::Isolate* JSEmbedderTest::isolate() {
-  return m_pIsolate;
+  m_pIsolate.Release()->Dispose();
 }
 
 v8::Local<v8::Context> JSEmbedderTest::GetV8Context() {
