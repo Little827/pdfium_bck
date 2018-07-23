f24e639e7bf4ed57c7d1eedf5406aa62fe00fee5
Ryan Harrison <rharrison@chromium.org>
2018-07-23 15:38:25 -0400

    Convert click logging into general mouse logging
    This CL converts the debugging macro for logging out mouse clicks to
    also include mouse moves. The name of the flag and macro have been
    changed to match the new behaviour.
    
    Change-Id: Ib2b95ea2fbf70c8132b8943ce429ddccfef5114f

--- fpdfsdk/fpdf_formfill.cpp
+++ fpdfsdk/fpdf_formfill.cpp
@@ -400,10 +400,10 @@ FPDF_EXPORT FPDF_BOOL FPDF_CALLCONV FORM_OnLButtonDown(FPDF_FORMHANDLE hHandle,
   CPDFSDK_PageView* pPageView = FormHandleToPageView(hHandle, page);
   if (!pPageView)
     return false;
-#ifdef PDF_ENABLE_CLICK_LOGGING
+#ifdef PDF_ENABLE_MOUSE_LOGGING
   fprintf(stderr, "mousedown,left,%d,%d\n", static_cast<int>(round(page_x)),
           static_cast<int>(round(page_y)));
-#endif  // PDF_ENABLE_CLICK_LOGGING
+#endif  // PDF_ENABLE_MOUSE_LOGGING
   return pPageView->OnLButtonDown(CFX_PointF(page_x, page_y), modifier);
 }
 
@@ -415,10 +415,10 @@ FPDF_EXPORT FPDF_BOOL FPDF_CALLCONV FORM_OnLButtonUp(FPDF_FORMHANDLE hHandle,
   CPDFSDK_PageView* pPageView = FormHandleToPageView(hHandle, page);
   if (!pPageView)
     return false;
-#ifdef PDF_ENABLE_CLICK_LOGGING
+#ifdef PDF_ENABLE_MOUSE_LOGGING
   fprintf(stderr, "mouseup,left,%d,%d\n", static_cast<int>(round(page_x)),
           static_cast<int>(round(page_y)));
-#endif  // PDF_ENABLE_CLICK_LOGGING
+#endif  // PDF_ENABLE_MOUSE_LOGGING
   return pPageView->OnLButtonUp(CFX_PointF(page_x, page_y), modifier);
 }
 
@@ -431,10 +431,10 @@ FPDF_EXPORT FPDF_BOOL FPDF_CALLCONV FORM_OnRButtonDown(FPDF_FORMHANDLE hHandle,
   CPDFSDK_PageView* pPageView = FormHandleToPageView(hHandle, page);
   if (!pPageView)
     return false;
-#ifdef PDF_ENABLE_CLICK_LOGGING
+#ifdef PDF_ENABLE_MOUSE_LOGGING
   fprintf(stderr, "mousedown,right,%d,%d\n", static_cast<int>(round(page_x)),
           static_cast<int>(round(page_y)));
-#endif  // PDF_ENABLE_CLICK_LOGGING
+#endif  // PDF_ENABLE_MOUSE_LOGGING
   return pPageView->OnRButtonDown(CFX_PointF(page_x, page_y), modifier);
 }
 
@@ -446,10 +446,10 @@ FPDF_EXPORT FPDF_BOOL FPDF_CALLCONV FORM_OnRButtonUp(FPDF_FORMHANDLE hHandle,
   CPDFSDK_PageView* pPageView = FormHandleToPageView(hHandle, page);
   if (!pPageView)
     return false;
-#ifdef PDF_ENABLE_CLICK_LOGGING
+#ifdef PDF_ENABLE_MOUSE_LOGGING
   fprintf(stderr, "mouseup,right,%d,%d\n", static_cast<int>(round(page_x)),
           static_cast<int>(round(page_y)));
-#endif  // PDF_ENABLE_CLICK_LOGGING
+#endif  // PDF_ENABLE_MOUSE_LOGGING
   return pPageView->OnRButtonUp(CFX_PointF(page_x, page_y), modifier);
 }
 #endif  // PDF_ENABLE_XFA
