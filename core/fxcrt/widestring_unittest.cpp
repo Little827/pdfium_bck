22bcd82ddb15268a0f3b88afe0b4f1bfdaccba20
Tom Sepez <tsepez@chromium.org>
2018-08-06 14:27:01 -0700

    Speculative fix for null deref above ThrowNoDefaultPropertyException
    Avoid possibility of an unterminated string or nullptr by avoiding
    stringviews, since the non-view classes copy and always provide
    a suitable result. Always use %ls and widestrings in wprintf()
    variants, since that combination is consistent across our platforms
    (%s, %S, etc. have idiosyncracies).
    
    Bug: 870952
    Change-Id: Ief62a42c3486e8298f9583b56e9333db1a74972a

--- core/fxcrt/widestring_unittest.cpp
+++ core/fxcrt/widestring_unittest.cpp
@@ -1381,6 +1381,16 @@ TEST(WideString, FormatOutOfRangeChar) {
   EXPECT_NE(L"", WideString::Format(L"unsupported char '%c'", 0x00FF00FF));
 }
 
+TEST(WideString, FormatString) {
+  // %ls and wide characters are the reliable combination across platforms.
+  EXPECT_EQ(L"", WideString::Format(L"%ls", L""));
+  EXPECT_EQ(L"", WideString::Format(L"%ls", WideString().c_str()));
+  EXPECT_EQ(L"clams", WideString::Format(L"%ls", L"clams"));
+  EXPECT_EQ(L"cla", WideString::Format(L"%.3ls", L"clams"));
+  EXPECT_EQ(L"\u208d\u208e", WideString::Format(L"%ls", L"\u208d\u208e"));
+  EXPECT_EQ(L"\u208d", WideString::Format(L"%.1ls", L"\u208d\u208e"));
+}
+
 TEST(WideString, Empty) {
   WideString empty_str;
   EXPECT_TRUE(empty_str.IsEmpty());
