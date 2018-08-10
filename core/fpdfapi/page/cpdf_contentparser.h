1c9b50dbe6e643e25b4916972148e14485dd8abb
Lei Zhang <thestig@chromium.org>
2018-08-10 15:25:53 -0700

    Refactor page content parsing code in CPDF_ContentParser.
    The page content can be an array or a stream. Make separate functions
    for array and stream parsing, and for handling failures.
    
    Change-Id: I9b725d10228ef046195cb46a9a8becf4de2f5d00

--- core/fpdfapi/page/cpdf_contentparser.h
+++ core/fpdfapi/page/cpdf_contentparser.h
@@ -18,8 +18,10 @@
 #include "core/fxcrt/unowned_ptr.h"
 
 class CPDF_AllStates;
+class CPDF_Array;
 class CPDF_Form;
 class CPDF_Page;
+class CPDF_Stream;
 class CPDF_StreamAcc;
 class CPDF_Type3Char;
 
@@ -52,6 +54,10 @@ class CPDF_ContentParser {
   Stage PrepareContent();
   Stage Parse();
   Stage CheckClip();
+
+  bool HandlePageContentArray(CPDF_Array* pArray);
+  void HandlePageContentFailure();
+  void HandlePageContentStream(CPDF_Stream* pStream);
 
   Stage m_CurrentStage;
   UnownedPtr<CPDF_PageObjectHolder> const m_pObjectHolder;
