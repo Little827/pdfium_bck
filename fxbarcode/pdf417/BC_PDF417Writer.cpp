e39dec4f17d54c46194b23ccfcc5883b9e2f79a5
Henrique Nakashima <hnakashima@chromium.org>
2018-08-13 13:12:58 -0400

    [debug] Optimize rendering of two dimensional barcodes.
    Bug: 872907
    Change-Id: I40c8bd3c3e6106e9392650d2340e393d56a16021

--- fxbarcode/pdf417/BC_PDF417Writer.cpp
+++ fxbarcode/pdf417/BC_PDF417Writer.cpp
@@ -23,6 +23,7 @@
 #include "fxbarcode/pdf417/BC_PDF417Writer.h"
 
 #include <algorithm>
+#include <iostream>
 
 #include "fxbarcode/BC_TwoDimWriter.h"
 #include "fxbarcode/common/BC_CommonBitArray.h"
@@ -34,9 +35,9 @@
 CBC_PDF417Writer::CBC_PDF417Writer() {
   m_bFixedSize = false;
 }
-CBC_PDF417Writer::~CBC_PDF417Writer() {
-  m_bTruncated = true;
-}
+
+CBC_PDF417Writer::~CBC_PDF417Writer() {}
+
 bool CBC_PDF417Writer::SetErrorCorrectionLevel(int32_t level) {
   if (level < 0 || level > 8) {
     return false;
@@ -44,16 +45,17 @@ bool CBC_PDF417Writer::SetErrorCorrectionLevel(int32_t level) {
   m_iCorrectLevel = level;
   return true;
 }
-void CBC_PDF417Writer::SetTruncated(bool truncated) {
-  m_bTruncated = truncated;
-}
 
 uint8_t* CBC_PDF417Writer::Encode(const WideString& contents,
                                   int32_t& outWidth,
                                   int32_t& outHeight) {
+  std::cerr << "CBC_PDF417Writer::Encode param contents "
+            << contents.GetLength() << std::endl;
   CBC_PDF417 encoder;
   int32_t col = (m_Width / m_ModuleWidth - 69) / 17;
   int32_t row = m_Height / (m_ModuleWidth * 20);
+  std::cerr << "CBC_PDF417Writer::Encode col " << col << ", row " << row
+            << std::endl;
   if (row >= 3 && row <= 90 && col >= 1 && col <= 30)
     encoder.setDimensions(col, col, row, row);
   else if (col >= 1 && col <= 30)
@@ -63,49 +65,21 @@ uint8_t* CBC_PDF417Writer::Encode(const WideString& contents,
   if (!encoder.generateBarcodeLogic(contents, m_iCorrectLevel))
     return nullptr;
 
-  int32_t lineThickness = 2;
-  int32_t aspectRatio = 4;
   CBC_BarcodeMatrix* barcodeMatrix = encoder.getBarcodeMatrix();
-  std::vector<uint8_t> originalScale = barcodeMatrix->getScaledMatrix(
-      lineThickness, aspectRatio * lineThickness);
-  int32_t width = outWidth;
-  int32_t height = outHeight;
+  std::cerr
+      << "CBC_PDF417Writer::Encode checkpoint01 barcodeMatrix->getWidth() "
+      << barcodeMatrix->getWidth() << ", barcodeMatrix->getHeight() "
+      << barcodeMatrix->getHeight() << std::endl;
+  std::vector<uint8_t> originalScale = barcodeMatrix->getMatrix();
+  std::cerr
+      << "CBC_PDF417Writer::Encode checkpoint02 barcodeMatrix->getWidth() "
+      << barcodeMatrix->getWidth() << ", barcodeMatrix->getHeight() "
+      << barcodeMatrix->getHeight() << std::endl;
   outWidth = barcodeMatrix->getWidth();
   outHeight = barcodeMatrix->getHeight();
-  bool rotated = false;
-  if ((height > width) ^ (outWidth < outHeight)) {
-    rotateArray(originalScale, outHeight, outWidth);
-    rotated = true;
-    int32_t temp = outHeight;
-    outHeight = outWidth;
-    outWidth = temp;
-  }
-  int32_t scaleX = width / outWidth;
-  int32_t scaleY = height / outHeight;
-  int32_t scale = std::min(scaleX, scaleY);
-  if (scale > 1) {
-    originalScale = barcodeMatrix->getScaledMatrix(
-        scale * lineThickness, scale * aspectRatio * lineThickness);
-    if (rotated) {
-      rotateArray(originalScale, outHeight, outWidth);
-      int32_t temp = outHeight;
-      outHeight = outWidth;
-      outWidth = temp;
-    }
-  }
+  std::cerr << "CBC_PDF417Writer::Encode checkpoint1 outWidth " << outWidth
+            << ", outHeight " << outHeight << std::endl;
   uint8_t* result = FX_Alloc2D(uint8_t, outHeight, outWidth);
   memcpy(result, originalScale.data(), outHeight * outWidth);
   return result;
-}
-
-void CBC_PDF417Writer::rotateArray(std::vector<uint8_t>& bitarray,
-                                   int32_t height,
-                                   int32_t width) {
-  std::vector<uint8_t> temp = bitarray;
-  for (int32_t ii = 0; ii < height; ii++) {
-    int32_t inverseii = height - ii - 1;
-    for (int32_t jj = 0; jj < width; jj++) {
-      bitarray[jj * height + inverseii] = temp[ii * width + jj];
-    }
-  }
 }
