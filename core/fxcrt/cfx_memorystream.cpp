e4b6741b825b1df05ccf4d97aaa3ed5b5ee39774
Lei Zhang <thestig@chromium.org>
2018-08-13 19:56:55 -0700

    Clean up CFX_MemoryStream.
    - Make it take in a std::unique_ptr.
    - Fix up various checks.
    - Order the methods in .cpp file like the header.
    
    Change-Id: I63f798170a8eb259cd0b9f846321f982dfae54de

--- core/fxcrt/cfx_memorystream.cpp
+++ core/fxcrt/cfx_memorystream.cpp
@@ -7,19 +7,16 @@
 #include "core/fxcrt/cfx_memorystream.h"
 
 #include <algorithm>
+#include <utility>
 
 #include "core/fxcrt/fx_safe_types.h"
 
-namespace {
-
-constexpr size_t kBlockSize = 64 * 1024;
-
-}  // namespace
-
 CFX_MemoryStream::CFX_MemoryStream() : m_nTotalSize(0), m_nCurSize(0) {}
 
-CFX_MemoryStream::CFX_MemoryStream(uint8_t* pBuffer, size_t nSize)
-    : m_data(pBuffer), m_nTotalSize(nSize), m_nCurSize(nSize) {}
+CFX_MemoryStream::CFX_MemoryStream(
+    std::unique_ptr<uint8_t, FxFreeDeleter> pBuffer,
+    size_t nSize)
+    : m_data(std::move(pBuffer)), m_nTotalSize(nSize), m_nCurSize(nSize) {}
 
 CFX_MemoryStream::~CFX_MemoryStream() = default;
 
@@ -27,16 +24,12 @@ FX_FILESIZE CFX_MemoryStream::GetSize() {
   return static_cast<FX_FILESIZE>(m_nCurSize);
 }
 
-bool CFX_MemoryStream::IsEOF() {
-  return m_nCurPos >= static_cast<size_t>(GetSize());
-}
-
 FX_FILESIZE CFX_MemoryStream::GetPosition() {
   return static_cast<FX_FILESIZE>(m_nCurPos);
 }
 
-bool CFX_MemoryStream::Flush() {
-  return true;
+bool CFX_MemoryStream::IsEOF() {
+  return m_nCurPos >= m_nCurSize;
 }
 
 bool CFX_MemoryStream::ReadBlock(void* buffer,
@@ -47,10 +40,8 @@ bool CFX_MemoryStream::ReadBlock(void* buffer,
 
   FX_SAFE_SIZE_T newPos = size;
   newPos += offset;
-  if (!newPos.IsValid() || newPos.ValueOrDefault(0) == 0 ||
-      newPos.ValueOrDie() > m_nCurSize) {
+  if (!newPos.IsValid() || newPos.ValueOrDie() > m_nCurSize)
     return false;
-  }
 
   m_nCurPos = newPos.ValueOrDie();
   memcpy(buffer, &GetBuffer()[offset], size);
@@ -62,7 +53,7 @@ size_t CFX_MemoryStream::ReadBlock(void* buffer, size_t size) {
     return 0;
 
   size_t nRead = std::min(size, m_nCurSize - m_nCurPos);
-  if (!ReadBlock(buffer, static_cast<int32_t>(m_nCurPos), nRead))
+  if (!ReadBlock(buffer, static_cast<FX_FILESIZE>(m_nCurPos), nRead))
     return 0;
 
   return nRead;
@@ -71,7 +62,7 @@ size_t CFX_MemoryStream::ReadBlock(void* buffer, size_t size) {
 bool CFX_MemoryStream::WriteBlock(const void* buffer,
                                   FX_FILESIZE offset,
                                   size_t size) {
-  if (!buffer || !size)
+  if (!buffer || !size || offset < 0)
     return false;
 
   FX_SAFE_SIZE_T newPos = size;
@@ -81,6 +72,7 @@ bool CFX_MemoryStream::WriteBlock(const void* buffer,
 
   m_nCurPos = newPos.ValueOrDie();
   if (m_nCurPos > m_nTotalSize) {
+    static constexpr size_t kBlockSize = 64 * 1024;
     m_nTotalSize = (m_nCurPos + kBlockSize - 1) / kBlockSize * kBlockSize;
     if (m_data)
       m_data.reset(FX_Realloc(uint8_t, m_data.get(), m_nTotalSize));
@@ -91,5 +83,9 @@ bool CFX_MemoryStream::WriteBlock(const void* buffer,
   memcpy(&m_data.get()[offset], buffer, size);
   m_nCurSize = std::max(m_nCurSize, m_nCurPos);
 
+  return true;
+}
+
+bool CFX_MemoryStream::Flush() {
   return true;
 }
