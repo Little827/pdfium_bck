9580e0ef354e88d460301f9868cf8e2d6fdb5bab
Tom Sepez <tsepez@chromium.org>
2018-08-16 12:16:41 -0700

    Replace optional bool bNotify with enum type.
    Adds clarity to the call sites.
    
    Change-Id: Id4deed9adda2ad79f0847d618792429044d4f7d6

--- core/fpdfdoc/cpdf_interform.h
+++ core/fpdfdoc/cpdf_interform.h
@@ -13,6 +13,7 @@
 
 #include "core/fpdfapi/parser/fpdf_parser_decode.h"
 #include "core/fpdfdoc/cpdf_defaultappearance.h"
+#include "core/fpdfdoc/cpdf_formfield.h"
 #include "core/fxcrt/fx_string.h"
 #include "core/fxcrt/fx_system.h"
 #include "core/fxcrt/unowned_ptr.h"
@@ -23,7 +24,6 @@ class CPDF_Document;
 class CPDF_Dictionary;
 class CPDF_Font;
 class CPDF_FormControl;
-class CPDF_FormField;
 class CPDF_Object;
 class CPDF_Page;
 class IPDF_FormNotify;
@@ -80,10 +80,10 @@ class CPDF_InterForm {
       bool bIncludeOrExclude,
       bool bSimpleFileSpec) const;
 
-  void ResetForm(const std::vector<CPDF_FormField*>& fields,
+  void ResetForm(NotificationOption notify);
+  void ResetForm(const std::vector<CPDF_FormField*>& fields,  // use span?
                  bool bIncludeOrExclude,
-                 bool bNotify);
-  void ResetForm(bool bNotify);
+                 NotificationOption notify);
 
   void SetFormNotify(IPDF_FormNotify* pNotify);
   bool HasXFAForm() const;
