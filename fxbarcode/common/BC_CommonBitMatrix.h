43f45cbd66e008cfbb4f36f6c3192cfd1714689c
Henrique Nakashima <hnakashima@chromium.org>
2018-08-17 14:42:13 -0400

    Optimize rendering of two dimensional barcodes: defer upscale.
    Defer upscaling as late as possible so that intermediary data
    structures are smaller.
    
    Make a couple of changes along the way to preserve the barcode
    correctness and fix some padding issues.
    
    For my example, this is a ~43x improvement in rendering time, down
    from ~390ms per barcode to ~9ms.
    
    Bug: 872907, pdfium:1135
    Change-Id: If532e0f168f02fea9c31d473f34c0009da4f4612

--- fxbarcode/common/BC_CommonBitMatrix.h
+++ fxbarcode/common/BC_CommonBitMatrix.h
@@ -22,7 +22,7 @@ class CBC_CommonBitMatrix {
   void Init(int32_t width, int32_t height);
 
   bool Get(int32_t x, int32_t y) const;
-  void Set(int32_t x, int32_t y);
+  bool Set(int32_t x, int32_t y);
   void Flip(int32_t x, int32_t y);
   void Clear();
   bool SetRegion(int32_t left, int32_t top, int32_t width, int32_t height);
