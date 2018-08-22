10145c80db510615617cea21b3ff9150338bc69d
Henrique Nakashima <hnakashima@chromium.org>
2018-08-22 12:23:48 -0400

    Remove rotation code from CBC_PDF417Writer.
    The in/out parameter of Encode is always passed as 0,
    so the scaling and rotation never happen.
    
    Change-Id: Iab1815208080606ee50411cdc11250471daef023

--- fxbarcode/pdf417/BC_PDF417Writer.h
+++ fxbarcode/pdf417/BC_PDF417Writer.h
@@ -16,22 +16,13 @@
 class CBC_PDF417Writer : public CBC_TwoDimWriter {
  public:
   CBC_PDF417Writer();
-  ~CBC_PDF417Writer() override;
 
   uint8_t* Encode(const WideString& contents,
-                  int32_t& outWidth,
-                  int32_t& outHeight);
+                  int32_t* outWidth,
+                  int32_t* outHeight);
 
   // CBC_TwoDimWriter
   bool SetErrorCorrectionLevel(int32_t level) override;
-
-  void SetTruncated(bool truncated);
-
- private:
-  void rotateArray(std::vector<uint8_t>& bitarray,
-                   int32_t width,
-                   int32_t height);
-  bool m_bTruncated;
 };
 
 #endif  // FXBARCODE_PDF417_BC_PDF417WRITER_H_
