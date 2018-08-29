a3962affcba22ef21455540281e1bc3844035482
Henrique Nakashima <hnakashima@chromium.org>
2018-08-29 16:16:31 -0400

    Simplify radial shading.
    Change-Id: I3b6a386cf376a418ad64514313c791437ea54cf6

--- core/fpdfapi/render/cpdf_renderstatus.cpp
+++ core/fpdfapi/render/cpdf_renderstatus.cpp
@@ -259,34 +259,32 @@ void DrawRadialShading(const RetainPtr<CFX_DIBitmap>& pBitmap,
         FXARGB_TODIB(ArgbEncode(alpha, FXSYS_round(R * 255),
                                 FXSYS_round(G * 255), FXSYS_round(B * 255)));
   }
-  float a = ((start_x - end_x) * (start_x - end_x)) +
-            ((start_y - end_y) * (start_y - end_y)) -
-            ((start_r - end_r) * (start_r - end_r));
+
+  float dx = end_x - start_x;
+  float dy = end_y - start_y;
+  float dr = end_r - start_r;
+  float a = dx * dx + dy * dy - dr * dr;
   if (a > -0.001f && a < 0.001f)
     a = 0.0f;
 
   int width = pBitmap->GetWidth();
   int height = pBitmap->GetHeight();
   int pitch = pBitmap->GetPitch();
-  bool bDecreasing = false;
-  if (start_r > end_r) {
-    int length = (int)sqrt((((start_x - end_x) * (start_x - end_x)) +
-                            ((start_y - end_y) * (start_y - end_y))));
-    if (length < start_r - end_r) {
-      bDecreasing = true;
-    }
-  }
+
+  bool bDecreasing =
+      (start_r > end_r &&
+       static_cast<int>(sqrt(dx * dx + dy * dy)) < start_r - end_r);
+
   CFX_Matrix matrix = pObject2Bitmap->GetInverse();
   for (int row = 0; row < height; row++) {
     uint32_t* dib_buf = (uint32_t*)(pBitmap->GetBuffer() + row * pitch);
     for (int column = 0; column < width; column++) {
       CFX_PointF pos = matrix.Transform(
           CFX_PointF(static_cast<float>(column), static_cast<float>(row)));
-      float b = -2 * (((pos.x - start_x) * (end_x - start_x)) +
-                      ((pos.y - start_y) * (end_y - start_y)) +
-                      (start_r * (end_r - start_r)));
-      float c = ((pos.x - start_x) * (pos.x - start_x)) +
-                ((pos.y - start_y) * (pos.y - start_y)) - (start_r * start_r);
+      float pos_dx = pos.x - start_x;
+      float pos_dy = pos.y - start_y;
+      float b = -2 * (pos_dx * dx + pos_dy * dy + start_r * dr);
+      float c = pos_dx * pos_dx + pos_dy * pos_dy - start_r * start_r;
       float s;
       if (a == 0.0f) {
         s = -c / b;
@@ -304,10 +302,11 @@ void DrawRadialShading(const RetainPtr<CFX_DIBitmap>& pBitmap,
           s = (s1 >= 0 || bStartExtend) ? s1 : s2;
         else
           s = (s2 <= 1.0f || bEndExtend) ? s2 : s1;
-        if ((start_r + s * (end_r - start_r)) < 0) {
+        if ((start_r + s * dr) < 0) {
           continue;
         }
       }
+
       int index = (int32_t)(s * (kShadingSteps - 1));
       if (index < 0) {
         if (!bStartExtend) {
