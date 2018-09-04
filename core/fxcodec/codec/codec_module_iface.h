21ccded39f13a7e0f3a8d43a81b51580591254ef
Tom Sepez <tsepez@chromium.org>
2018-09-04 15:38:05 -0700

    Introduce CodecModuleIface for progressive decoder modules.
    Another step before trying to fix the memory issue. Forces common
    APIs on the bunch of decoders, though some methods are unused.
    Requires adding some arguments/return values to get to a common
    API which are not used in all cases (yet). Required converting
    some args to spans. Required proxying a GetJumpMark call through
    the public module API to the private context.
    
    Change-Id: I0c0b7415141ff2a6f4f44777ca3d05521f08130d

---
+++ core/fxcodec/codec/codec_module_iface.h
@@ -0,0 +1,30 @@
+// Copyright 2018 PDFium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Original code copyright 2014 Foxit Software Inc. http://www.foxitsoftware.com
+
+#ifndef CORE_FXCODEC_CODEC_CODEC_MODULE_IFACE_H_
+#define CORE_FXCODEC_CODEC_CODEC_MODULE_IFACE_H_
+
+#include "core/fxcrt/fx_system.h"
+#include "third_party/base/span.h"
+
+class CFX_DIBAttribute;
+
+class CodecModuleIface {
+ public:
+  class Context {
+   public:
+    virtual ~Context() {}
+  };
+
+  virtual ~CodecModuleIface() {}
+
+  virtual FX_FILESIZE GetAvailInput(Context* pContext) const = 0;
+  virtual bool Input(Context* pContext,
+                     pdfium::span<uint8_t> src_buf,
+                     CFX_DIBAttribute* pAttribute) = 0;
+};
+
+#endif  // CORE_FXCODEC_CODEC_CODEC_MODULE_IFACE_H_
