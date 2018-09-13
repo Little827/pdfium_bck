8f2760b3250eddf6de12cd5bbae8567964111ee5
Tom Sepez <tsepez@chromium.org>
2018-09-13 11:56:27 -0700

    Revert "Optimize CPDF_CMapParser"
    This reverts commit 5b9a0a9c12960e303d312656da4d49c8997b617d.
    
    Change-Id: I1b28509fd2b2ec1e8cf0ec2be1e28d6b23a09a48

--- core/fpdfapi/font/cpdf_cmap.h
+++ core/fpdfapi/font/cpdf_cmap.h
@@ -66,11 +66,8 @@ class CPDF_CMap final : public Retainable {
 
   void SetVertical(bool vert) { m_bVertical = vert; }
   void SetCodingScheme(CodingScheme scheme) { m_CodingScheme = scheme; }
-  const std::vector<CodeRange>& GetMixedFourByteLeadingRanges() {
-    return m_MixedFourByteLeadingRanges;
-  }
-  void AppendMixedFourByteLeadingRanges(const CodeRange& range) {
-    m_MixedFourByteLeadingRanges.push_back(range);
+  void SetMixedFourByteLeadingRanges(std::vector<CodeRange> range) {
+    m_MixedFourByteLeadingRanges = range;
   }
 
   int GetCoding() const { return m_Coding; }
