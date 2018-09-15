419925a6640fa400e8a4a3449d16c76acd065b86
Lei Zhang <thestig@chromium.org>
2018-09-15 00:16:29 -0700

    Make PDF_DataDecode()'s buffer out parameter a std::unique_ptr.
    Change-Id: Id8f7473c590ef94799d0dd29f5de790a453c86f6

--- core/fpdfapi/parser/cpdf_stream_acc.cpp
+++ core/fpdfapi/parser/cpdf_stream_acc.cpp
@@ -56,7 +56,7 @@ void CPDF_StreamAcc::LoadAllData(bool bRawAccess,
     pSrcData = std::move(pTempSrcData);
   }
 
-  uint8_t* pDecodedData = nullptr;
+  std::unique_ptr<uint8_t, FxFreeDeleter> pDecodedData;
   uint32_t dwDecodedSize = 0;
   if (!PDF_DataDecode({pSrcData.Get(), dwSrcSize}, m_pStream->GetDict(),
                       estimated_size, bImageAcc, &pDecodedData, &dwDecodedSize,
@@ -66,16 +66,13 @@ void CPDF_StreamAcc::LoadAllData(bool bRawAccess,
     return;
   }
 
-  if (!pDecodedData)
-    return;
-
-  if (pDecodedData == pSrcData.Get()) {
+  if (pDecodedData) {
+    ASSERT(pDecodedData.get() != pSrcData.Get());
+    m_pData = std::move(pDecodedData);
+    m_dwSize = dwDecodedSize;
+  } else {
     m_pData = std::move(pSrcData);
     m_dwSize = dwSrcSize;
-  } else {
-    std::unique_ptr<uint8_t, FxFreeDeleter> p(pDecodedData);
-    m_pData = std::move(p);
-    m_dwSize = dwDecodedSize;
   }
 }
 
