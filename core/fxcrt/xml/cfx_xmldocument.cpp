413982f834c165b175c01f8e73d0810b6d7a6d71
Ryan Harrison <rharrison@chromium.org>
2018-09-18 12:55:34 -0400

    Transfer ownership of nodes to top-level XML doc
    For XFA, XML nodes are owned by the XML doc that they were created by,
    but references to them are stored elsewhere. For a PDF document there
    is one top-level XML document created and retained when the initial
    XFA XML is parsed. Another can be created if loadXML is called by
    JS. In the existing code the XML doc that owns the newly created nodes
    is local to loadXML. So the nodes are destroyed right after putting
    refernces to them into the main XFA data structures.
    
    This CL adds in a method to transfer ownership of the XML nodes from
    one doc to another, and uses it to correctly retain the newly created
    nodes, by having them owned by the top-level XML doc.
    
    BUG=chromium:884664
    
    Change-Id: Id29b4edbfe44aefb9713328e4e217e830f7e9e14

--- core/fxcrt/xml/cfx_xmldocument.cpp
+++ core/fxcrt/xml/cfx_xmldocument.cpp
@@ -12,3 +12,12 @@ CFX_XMLDocument::CFX_XMLDocument() {
 }
 
 CFX_XMLDocument::~CFX_XMLDocument() = default;
+
+void CFX_XMLDocument::AppendNodesFrom(CFX_XMLDocument* other) {
+  size_t old_node_count = nodes_.size();
+  nodes_.resize(old_node_count + other->nodes_.size());
+
+  for (size_t i = 0; i < other->nodes_.size(); i++)
+    nodes_[old_node_count + i].swap(other->nodes_[i]);
+  other->nodes_.clear();
+}
