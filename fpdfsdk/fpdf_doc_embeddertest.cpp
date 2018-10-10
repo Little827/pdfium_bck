f9e70536ea2a24894aa9c3432fd859165c4c1821
Tom Sepez <tsepez@chromium.org>
2018-10-10 11:54:51 -0700

    Embedder tests for other kinds of actions.
    Change-Id: Ia4619be65e2ab8ee4bf19ba9608c1cc94594ba89

--- fpdfsdk/fpdf_doc_embeddertest.cpp
+++ fpdfsdk/fpdf_doc_embeddertest.cpp
@@ -264,7 +264,119 @@ TEST_F(FPDFDocEmbeddertest, ActionLaunch) {
   // Other public methods are not appropriate for this action type.
   EXPECT_EQ(nullptr, FPDFAction_GetDest(document(), action));
   EXPECT_EQ(0u, FPDFAction_GetURIPath(document(), action, buf, bufsize));
+  UnloadPage(page);
+}
 
+TEST_F(FPDFDocEmbeddertest, ActionURI) {
+  EXPECT_TRUE(OpenDocument("uri_action.pdf"));
+
+  FPDF_PAGE page = LoadPage(0);
+  ASSERT_TRUE(page);
+
+  // The target action is nearly the size of the whole page.
+  FPDF_LINK link = FPDFLink_GetLinkAtPoint(page, 100, 100);
+  ASSERT_TRUE(link);
+
+  FPDF_ACTION action = FPDFLink_GetAction(link);
+  ASSERT_TRUE(action);
+
+  const char kExpectedResult[] = "https://example.com/page.html";
+  const unsigned long kExpectedLength = sizeof(kExpectedResult);
+  unsigned long bufsize = FPDFAction_GetURIPath(document(), action, nullptr, 0);
+  ASSERT_EQ(kExpectedLength, bufsize);
+
+  char buf[kExpectedLength];
+  EXPECT_EQ(bufsize, FPDFAction_GetURIPath(document(), action, buf, bufsize));
+  EXPECT_EQ(std::string(kExpectedResult), std::string(buf));
+  UnloadPage(page);
+
+  // Other public methods are not appropriate for this action type.
+  EXPECT_EQ(nullptr, FPDFAction_GetDest(document(), action));
+  EXPECT_EQ(0u, FPDFAction_GetFilePath(action, nullptr, 0));
+}
+
+TEST_F(FPDFDocEmbeddertest, ActionGoto) {
+  EXPECT_TRUE(OpenDocument("goto_action.pdf"));
+
+  FPDF_PAGE page = LoadPage(0);
+  ASSERT_TRUE(page);
+
+  // The target action is nearly the size of the whole page.
+  FPDF_LINK link = FPDFLink_GetLinkAtPoint(page, 100, 100);
+  ASSERT_TRUE(link);
+
+  FPDF_ACTION action = FPDFLink_GetAction(link);
+  ASSERT_TRUE(action);
+
+  EXPECT_TRUE(FPDFAction_GetDest(document(), action));
+  UnloadPage(page);
+}
+
+TEST_F(FPDFDocEmbeddertest, ActionHide) {
+  EXPECT_TRUE(OpenDocument("hide_action.pdf"));
+
+  FPDF_PAGE page = LoadPage(0);
+  ASSERT_TRUE(page);
+
+  // The target action is nearly the size of the whole page.
+  FPDF_LINK link = FPDFLink_GetLinkAtPoint(page, 100, 100);
+  ASSERT_TRUE(link);
+
+  FPDF_ACTION action = FPDFLink_GetAction(link);
+  ASSERT_TRUE(action);
+
+  EXPECT_TRUE(FPDFAction_GetDest(document(), action));
+  UnloadPage(page);
+}
+
+TEST_F(FPDFDocEmbeddertest, ActionNamed) {
+  EXPECT_TRUE(OpenDocument("named_action.pdf"));
+
+  FPDF_PAGE page = LoadPage(0);
+  ASSERT_TRUE(page);
+
+  // The target action is nearly the size of the whole page.
+  FPDF_LINK link = FPDFLink_GetLinkAtPoint(page, 100, 100);
+  ASSERT_TRUE(link);
+
+  FPDF_ACTION action = FPDFLink_GetAction(link);
+  ASSERT_TRUE(action);
+
+  EXPECT_TRUE(FPDFAction_GetDest(document(), action));
+  UnloadPage(page);
+}
+
+TEST_F(FPDFDocEmbeddertest, ActionResetForm) {
+  EXPECT_TRUE(OpenDocument("resetform_action.pdf"));
+
+  FPDF_PAGE page = LoadPage(0);
+  ASSERT_TRUE(page);
+
+  // The target action is nearly the size of the whole page.
+  FPDF_LINK link = FPDFLink_GetLinkAtPoint(page, 100, 100);
+  ASSERT_TRUE(link);
+
+  FPDF_ACTION action = FPDFLink_GetAction(link);
+  ASSERT_TRUE(action);
+
+  EXPECT_TRUE(FPDFAction_GetDest(document(), action));
+  UnloadPage(page);
+}
+
+TEST_F(FPDFDocEmbeddertest, ActionSubmitForm) {
+  EXPECT_TRUE(OpenDocument("submitform_action.pdf"));
+
+  FPDF_PAGE page = LoadPage(0);
+  ASSERT_TRUE(page);
+
+  // The target action is nearly the size of the whole page.
+  FPDF_LINK link = FPDFLink_GetLinkAtPoint(page, 100, 100);
+  ASSERT_TRUE(link);
+
+  FPDF_ACTION action = FPDFLink_GetAction(link);
+  ASSERT_TRUE(action);
+
+  EXPECT_TRUE(FPDFAction_GetDest(document(), action));
   UnloadPage(page);
 }
 
