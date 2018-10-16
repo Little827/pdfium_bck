617f39c7940758eaf33ff724b304bd74c414456a
Lei Zhang <thestig@chromium.org>
2018-10-15 22:28:38 -0700

    Update PartitionAlloc from Chromium at r599712.
    BUG=pdfium:1170
    
    Change-Id: I0f8dfb3d517beaa682a9ca7ad4831c5a7a10dc3b

--- third_party/base/allocator/partition_allocator/spin_lock.h
+++ third_party/base/allocator/partition_allocator/spin_lock.h
@@ -9,24 +9,21 @@
 #include <memory>
 #include <mutex>
 
-#include "third_party/base/base_export.h"
-#include "third_party/base/compiler_specific.h"
+#include "base/base_export.h"
+#include "base/compiler_specific.h"
 
 // Spinlock is a simple spinlock class based on the standard CPU primitive of
 // atomic increment and decrement of an int at a given memory address. These are
 // intended only for very short duration locks and assume a system with multiple
 // cores. For any potentially longer wait you should use a real lock, such as
 // |base::Lock|.
-//
-// |SpinLock|s MUST be globals. Using them as (e.g.) struct/class members will
-// result in an uninitialized lock, which is dangerously incorrect.
-
-namespace pdfium {
 namespace base {
 namespace subtle {
 
-class SpinLock {
+class BASE_EXPORT SpinLock {
  public:
+  constexpr SpinLock() = default;
+  ~SpinLock() = default;
   using Guard = std::lock_guard<SpinLock>;
 
   ALWAYS_INLINE void lock() {
@@ -42,13 +39,12 @@ class SpinLock {
  private:
   // This is called if the initial attempt to acquire the lock fails. It's
   // slower, but has a much better scheduling and power consumption behavior.
-  BASE_EXPORT void LockSlow();
+  void LockSlow();
 
-  std::atomic_int lock_;
+  std::atomic_int lock_{0};
 };
 
 }  // namespace subtle
 }  // namespace base
-}  // namespace pdfium
 
 #endif  // BASE_ALLOCATOR_PARTITION_ALLOCATOR_SPIN_LOCK_H
