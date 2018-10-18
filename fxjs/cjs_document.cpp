244db46f8b9698ffd0615310f3a937b6e9a15734
Tom Sepez <tsepez@chromium.org>
2018-10-18 12:54:13 -0700

    Expand CJS_Document::mail{Doc,Form}() parameters
    Use ExpandKeywordParams() rather than ad-hoc processing.
    Add test case for {} argument.
    
    Change-Id: I1f60a87b3995d62f4bead79245a1f58fd31b102e

--- fxjs/cjs_document.cpp
+++ fxjs/cjs_document.cpp
@@ -25,7 +25,6 @@
 #include "fxjs/cjs_delaydata.h"
 #include "fxjs/cjs_field.h"
 #include "fxjs/cjs_icon.h"
-#include "fxjs/cjs_printparamsobj.h"
 #include "fxjs/js_resources.h"
 
 namespace {
@@ -378,42 +377,40 @@ CJS_Result CJS_Document::importTextData(
 CJS_Result CJS_Document::mailDoc(
     CJS_Runtime* pRuntime,
     const std::vector<v8::Local<v8::Value>>& params) {
-  // TODO(tsepez): Check maximum number of allowed params.
-  size_t nLength = params.size();
-  bool bUI = true;
-  WideString cTo;
-  WideString cCc;
-  WideString cBcc;
-  WideString cSubject;
-  WideString cMsg;
+  if (!m_pFormFillEnv)
+    return CJS_Result::Failure(JSMessage::kBadObjectError);
 
-  if (nLength > 0 && params[0]->IsObject()) {
-    v8::Local<v8::Object> pObj = pRuntime->ToObject(params[0]);
-    bUI = pRuntime->ToBoolean(pRuntime->GetObjectProperty(pObj, L"bUI"));
-    cTo = pRuntime->ToWideString(pRuntime->GetObjectProperty(pObj, L"cTo"));
-    cCc = pRuntime->ToWideString(pRuntime->GetObjectProperty(pObj, L"cCc"));
-    cBcc = pRuntime->ToWideString(pRuntime->GetObjectProperty(pObj, L"cBcc"));
-    cSubject =
-        pRuntime->ToWideString(pRuntime->GetObjectProperty(pObj, L"cSubject"));
-    cMsg = pRuntime->ToWideString(pRuntime->GetObjectProperty(pObj, L"cMsg"));
-  } else {
-    if (nLength > 0)
-      bUI = pRuntime->ToBoolean(params[0]);
-    if (nLength > 1)
-      cTo = pRuntime->ToWideString(params[1]);
-    if (nLength > 2)
-      cCc = pRuntime->ToWideString(params[2]);
-    if (nLength > 3)
-      cBcc = pRuntime->ToWideString(params[3]);
-    if (nLength > 4)
-      cSubject = pRuntime->ToWideString(params[4]);
-    if (nLength > 5)
-      cMsg = pRuntime->ToWideString(params[5]);
-  }
+  std::vector<v8::Local<v8::Value>> newParams =
+      ExpandKeywordParams(pRuntime, params, 6, L"bUI", L"cTo", L"cCc", L"cBcc",
+                          L"cSubject", L"cMsg");
+
+  bool bUI = true;
+  if (IsExpandedParamKnown(newParams[0]))
+    bUI = pRuntime->ToBoolean(newParams[0]);
+
+  WideString cTo;
+  if (IsExpandedParamKnown(newParams[1]))
+    cTo = pRuntime->ToWideString(newParams[1]);
+
+  WideString cCc;
+  if (IsExpandedParamKnown(newParams[2]))
+    cCc = pRuntime->ToWideString(newParams[2]);
+
+  WideString cBcc;
+  if (IsExpandedParamKnown(newParams[3]))
+    cBcc = pRuntime->ToWideString(newParams[3]);
+
+  WideString cSubject;
+  if (IsExpandedParamKnown(newParams[4]))
+    cSubject = pRuntime->ToWideString(newParams[4]);
+
+  WideString cMsg;
+  if (IsExpandedParamKnown(newParams[5]))
+    cMsg = pRuntime->ToWideString(newParams[5]);
 
   pRuntime->BeginBlock();
-  CPDFSDK_FormFillEnvironment* pFormFillEnv = pRuntime->GetFormFillEnv();
-  pFormFillEnv->JS_docmailForm(nullptr, 0, bUI, cTo, cSubject, cCc, cBcc, cMsg);
+  m_pFormFillEnv->JS_docmailForm(nullptr, 0, bUI, cTo, cSubject, cCc, cBcc,
+                                 cMsg);
   pRuntime->EndBlock();
   return CJS_Result::Success();
 }
@@ -434,19 +431,38 @@ CJS_Result CJS_Document::mailForm(
   if (sTextBuf.GetLength() == 0)
     return CJS_Result::Failure(L"Bad FDF format.");
 
-  size_t nLength = params.size();
-  bool bUI = nLength > 0 ? pRuntime->ToBoolean(params[0]) : true;
-  WideString cTo = nLength > 1 ? pRuntime->ToWideString(params[1]) : L"";
-  WideString cCc = nLength > 2 ? pRuntime->ToWideString(params[2]) : L"";
-  WideString cBcc = nLength > 3 ? pRuntime->ToWideString(params[3]) : L"";
-  WideString cSubject = nLength > 4 ? pRuntime->ToWideString(params[4]) : L"";
-  WideString cMsg = nLength > 5 ? pRuntime->ToWideString(params[5]) : L"";
-  std::vector<char> mutable_buf(sTextBuf.begin(), sTextBuf.end());
+  std::vector<v8::Local<v8::Value>> newParams =
+      ExpandKeywordParams(pRuntime, params, 6, L"bUI", L"cTo", L"cCc", L"cBcc",
+                          L"cSubject", L"cMsg");
 
+  bool bUI = true;
+  if (IsExpandedParamKnown(newParams[0]))
+    bUI = pRuntime->ToBoolean(newParams[0]);
+
+  WideString cTo;
+  if (IsExpandedParamKnown(newParams[1]))
+    cTo = pRuntime->ToWideString(newParams[1]);
+
+  WideString cCc;
+  if (IsExpandedParamKnown(newParams[2]))
+    cCc = pRuntime->ToWideString(newParams[2]);
+
+  WideString cBcc;
+  if (IsExpandedParamKnown(newParams[3]))
+    cBcc = pRuntime->ToWideString(newParams[3]);
+
+  WideString cSubject;
+  if (IsExpandedParamKnown(newParams[4]))
+    cSubject = pRuntime->ToWideString(newParams[4]);
+
+  WideString cMsg;
+  if (IsExpandedParamKnown(newParams[5]))
+    cMsg = pRuntime->ToWideString(newParams[5]);
+
+  std::vector<char> mutable_buf(sTextBuf.begin(), sTextBuf.end());
   pRuntime->BeginBlock();
-  CPDFSDK_FormFillEnvironment* pFormFillEnv = pRuntime->GetFormFillEnv();
-  pFormFillEnv->JS_docmailForm(mutable_buf.data(), mutable_buf.size(), bUI, cTo,
-                               cSubject, cCc, cBcc, cMsg);
+  m_pFormFillEnv->JS_docmailForm(mutable_buf.data(), mutable_buf.size(), bUI,
+                                 cTo, cSubject, cCc, cBcc, cMsg);
   pRuntime->EndBlock();
   return CJS_Result::Success();
 }
@@ -457,51 +473,41 @@ CJS_Result CJS_Document::print(
   if (!m_pFormFillEnv)
     return CJS_Result::Failure(JSMessage::kBadObjectError);
 
-  bool bUI = true;
-  int nStart = 0;
-  int nEnd = 0;
-  bool bSilent = false;
-  bool bShrinkToFit = false;
-  bool bPrintAsImage = false;
-  bool bReverse = false;
-  bool bAnnotations = false;
-  size_t nLength = params.size();
-  if (nLength == 9) {
-    if (params[8]->IsObject()) {
-      v8::Local<v8::Object> pObj = pRuntime->ToObject(params[8]);
-      auto pPrintObj = JSGetObject<CJS_PrintParamsObj>(pObj);
-      if (pPrintObj) {
-        bUI = pPrintObj->GetUI();
-        nStart = pPrintObj->GetStart();
-        nEnd = pPrintObj->GetEnd();
-        bSilent = pPrintObj->GetSilent();
-        bShrinkToFit = pPrintObj->GetShrinkToFit();
-        bPrintAsImage = pPrintObj->GetPrintAsImage();
-        bReverse = pPrintObj->GetReverse();
-        bAnnotations = pPrintObj->GetAnnotations();
-      }
-    }
-  } else {
-    if (nLength > 0)
-      bUI = pRuntime->ToBoolean(params[0]);
-    if (nLength > 1)
-      nStart = pRuntime->ToInt32(params[1]);
-    if (nLength > 2)
-      nEnd = pRuntime->ToInt32(params[2]);
-    if (nLength > 3)
-      bSilent = pRuntime->ToBoolean(params[3]);
-    if (nLength > 4)
-      bShrinkToFit = pRuntime->ToBoolean(params[4]);
-    if (nLength > 5)
-      bPrintAsImage = pRuntime->ToBoolean(params[5]);
-    if (nLength > 6)
-      bReverse = pRuntime->ToBoolean(params[6]);
-    if (nLength > 7)
-      bAnnotations = pRuntime->ToBoolean(params[7]);
-  }
+  std::vector<v8::Local<v8::Value>> newParams = ExpandKeywordParams(
+      pRuntime, params, 8, L"bUI", L"nStart", L"nEnd", L"bSilent",
+      L"bShrinkToFit", L"bPrintAsImage", L"bReverse", L"bAnnotations");
 
-  if (!m_pFormFillEnv)
-    return CJS_Result::Failure(JSMessage::kBadObjectError);
+  bool bUI = true;
+  if (IsExpandedParamKnown(newParams[0]))
+    bUI = pRuntime->ToBoolean(newParams[0]);
+
+  int nStart = 0;
+  if (IsExpandedParamKnown(newParams[1]))
+    nStart = pRuntime->ToInt32(newParams[1]);
+
+  int nEnd = 0;
+  if (IsExpandedParamKnown(newParams[2]))
+    nEnd = pRuntime->ToInt32(newParams[2]);
+
+  bool bSilent = false;
+  if (IsExpandedParamKnown(newParams[3]))
+    bSilent = pRuntime->ToBoolean(newParams[3]);
+
+  bool bShrinkToFit = false;
+  if (IsExpandedParamKnown(newParams[4]))
+    bShrinkToFit = pRuntime->ToBoolean(newParams[4]);
+
+  bool bPrintAsImage = false;
+  if (IsExpandedParamKnown(newParams[5]))
+    bPrintAsImage = pRuntime->ToBoolean(newParams[5]);
+
+  bool bReverse = false;
+  if (IsExpandedParamKnown(newParams[6]))
+    bReverse = pRuntime->ToBoolean(newParams[6]);
+
+  bool bAnnotations = false;
+  if (IsExpandedParamKnown(newParams[7]))
+    bAnnotations = pRuntime->ToBoolean(newParams[7]);
 
   m_pFormFillEnv->JS_docprint(bUI, nStart, nEnd, bSilent, bShrinkToFit,
                               bPrintAsImage, bReverse, bAnnotations);
@@ -1362,10 +1368,6 @@ CJS_Result CJS_Document::getPageNumWords(
 CJS_Result CJS_Document::getPrintParams(
     CJS_Runtime* pRuntime,
     const std::vector<v8::Local<v8::Value>>& params) {
-  v8::Local<v8::Object> pRetObj = pRuntime->NewFXJSBoundObject(
-      CJS_PrintParamsObj::GetObjDefnID(), FXJSOBJTYPE_DYNAMIC);
-  if (pRetObj.IsEmpty())
-    return CJS_Result::Failure(JSMessage::kBadObjectError);
   return CJS_Result::Failure(JSMessage::kNotSupportedError);
 }
 
