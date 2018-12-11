17e9c8d856c077fef72cec0656eab4874c54ae8b
Tom Sepez <tsepez@chromium.org>
2018-12-11 12:58:07 -0800

    De-duplicate rows in element-attributes table
    Change-Id: Iab4cc2ecab4b5c02714012414319f9b71d339a71

--- xfa/fxfa/parser/cxfa_document_parser.cpp
+++ xfa/fxfa/parser/cxfa_document_parser.cpp
@@ -773,7 +773,7 @@ CXFA_Node* CXFA_DocumentParser::NormalLoader(CXFA_Node* pXFANode,
       case FX_XMLNODE_Element: {
         CFX_XMLElement* pXMLElement = static_cast<CFX_XMLElement*>(pXMLChild);
         WideString wsTagName = pXMLElement->GetLocalTagName();
-        XFA_Element eType = CXFA_Node::NameToElement(wsTagName);
+        XFA_Element eType = XFA_GetElementByName(wsTagName);
         if (eType == XFA_Element::Unknown)
           continue;
 
@@ -800,17 +800,17 @@ CXFA_Node* CXFA_DocumentParser::NormalLoader(CXFA_Node* pXFANode,
           if (wsAttrName.EqualsASCII("nil") && it.second.EqualsASCII("true"))
             IsNeedValue = false;
 
-          XFA_Attribute attr =
-              CXFA_Node::NameToAttribute(wsAttrName.AsStringView());
-          if (attr == XFA_Attribute::Unknown)
+          Optional<XFA_ATTRIBUTEINFO> attr =
+              XFA_GetAttributeByName(wsAttrName.AsStringView());
+          if (!attr.has_value())
             continue;
 
-          if (!bUseAttribute && attr != XFA_Attribute::Name &&
-              attr != XFA_Attribute::Save) {
+          if (!bUseAttribute && attr.value().attribute != XFA_Attribute::Name &&
+              attr.value().attribute != XFA_Attribute::Save) {
             continue;
           }
-          pXFAChild->JSObject()->SetAttribute(attr, it.second.AsStringView(),
-                                              false);
+          pXFAChild->JSObject()->SetAttribute(attr.value().attribute,
+                                              it.second.AsStringView(), false);
         }
         pXFANode->InsertChild(pXFAChild, nullptr);
         if (eType == XFA_Element::Validate || eType == XFA_Element::Locale) {
--- xfa/fxfa/parser/xfa_basic_data_unittest.cpp
+++ xfa/fxfa/parser/xfa_basic_data_unittest.cpp
@@ -4,13 +4,51 @@
 
 #include "xfa/fxfa/parser/xfa_basic_data.h"
 
+#include <utility>
+
 #include "testing/gtest/include/gtest/gtest.h"
 
-TEST(XFABasicDataTest, GetAttributeValueByName) {
-  EXPECT_FALSE(XFA_GetAttributeValueByName(L"").has_value());
-  EXPECT_FALSE(XFA_GetAttributeValueByName(L"nonesuch").has_value());
+namespace {
 
-  Optional<XFA_AttributeValue> result = XFA_GetAttributeValueByName(L"*");
+void DoElementAttrTestCase(XFA_Element elem,
+                           XFA_Attribute attr,
+                           std::pair<XFA_Element, XFA_Attribute>* so_far) {
+  auto curr = std::make_pair(elem, attr);
+  EXPECT_LT(*so_far, curr) << " for " << static_cast<int>(elem) << ", "
+                           << static_cast<int>(attr);
+  *so_far = curr;
+}
+
+}  // namespace
+
+TEST(XFABasicDataTest, GetAttributeByName) {
+  Optional<XFA_ATTRIBUTEINFO> result = XFA_GetAttributeByName(L"");
+  EXPECT_FALSE(result.has_value());
+
+  result = XFA_GetAttributeByName(L"nonesuch");
+  EXPECT_FALSE(result.has_value());
+
+  result = XFA_GetAttributeByName(L"h");
+  ASSERT_TRUE(result.has_value());
+  EXPECT_EQ(XFA_Attribute::H, result.value().attribute);
+
+  result = XFA_GetAttributeByName(L"short");
+  ASSERT_TRUE(result.has_value());
+  EXPECT_EQ(XFA_Attribute::Short, result.value().attribute);
+
+  result = XFA_GetAttributeByName(L"decipherOnly");
+  ASSERT_TRUE(result.has_value());
+  EXPECT_EQ(XFA_Attribute::DecipherOnly, result.value().attribute);
+}
+
+TEST(XFABasicDataTest, GetAttributeValueByName) {
+  Optional<XFA_AttributeValue> result = XFA_GetAttributeValueByName(L"");
+  EXPECT_FALSE(result.has_value());
+
+  result = XFA_GetAttributeValueByName(L"nonesuch");
+  EXPECT_FALSE(result.has_value());
+
+  result = XFA_GetAttributeValueByName(L"*");
   ASSERT_TRUE(result.has_value());
   EXPECT_EQ(XFA_AttributeValue::Asterisk, result.value());
 
@@ -21,4 +59,13 @@ TEST(XFABasicDataTest, GetAttributeValueByName) {
   result = XFA_GetAttributeValueByName(L"lowered");
   ASSERT_TRUE(result.has_value());
   EXPECT_EQ(XFA_AttributeValue::Lowered, result.value());
+}
+
+TEST(XFABasicDataText, ElementAttributeOrder) {
+  std::pair<XFA_Element, XFA_Attribute> so_far = {};
+#undef ELEM_ATTR____
+#define ELEM_ATTR____(a, b, c) \
+  DoElementAttrTestCase(XFA_Element::a, XFA_Attribute::b, &so_far);
+#include "xfa/fxfa/parser/element_attributes.inc"
+#undef ELEM_ATTR____
 }
