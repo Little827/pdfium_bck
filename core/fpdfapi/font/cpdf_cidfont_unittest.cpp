a5f8df4ddf7c838d5cc6355aef90c011a9228522
Lei Zhang <thestig@chromium.org>
2019-01-10 15:42:34 -0800

    Remove a NOTREACHED() in CPDF_Font::GetAdobeCharName().
    It is reachable. Add a unit test to exercise this code path.
    
    BUG=chromium:920636
    
    Change-Id: I57c9102fe215ae48fc46d496bae4efc399629e0b

---
+++ core/fpdfapi/font/cpdf_cidfont_unittest.cpp
@@ -0,0 +1,54 @@
+// Copyright 2019 PDFium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/fpdfapi/font/cpdf_cidfont.h"
+
+#include <utility>
+
+#include "core/fpdfapi/cpdf_modulemgr.h"
+#include "core/fpdfapi/parser/cpdf_array.h"
+#include "core/fpdfapi/parser/cpdf_dictionary.h"
+#include "core/fpdfapi/parser/cpdf_document.h"
+#include "core/fpdfapi/parser/cpdf_name.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+class CPDF_CIDFontTest : public testing::Test {
+ protected:
+  void SetUp() override { CPDF_ModuleMgr::Get()->Init(); }
+
+  void TearDown() override { CPDF_ModuleMgr::Destroy(); }
+};
+
+TEST_F(CPDF_CIDFontTest, BUG_920636) {
+  CPDF_Document doc;
+  CPDF_Dictionary font_dict;
+  font_dict.SetNewFor<CPDF_Name>("Encoding", "Identity−H");
+
+  {
+    auto descendant_fonts = pdfium::MakeUnique<CPDF_Array>();
+    {
+      auto descendant_font = pdfium::MakeUnique<CPDF_Dictionary>();
+      descendant_font->SetNewFor<CPDF_Name>("BaseFont", "CourierStd");
+      descendant_fonts->Add(std::move(descendant_font));
+    }
+    font_dict.SetFor("DescendantFonts", std::move(descendant_fonts));
+  }
+
+  CPDF_CIDFont font(&doc, &font_dict);
+  ASSERT_TRUE(font.Load());
+
+  // It would be nice if we can test more values here. However, the glyph
+  // indices are sometimes machine dependent.
+  static constexpr uint32_t kCharcodes[] = {0, 256, 34661};
+#if _FX_PLATFORM_ == _FX_PLATFORM_WINDOWS_
+  static constexpr int kGlyphs[] = {31, 287, 34692};
+#else
+  static constexpr int kGlyphs[] = {31, 287, 34692};
+#endif
+  static_assert(FX_ArraySize(kCharcodes) == FX_ArraySize(kGlyphs),
+                "size mismatch");
+
+  for (size_t i = 0; i < FX_ArraySize(kCharcodes); ++i)
+    EXPECT_EQ(kGlyphs[i], font.GlyphFromCharCode(kCharcodes[i], nullptr));
+}
