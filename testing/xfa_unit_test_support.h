b6095888de35c7c4e2d8d26130d6063bf21b2e83
Lei Zhang <thestig@chromium.org>
2019-02-04 17:42:37 -0800

    Add a unit_test_support target.
    Split out XFA-only unit test code into xfa_unit_test_support.h. Add a
    target for it since it is for unit tests only but there is no other
    place to put it.
    
    Change-Id: I0a6e291a4827b754236ddd8040528c0e7489d3a4

---
+++ testing/xfa_unit_test_support.h
@@ -0,0 +1,16 @@
+// Copyright 2019 PDFium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef TESTING_XFA_UNIT_TEST_SUPPORT_H_
+#define TESTING_XFA_UNIT_TEST_SUPPORT_H_
+
+#ifndef PDF_ENABLE_XFA
+#error "XFA must be enabled"
+#endif
+
+class CFGAS_FontMgr;
+
+CFGAS_FontMgr* GetGlobalFontManager();
+
+#endif  // TESTING_XFA_UNIT_TEST_SUPPORT_H_
