b4327e5ab52b18203bc4ae65eb4b659db80b8862
Felix Kauselmann <licorn@gmail.com>
2019-02-11 21:03:51 +0100

    v8_initializer: Add cstring header
    Using strlen? #include <cstring>!
    
    Change-Id: Ie9f6ec80faba37708dc3c2b2302df1e4ab1a1b21

--- testing/v8_initializer.cpp
+++ testing/v8_initializer.cpp
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <cstring>
+
 #include "testing/v8_initializer.h"
 
 #include "public/fpdfview.h"
