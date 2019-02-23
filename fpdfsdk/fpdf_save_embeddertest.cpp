103b34d8f0af055b7f32e0b3329388dcd6d5d822
Lei Zhang <thestig@chromium.org>
2019-02-22 23:26:43 -0800

    Add a test for saving a full XFA document.
    The new test some some additional checks compared to the one it copied
    from. Make those same changes in the original.
    
    Change-Id: I1ef008345c28488585bb91b6d2182c2ed567b1fa

--- fpdfsdk/fpdf_save_embeddertest.cpp
+++ fpdfsdk/fpdf_save_embeddertest.cpp
@@ -60,10 +60,11 @@ TEST_F(FPDFSaveEmbedderTest, SaveCopiedDoc) {
 }
 
 TEST_F(FPDFSaveEmbedderTest, SaveLinearizedDoc) {
-  const int kPageCount = 3;
+  static constexpr int kPageCount = 3;
   std::string original_md5[kPageCount];
 
-  EXPECT_TRUE(OpenDocument("linearized.pdf"));
+  ASSERT_TRUE(OpenDocument("linearized.pdf"));
+  ASSERT_EQ(kPageCount, FPDF_GetPageCount(document()));
   for (int i = 0; i < kPageCount; ++i) {
     FPDF_PAGE page = LoadPage(i);
     ASSERT_TRUE(page);
@@ -74,18 +75,22 @@ TEST_F(FPDFSaveEmbedderTest, SaveLinearizedDoc) {
     UnloadPage(page);
   }
 
-  EXPECT_TRUE(FPDF_SaveAsCopy(document(), this, 0));
+  ASSERT_TRUE(FPDF_SaveAsCopy(document(), this, 0));
   EXPECT_THAT(GetString(), testing::StartsWith("%PDF-1.6\r\n"));
   EXPECT_THAT(GetString(), testing::HasSubstr("/Root "));
   EXPECT_THAT(GetString(), testing::HasSubstr("/Info "));
   EXPECT_EQ(8219u, GetString().length());
 
   // Make sure new document renders the same as the old one.
-  ASSERT_TRUE(OpenSavedDocument());
+  FPDF_DOCUMENT saved_doc = OpenSavedDocument();
+  ASSERT_TRUE(saved_doc);
+  ASSERT_EQ(kPageCount, FPDF_GetPageCount(saved_doc));
   for (int i = 0; i < kPageCount; ++i) {
     FPDF_PAGE page = LoadSavedPage(i);
     ASSERT_TRUE(page);
     ScopedFPDFBitmap bitmap = RenderSavedPage(page);
+    EXPECT_EQ(612, FPDFBitmap_GetWidth(bitmap.get()));
+    EXPECT_EQ(792, FPDFBitmap_GetHeight(bitmap.get()));
     EXPECT_EQ(original_md5[i], HashBitmap(bitmap.get()));
     CloseSavedPage(page);
   }
@@ -104,4 +109,43 @@ TEST_F(FPDFSaveEmbedderTest, BUG_905142) {
   EXPECT_TRUE(OpenDocument("bug_905142.pdf"));
   EXPECT_TRUE(FPDF_SaveAsCopy(document(), this, 0));
   EXPECT_THAT(GetString(), testing::HasSubstr("/Length 0"));
+}
+
+TEST_F(FPDFSaveEmbedderTest, SimpleXFA) {
+  static constexpr int kPageCount = 2;
+  std::string original_md5[kPageCount];
+
+  ASSERT_TRUE(OpenDocument("simple_xfa.pdf"));
+  ASSERT_EQ(kPageCount, FPDF_GetPageCount(document()));
+  for (int i = 0; i < kPageCount; ++i) {
+    FPDF_PAGE page = LoadPage(i);
+    ASSERT_TRUE(page);
+    ScopedFPDFBitmap bitmap = RenderLoadedPage(page);
+    EXPECT_EQ(612, FPDFBitmap_GetWidth(bitmap.get()));
+    EXPECT_EQ(792, FPDFBitmap_GetHeight(bitmap.get()));
+    original_md5[i] = HashBitmap(bitmap.get());
+    UnloadPage(page);
+  }
+
+  EXPECT_TRUE(FPDF_SaveAsCopy(document(), this, 0));
+  EXPECT_THAT(GetString(), testing::StartsWith("%PDF-1.7\r\n"));
+  EXPECT_THAT(GetString(), testing::HasSubstr("/Root "));
+  EXPECT_THAT(GetString(), testing::HasSubstr("/Info "));
+  EXPECT_GT(GetString().length(), 80000u);
+
+  // Make sure new document renders the same as the old one.
+  FPDF_DOCUMENT saved_doc = OpenSavedDocument();
+  ASSERT_TRUE(saved_doc);
+  ASSERT_EQ(kPageCount, FPDF_GetPageCount(saved_doc));
+  for (int i = 0; i < kPageCount; ++i) {
+    FPDF_PAGE page = LoadSavedPage(i);
+    ASSERT_TRUE(page);
+    ScopedFPDFBitmap bitmap = RenderSavedPage(page);
+    EXPECT_EQ(612, FPDFBitmap_GetWidth(bitmap.get()));
+    EXPECT_EQ(792, FPDFBitmap_GetHeight(bitmap.get()));
+    // TODO(thestig): Figure out why this fails.
+    // EXPECT_EQ(original_md5[i], HashBitmap(bitmap.get()));
+    CloseSavedPage(page);
+  }
+  CloseSavedDocument();
 }
