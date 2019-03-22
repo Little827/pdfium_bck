bd0fc8518ad455d84f5a026ed25cfe5116223b12
Lei Zhang <thestig@chromium.org>
2019-03-22 14:25:27 -0700

    Use early returns in CXFA_LayoutPageMgr::ProcessBookendLeaderOrTrailer().
    Also remove a check that is always false.
    
    Change-Id: I20564e2e80f014f3de36475449caa3659836d42a

--- xfa/fxfa/layout/cxfa_layoutpagemgr.cpp
+++ xfa/fxfa/layout/cxfa_layoutpagemgr.cpp
@@ -888,24 +888,20 @@ bool CXFA_LayoutPageMgr::ProcessBookendLeaderOrTrailer(
     CXFA_Node*& pBookendAppendNode) {
   CXFA_Node* pLeaderTemplate = nullptr;
   CXFA_Node* pFormNode = pBookendNode->GetContainerParent();
-  if (ResolveBookendLeaderOrTrailer(pBookendNode, bLeader, pLeaderTemplate)) {
-    CXFA_Document* pDocument = pBookendNode->GetDocument();
-    CXFA_Node* pDataScope = nullptr;
-    if (pLeaderTemplate) {
-      if (!pDataScope)
-        pDataScope = XFA_DataMerge_FindDataScope(pFormNode);
+  if (!ResolveBookendLeaderOrTrailer(pBookendNode, bLeader, pLeaderTemplate))
+    return false;
 
-      pBookendAppendNode = pDocument->DataMerge_CopyContainer(
-          pLeaderTemplate, pFormNode, pDataScope, true, true, true);
-      if (!pBookendAppendNode)
-        return false;
+  ASSERT(pLeaderTemplate);
+  CXFA_Document* pDocument = pBookendNode->GetDocument();
+  CXFA_Node* pDataScope = XFA_DataMerge_FindDataScope(pFormNode);
+  pBookendAppendNode = pDocument->DataMerge_CopyContainer(
+      pLeaderTemplate, pFormNode, pDataScope, true, true, true);
+  if (!pBookendAppendNode)
+    return false;
 
-      pDocument->DataMerge_UpdateBindingRelations(pBookendAppendNode);
-      SetLayoutGeneratedNodeFlag(pBookendAppendNode);
-      return true;
-    }
-  }
-  return false;
+  pDocument->DataMerge_UpdateBindingRelations(pBookendAppendNode);
+  SetLayoutGeneratedNodeFlag(pBookendAppendNode);
+  return true;
 }
 
 CXFA_Node* CXFA_LayoutPageMgr::BreakOverflow(CXFA_Node* pOverflowNode,
