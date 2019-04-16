659157a0520ba071fbdeb0fc106f34ffef3bcf7c
Tom Sepez <tsepez@chromium.org>
2019-04-16 14:10:40 -0700

    Check for possibility of inf value from FXSYS_wcstof()
    Bug: chromium:951712
    Change-Id: I9a4572aa9879e2c4ba374e78d37d9a959752318f

--- xfa/fxfa/parser/cxfa_measurement.cpp
+++ xfa/fxfa/parser/cxfa_measurement.cpp
@@ -32,8 +32,7 @@ CXFA_Measurement::CXFA_Measurement(float fValue, XFA_Unit eUnit) {
 
 void CXFA_Measurement::SetString(WideStringView wsMeasure) {
   if (wsMeasure.IsEmpty()) {
-    m_fValue = 0;
-    m_eUnit = XFA_Unit::Unknown;
+    Set(0, XFA_Unit::Unknown);
     return;
   }
 
@@ -43,6 +42,9 @@ void CXFA_Measurement::SetString(WideStringView wsMeasure) {
   int32_t iUsedLen = 0;
   float fValue = FXSYS_wcstof(wsMeasure.unterminated_c_str(),
                               wsMeasure.GetLength(), &iUsedLen);
+  if (!std::isfinite(fValue))
+    fValue = 0.0f;
+
   wsMeasure = wsMeasure.Right(wsMeasure.GetLength() - iUsedLen);
   Set(fValue, GetUnitFromString(wsMeasure));
 }
