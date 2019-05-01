b79d9d2caf9c7350a515e82d23421cbf02ada310
Patrik Weiskircher <patrik@pspdfkit.com>
2019-05-01 16:26:12 -0400

    Provide correct state to pattern rendering.
    This reverts commit c1aefb717810667d6889990d76798a481acce148.
    
    Original change's description:
    > Revert "Provide correct state to pattern rendering."
    >
    > This reverts commit f188aca283d6dceb874bbe5e20d8003e6bbe184f.
    >
    > Reason for revert: Has memory safety issues.
    >
    > Original change's description:
    > > Provide correct state to pattern rendering.
    > >
    > > According to 8.7.3.1 in the PDF 2.0 spec, a tiling pattern should
    > > install the graphics state that was in effect at the beginning of the
    > > pattern’s parent content stream.
    > >
    > > This means that when loading the pattern we have to supply the current
    > > graphics state and we also can’t keep the pattern loaded past the
    > > current usage.
    > >
    > > Bug: pdfium:1286
    > > Change-Id: Id9f609a52803fec29cb42be2ef9bcda6ff31ce3c
    > > Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/53330
    > > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > > Commit-Queue: Lei Zhang <thestig@chromium.org>
    >
    > TBR=thestig@chromium.org,patrik@pspdfkit.com
    >
    > Change-Id: I89d16a263a53e0298fe5cf812f16da14d6bbbf2d
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: pdfium:1286,chromium:957814
    > Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/53711
    > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > Commit-Queue: Lei Zhang <thestig@chromium.org>
    
    Bug: pdfium:1286, chromium:957814
    Change-Id: I013dd59587caa9be32963c51934f5030ef1e72fb

--- core/fpdfapi/page/cpdf_tilingpattern.cpp
+++ core/fpdfapi/page/cpdf_tilingpattern.cpp
@@ -6,7 +6,9 @@
 
 #include "core/fpdfapi/page/cpdf_tilingpattern.h"
 
+#include "core/fpdfapi/page/cpdf_allstates.h"
 #include "core/fpdfapi/page/cpdf_form.h"
+#include "core/fpdfapi/page/cpdf_pageobject.h"
 #include "core/fpdfapi/parser/cpdf_dictionary.h"
 #include "core/fpdfapi/parser/cpdf_object.h"
 #include "core/fpdfapi/parser/cpdf_stream.h"
@@ -31,13 +33,10 @@ CPDF_ShadingPattern* CPDF_TilingPattern::AsShadingPattern() {
   return nullptr;
 }
 
-bool CPDF_TilingPattern::Load() {
-  if (m_pForm)
-    return true;
-
+std::unique_ptr<CPDF_Form> CPDF_TilingPattern::Load(CPDF_PageObject* pPageObj) {
   const CPDF_Dictionary* pDict = pattern_obj()->GetDict();
   if (!pDict)
-    return false;
+    return nullptr;
 
   m_bColored = pDict->GetIntegerFor("PaintType") == 1;
   m_XStep = static_cast<float>(fabs(pDict->GetNumberFor("XStep")));
@@ -45,11 +44,20 @@ bool CPDF_TilingPattern::Load() {
 
   CPDF_Stream* pStream = pattern_obj()->AsStream();
   if (!pStream)
-    return false;
+    return nullptr;
 
   const CFX_Matrix& matrix = parent_matrix();
-  m_pForm = pdfium::MakeUnique<CPDF_Form>(document(), nullptr, pStream);
-  m_pForm->ParseContent(nullptr, &matrix, nullptr, nullptr);
+  std::unique_ptr<CPDF_Form> form =
+      pdfium::MakeUnique<CPDF_Form>(document(), nullptr, pStream);
+
+  CPDF_AllStates allStates;
+  allStates.CopyStates(*pPageObj);
+  // Reset color state to default. The current pattern is set in the color
+  // state and this would lead to recursion problems. We also reset the
+  // clip path as it isn't relevant for pattern rendering either.
+  allStates.m_ColorState.SetDefault();
+  allStates.m_ClipPath.SetNull();
+  form->ParseContent(&allStates, &matrix, nullptr, nullptr);
   m_BBox = pDict->GetRectFor("BBox");
-  return true;
+  return form;
 }
