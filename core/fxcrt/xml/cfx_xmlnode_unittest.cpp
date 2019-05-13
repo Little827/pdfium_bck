a446c551c378a602e8f28004fbcb314aa27f067a
Tom Sepez <tsepez@chromium.org>
2019-05-13 15:59:41 -0700

    Make CFX_XMLNode inherit from TreeNode<>.
    Simplify things by making GetNthChild() interpret its index argument
    in the same manner as existing code.
    
    Change-Id: Ia80844b593e3ad03ebb0cd466c0b4cdf741cf192

--- core/fxcrt/xml/cfx_xmlnode_unittest.cpp
+++ core/fxcrt/xml/cfx_xmlnode_unittest.cpp
@@ -10,8 +10,8 @@ TEST(CFX_XMLNodeTest, GetParent) {
   CFX_XMLElement node2(L"node2");
   CFX_XMLElement node3(L"node3");
 
-  node1.AppendChild(&node2);
-  node2.AppendChild(&node3);
+  node1.AppendLastChild(&node2);
+  node2.AppendLastChild(&node3);
 
   EXPECT_EQ(nullptr, node1.GetParent());
   EXPECT_EQ(&node1, node2.GetParent());
@@ -23,8 +23,8 @@ TEST(CFX_XMLNodeTest, GetRoot) {
   CFX_XMLElement node2(L"node2");
   CFX_XMLElement node3(L"node3");
 
-  node1.AppendChild(&node2);
-  node2.AppendChild(&node3);
+  node1.AppendLastChild(&node2);
+  node2.AppendLastChild(&node3);
 
   EXPECT_EQ(&node1, node1.GetRoot());
   EXPECT_EQ(&node1, node2.GetRoot());
@@ -37,9 +37,9 @@ TEST(CFX_XMLNodeTest, GetChildren) {
   CFX_XMLElement node3(L"node3");
   CFX_XMLElement node4(L"node4");
 
-  node1.AppendChild(&node2);
-  node1.AppendChild(&node4);
-  node2.AppendChild(&node3);
+  node1.AppendLastChild(&node2);
+  node1.AppendLastChild(&node4);
+  node2.AppendLastChild(&node3);
 
   EXPECT_EQ(&node2, node1.GetFirstChild());
 
@@ -59,18 +59,18 @@ TEST(CFX_XMLNodeTest, DeleteChildren) {
   CFX_XMLElement node3(L"node3");
   CFX_XMLElement node4(L"node4");
 
-  node1.AppendChild(&node2);
-  node1.AppendChild(&node4);
-  node2.AppendChild(&node3);
+  node1.AppendLastChild(&node2);
+  node1.AppendLastChild(&node4);
+  node2.AppendLastChild(&node3);
 
-  node1.DeleteChildren();
+  node1.RemoveAllChildren();
   EXPECT_TRUE(node1.GetFirstChild() == nullptr);
   EXPECT_TRUE(node2.GetParent() == nullptr);
   EXPECT_TRUE(node4.GetParent() == nullptr);
 
   // node2 and node4 should no longer be siblings.
   EXPECT_TRUE(node2.GetNextSibling() == nullptr);
-  EXPECT_TRUE(node4.GetPrevSiblingForTesting() == nullptr);
+  EXPECT_TRUE(node4.GetPrevSibling() == nullptr);
 
   // Deleting children doesn't change deleted substructure
   EXPECT_EQ(&node3, node2.GetFirstChild());
@@ -82,8 +82,8 @@ TEST(CFX_XMLNodeTest, AddingChildren) {
   CFX_XMLElement node2(L"node2");
   CFX_XMLElement node3(L"node3");
 
-  node1.AppendChild(&node2);
-  node1.AppendChild(&node3);
+  node1.AppendLastChild(&node2);
+  node1.AppendLastChild(&node3);
 
   EXPECT_EQ(&node1, node2.GetParent());
   EXPECT_EQ(&node1, node3.GetParent());
@@ -114,8 +114,8 @@ TEST(CFX_XMLNodeTest, DuplicateInsertOfNode) {
   CFX_XMLElement node1(L"node");
   CFX_XMLElement node2(L"node2");
 
-  node1.AppendChild(&node2);
-  EXPECT_DEATH(node1.AppendChild(&node2), "");
+  node1.AppendLastChild(&node2);
+  EXPECT_DEATH(node1.AppendLastChild(&node2), "");
 }
 #endif  // NDEBUG
 
@@ -125,23 +125,23 @@ TEST(CFX_XMLNodeTest, RemovingMiddleChild) {
   CFX_XMLElement node3(L"node3");
   CFX_XMLElement node4(L"node4");
 
-  node1.AppendChild(&node2);
-  node1.AppendChild(&node3);
-  node1.AppendChild(&node4);
+  node1.AppendLastChild(&node2);
+  node1.AppendLastChild(&node3);
+  node1.AppendLastChild(&node4);
 
   EXPECT_EQ(&node2, node1.GetFirstChild());
   EXPECT_EQ(&node3, node2.GetNextSibling());
   EXPECT_EQ(&node4, node3.GetNextSibling());
   EXPECT_TRUE(node4.GetNextSibling() == nullptr);
 
-  node1.RemoveChildNode(&node3);
+  node1.RemoveChild(&node3);
   EXPECT_TRUE(node3.GetParent() == nullptr);
   EXPECT_TRUE(node3.GetNextSibling() == nullptr);
-  EXPECT_TRUE(node3.GetPrevSiblingForTesting() == nullptr);
+  EXPECT_TRUE(node3.GetPrevSibling() == nullptr);
 
   EXPECT_EQ(&node2, node1.GetFirstChild());
   EXPECT_EQ(&node4, node2.GetNextSibling());
-  EXPECT_EQ(&node2, node4.GetPrevSiblingForTesting());
+  EXPECT_EQ(&node2, node4.GetPrevSibling());
   EXPECT_TRUE(node4.GetNextSibling() == nullptr);
 }
 
@@ -151,22 +151,22 @@ TEST(CFX_XMLNodeTest, RemovingFirstChild) {
   CFX_XMLElement node3(L"node3");
   CFX_XMLElement node4(L"node4");
 
-  node1.AppendChild(&node2);
-  node1.AppendChild(&node3);
-  node1.AppendChild(&node4);
+  node1.AppendLastChild(&node2);
+  node1.AppendLastChild(&node3);
+  node1.AppendLastChild(&node4);
 
   EXPECT_EQ(&node2, node1.GetFirstChild());
   EXPECT_EQ(&node3, node2.GetNextSibling());
   EXPECT_EQ(&node4, node3.GetNextSibling());
   EXPECT_TRUE(node4.GetNextSibling() == nullptr);
 
-  node1.RemoveChildNode(&node2);
+  node1.RemoveChild(&node2);
   EXPECT_TRUE(node2.GetParent() == nullptr);
   EXPECT_TRUE(node2.GetNextSibling() == nullptr);
-  EXPECT_TRUE(node2.GetPrevSiblingForTesting() == nullptr);
+  EXPECT_TRUE(node2.GetPrevSibling() == nullptr);
 
   EXPECT_EQ(&node3, node1.GetFirstChild());
-  EXPECT_TRUE(node3.GetPrevSiblingForTesting() == nullptr);
+  EXPECT_TRUE(node3.GetPrevSibling() == nullptr);
   EXPECT_EQ(&node4, node3.GetNextSibling());
   EXPECT_TRUE(node4.GetNextSibling() == nullptr);
 }
@@ -177,19 +177,19 @@ TEST(CFX_XMLNodeTest, RemovingLastChild) {
   CFX_XMLElement node3(L"node3");
   CFX_XMLElement node4(L"node4");
 
-  node1.AppendChild(&node2);
-  node1.AppendChild(&node3);
-  node1.AppendChild(&node4);
+  node1.AppendLastChild(&node2);
+  node1.AppendLastChild(&node3);
+  node1.AppendLastChild(&node4);
 
   EXPECT_EQ(&node2, node1.GetFirstChild());
   EXPECT_EQ(&node3, node2.GetNextSibling());
   EXPECT_EQ(&node4, node3.GetNextSibling());
   EXPECT_TRUE(node4.GetNextSibling() == nullptr);
 
-  node1.RemoveChildNode(&node4);
+  node1.RemoveChild(&node4);
   EXPECT_TRUE(node4.GetParent() == nullptr);
   EXPECT_TRUE(node4.GetNextSibling() == nullptr);
-  EXPECT_TRUE(node4.GetPrevSiblingForTesting() == nullptr);
+  EXPECT_TRUE(node4.GetPrevSibling() == nullptr);
 
   EXPECT_EQ(&node2, node1.GetFirstChild());
   EXPECT_EQ(&node3, node2.GetNextSibling());
@@ -200,17 +200,17 @@ TEST(CFX_XMLNodeTest, RemovingOnlyChild) {
   CFX_XMLElement node1(L"node1");
   CFX_XMLElement node2(L"node2");
 
-  node1.AppendChild(&node2);
+  node1.AppendLastChild(&node2);
 
   EXPECT_EQ(&node2, node1.GetFirstChild());
   EXPECT_TRUE(node2.GetNextSibling() == nullptr);
 
-  node1.RemoveChildNode(&node2);
+  node1.RemoveChild(&node2);
   EXPECT_TRUE(node2.GetParent() == nullptr);
 
   EXPECT_TRUE(node1.GetFirstChild() == nullptr);
   EXPECT_TRUE(node2.GetNextSibling() == nullptr);
-  EXPECT_TRUE(node2.GetPrevSiblingForTesting() == nullptr);
+  EXPECT_TRUE(node2.GetPrevSibling() == nullptr);
 }
 
 TEST(CFX_XMLNodeTest, RemoveMissingChild) {
@@ -218,8 +218,8 @@ TEST(CFX_XMLNodeTest, RemoveMissingChild) {
   CFX_XMLElement node2(L"node2");
   CFX_XMLElement node3(L"node3");
 
-  node1.AppendChild(&node2);
-  node1.RemoveChildNode(&node3);
+  node1.AppendLastChild(&node2);
+  node1.RemoveChild(&node3);
 
   EXPECT_TRUE(node3.GetParent() == nullptr);
   EXPECT_EQ(&node2, node1.GetFirstChild());
