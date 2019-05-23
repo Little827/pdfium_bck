a408c587b95cda27e340cc8788fb5a1f134a0169
Tom Sepez <tsepez@chromium.org>
2019-05-23 15:30:04 -0700

    explicit
    Split off CFX_Module::Create() from CFX_GEModule::Get()
    
    
    mac
    
    
    fix take 2
    
    
    fix win android and apple
    
    
    fix
    
    
    naming consistency
    
    
    finish
    
    
    Introduce CFX_GEModule::PlatformIface pure virtual class
    
    Change-Id: I0481b1883c2fbe41396e4c60ffcedffd19dec3b5

--- core/fxge/cfx_gemodule.h
+++ core/fxge/cfx_gemodule.h
@@ -9,31 +9,40 @@
 
 #include <memory>
 
+#include "core/fxcrt/unowned_ptr.h"
+
 class CFX_FontCache;
 class CFX_FontMgr;
 
 class CFX_GEModule {
  public:
+  class PlatformIface {
+   public:
+    static std::unique_ptr<PlatformIface> Create();
+    virtual ~PlatformIface() {}
+
+    virtual void Init() = 0;
+  };
+
+  static CFX_GEModule* Create(const char** pUserFontPaths);
   static CFX_GEModule* Get();
   static void Destroy();
 
-  void Init(const char** pUserFontPaths);
-  CFX_FontCache* GetFontCache();
-  CFX_FontMgr* GetFontMgr() { return m_pFontMgr.get(); }
-
-  void* GetPlatformData() { return m_pPlatformData; }
+  CFX_FontCache* GetFontCache() const { return m_pFontCache.get(); }
+  CFX_FontMgr* GetFontMgr() const { return m_pFontMgr.get(); }
+  PlatformIface* GetPlatform() const { return m_pPlatform.get(); }
+  const char** GetUserFontPaths() const { return m_pUserFontPaths; }
 
  private:
-  CFX_GEModule();
+  explicit CFX_GEModule(const char** pUserFontPaths);
   ~CFX_GEModule();
 
-  void InitPlatform();
-  void DestroyPlatform();
+  void Init();
 
   std::unique_ptr<CFX_FontCache> m_pFontCache;
-  std::unique_ptr<CFX_FontMgr> m_pFontMgr;
-  void* m_pPlatformData;
-  const char** m_pUserFontPaths;
+  std::unique_ptr<CFX_FontMgr> const m_pFontMgr;
+  std::unique_ptr<PlatformIface> const m_pPlatform;
+  const char** const m_pUserFontPaths;
 };
 
 #endif  // CORE_FXGE_CFX_GEMODULE_H_
