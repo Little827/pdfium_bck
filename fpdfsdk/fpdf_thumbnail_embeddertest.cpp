f1f748fe22c038f2a42134c68e8a3b64bb242e44
Jeremy Chinsen <chinsenj@google.com>
2019-06-03 14:50:27 -0700

    Create FPDF_Thumbnail API and refactor raw stream retrieval
    Initially FPDF_Thumbnail API contains:
    - FPDFPage_GetThumbnailStreamSize
    - FPDFPage_GetDecodedThumbnailDataFromPage
    - FPDFPage_GetRawThumbnailDataFromPage
    
    These allow getting the stream data for a thumbnail from a
    page.
    
    Also refactors raw stream retrieval previously implemented in
    fpdf_editimg.cpp to a function in cpdfsdk_helpers.cpp
    
    Bug: pdfium:1312
    Change-Id: I8363ef52651986fb6bf959b596448f58c3c8b86c

---
+++ fpdfsdk/fpdf_thumbnail_embeddertest.cpp
@@ -0,0 +1,110 @@
+// Copyright 2019 PDFium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "public/fpdf_thumbnail.h"
+#include "public/fpdfview.h"
+#include "testing/embedder_test.h"
+#include "testing/utils/hash.h"
+
+class FPDFThumbnailEmbedderTest : public EmbedderTest {};
+
+TEST_F(FPDFThumbnailEmbedderTest, GetDecodedThumbnailDataFromPage) {
+  // Open a file with thumbnails
+  ASSERT_TRUE(OpenDocument("simple_thumbnail.pdf"));
+
+  {
+    FPDF_PAGE page = LoadPage(0);
+    ASSERT_TRUE(page);
+
+    const unsigned long kBufLen = 7524u;
+    uint8_t buffer[kBufLen];
+    const char kHashedDecodedData[] = "033864db5248bf30bc3a0c4a9f61535e";
+
+    EXPECT_EQ(7524u,
+              FPDFPage_GetDecodedThumbnailDataFromPage(page, buffer, kBufLen));
+    EXPECT_EQ(kHashedDecodedData, GenerateMD5Base16(buffer, 7524));
+
+    UnloadPage(page);
+  }
+
+  {
+    FPDF_PAGE page = LoadPage(1);
+    ASSERT_TRUE(page);
+
+    const unsigned long kBufLen = 7524u;
+    uint8_t buffer[kBufLen];
+    const char kHashedDecodedData[] = "0f5883a7b9316a2bd6b256bab5590853";
+
+    EXPECT_EQ(7524u,
+              FPDFPage_GetDecodedThumbnailDataFromPage(page, buffer, kBufLen));
+    EXPECT_EQ(kHashedDecodedData, GenerateMD5Base16(buffer, 7524));
+
+    UnloadPage(page);
+  }
+
+  {
+    FPDF_PAGE page = LoadPage(2);
+    ASSERT_TRUE(page);
+
+    unsigned long kBufLen = 7524u;
+    uint8_t buffer[kBufLen];
+    const char kHashedDecodedData[] = "100a1b436bdb6bf814ecb44b3a8a4f3b";
+
+    EXPECT_EQ(7524u,
+              FPDFPage_GetDecodedThumbnailDataFromPage(page, buffer, kBufLen));
+    EXPECT_EQ(kHashedDecodedData, GenerateMD5Base16(buffer, 7524));
+
+    UnloadPage(page);
+  }
+}
+
+TEST_F(FPDFThumbnailEmbedderTest, GetRawThumbnailDataFromPage) {
+  // Open a file with thumbnails
+  ASSERT_TRUE(OpenDocument("simple_thumbnail.pdf"));
+
+  {
+    FPDF_PAGE page = LoadPage(0);
+    ASSERT_TRUE(page);
+
+    unsigned long kBufLen = 987u;
+    uint8_t buffer[kBufLen];
+    const char kHashedRawData[] = "6fbea9faf93f34840f4c51460d9d3568";
+
+    EXPECT_EQ(987u,
+              FPDFPage_GetRawThumbnailDataFromPage(page, buffer, kBufLen));
+    EXPECT_EQ(kHashedRawData, GenerateMD5Base16(buffer, 987));
+
+    UnloadPage(page);
+  }
+
+  {
+    FPDF_PAGE page = LoadPage(1);
+    ASSERT_TRUE(page);
+
+    unsigned long kBufLen = 1218u;
+    uint8_t buffer[kBufLen];
+    const char kHashedRawData[] = "e7c5732b4b97cf4009d4d2ad8cebd800";
+
+    EXPECT_EQ(1218u,
+              FPDFPage_GetRawThumbnailDataFromPage(page, buffer, kBufLen));
+    EXPECT_EQ(kHashedRawData, GenerateMD5Base16(buffer, 1218));
+
+    UnloadPage(page);
+  }
+
+  {
+    FPDF_PAGE page = LoadPage(2);
+    ASSERT_TRUE(page);
+
+    unsigned long kBufLen = 1275u;
+    uint8_t buffer[kBufLen];
+    const char kHashedRawData[] = "622cf5ab7228533d172a634cecbc3282";
+
+    EXPECT_EQ(1275u,
+              FPDFPage_GetRawThumbnailDataFromPage(page, buffer, kBufLen));
+    EXPECT_EQ(kHashedRawData, GenerateMD5Base16(buffer, 1275));
+
+    UnloadPage(page);
+  }
+}
