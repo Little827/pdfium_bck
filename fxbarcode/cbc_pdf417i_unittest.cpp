e39829db9b450343036ca6ff4635bac4104c58f0
Lei Zhang <thestig@chromium.org>
2019-06-05 01:00:25 -0700

    Add a unit test for PDF417 barcode length.
    Make sure CBC_PDF417I will reject barcodes over a certain limit.
    
    BUG=chromium:960176
    
    Change-Id: I5f270cbd1efaa5ec959c49566c45d22481c39abf

---
+++ fxbarcode/cbc_pdf417i_unittest.cpp
@@ -0,0 +1,23 @@
+// Copyright 2019 PDFium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "fxbarcode/cbc_pdf417i.h"
+
+#include <algorithm>
+#include <vector>
+
+#include "testing/gtest/include/gtest/gtest.h"
+
+TEST(CBC_PDF417ITest, MaxLength) {
+  {
+    CBC_PDF417I encoder;
+    EXPECT_TRUE(encoder.Encode(L"Foo"));
+  }
+  {
+    std::vector<wchar_t> input(2711);
+    std::fill(input.begin(), input.end(), L'1');
+    CBC_PDF417I encoder;
+    EXPECT_FALSE(encoder.Encode(WideStringView(input.data(), input.size())));
+  }
+}
