ce8af692842fa7c4a10e0ab62ea8404d2b944cf7
Tom Sepez <tsepez@chromium.org>
2019-06-06 15:20:31 -0700

    Prevent CPDF_Type3Font from knowing that CPDF_Type3Char has a form.
    Change-Id: I4de6d610102ba5897bb840465356a5f1bfafb835

--- core/fpdfapi/font/cpdf_type3char.h
+++ core/fpdfapi/font/cpdf_type3char.h
@@ -8,39 +8,50 @@
 #define CORE_FPDFAPI_FONT_CPDF_TYPE3CHAR_H_
 
 #include <memory>
+#include <set>
 
 #include "core/fxcrt/fx_coordinates.h"
 #include "core/fxcrt/fx_system.h"
 #include "core/fxcrt/retain_ptr.h"
 
 class CFX_DIBitmap;
+class CPDF_AllStates;
+class CPDF_Dictionary;
+class CPDF_Document;
 class CPDF_Form;
 class CPDF_RenderContext;
+class CPDF_Stream;
 
 class CPDF_Type3Char {
  public:
-  explicit CPDF_Type3Char(std::unique_ptr<CPDF_Form> pForm);
+  CPDF_Type3Char(CPDF_Document* pDocument,
+                 CPDF_Dictionary* pPageResources,
+                 CPDF_Stream* pFormStream);
   ~CPDF_Type3Char();
 
   static float TextUnitToGlyphUnit(float fTextUnit);
   static void TextUnitRectToGlyphUnitRect(CFX_FloatRect* pRect);
 
   bool LoadBitmap(CPDF_RenderContext* pContext);
-
   void InitializeFromStreamData(bool bColored, const float* pData);
   void Transform(const CFX_Matrix& matrix);
   void ResetForm();
+  void ParseContent(const CPDF_AllStates* pGraphicStates,
+                    const CFX_Matrix* pParentMatrix,
+                    CPDF_Type3Char* pType3Char,
+                    std::set<const uint8_t*>* parsedSet);
+  size_t GetPageObjectCount() const;
 
   RetainPtr<CFX_DIBitmap> GetBitmap();
   const RetainPtr<CFX_DIBitmap>& GetBitmap() const;
-
-  const CPDF_Form* form() const { return m_pForm.get(); }
-  CPDF_Form* form() { return m_pForm.get(); }
 
   bool colored() const { return m_bColored; }
   uint32_t width() const { return m_Width; }
   const CFX_Matrix& matrix() const { return m_ImageMatrix; }
   const FX_RECT& bbox() const { return m_BBox; }
+
+  const CPDF_Form* form() const { return m_pForm.get(); }
+  CPDF_Form* form() { return m_pForm.get(); }
 
  private:
   std::unique_ptr<CPDF_Form> m_pForm;
