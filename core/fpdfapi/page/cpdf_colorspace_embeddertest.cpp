01a73876e2e69349e8fd638a8f850a3bc7b78c7e
Tom Sepez <tsepez@chromium.org>
2019-07-08 12:23:23 -0700

    Add CF test case for bug 981288.
    The test passes on ToT, but fails on 73bd37c824051aac631abaf567bab7dcd02093df
    
    Bug: chromium:981288
    Change-Id: I48b5970fe1813d7b01e57f860ac863d3761992f4

---
+++ core/fpdfapi/page/cpdf_colorspace_embeddertest.cpp
@@ -0,0 +1,40 @@
+// Copyright 2019 PDFium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "testing/embedder_test.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+class CPDF_ColorspaceEmbedderTest : public EmbedderTest {};
+
+// Test passes if it does not crash.
+TEST_F(CPDF_ColorspaceEmbedderTest, Bug_981256) {
+  ASSERT_TRUE(OpenDocument("bug_981256.pdf"));
+  FPDF_PAGE page = LoadPage(0);
+  ASSERT_TRUE(page);
+  UnloadPage(page);
+}
+
+// Test passes if it does not crash.
+TEST_F(CPDF_ColorspaceEmbedderTest, Bug_981288) {
+  ASSERT_TRUE(OpenDocument("bug_981288.pdf"));
+  FPDF_PAGE page = LoadPage(0);
+  ASSERT_TRUE(page);
+  UnloadPage(page);
+}
+
+// Test passes if it does not crash.
+TEST_F(CPDF_ColorspaceEmbedderTest, Bug_981347) {
+  ASSERT_TRUE(OpenDocument("bug_981347.pdf"));
+  FPDF_PAGE page = LoadPage(0);
+  ASSERT_TRUE(page);
+  UnloadPage(page);
+}
+
+// Test passes if it does not crash.
+TEST_F(CPDF_ColorspaceEmbedderTest, Bug_981412) {
+  ASSERT_TRUE(OpenDocument("bug_981412.pdf"));
+  FPDF_PAGE page = LoadPage(0);
+  ASSERT_TRUE(page);
+  UnloadPage(page);
+}
