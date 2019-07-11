7872a6214780127887f78657908e7109bbf69d26
Tom Sepez <tsepez@chromium.org>
2019-07-11 15:23:30 -0700

    Push logic back into appropriate files.
    Change-Id: I2d7c7547fd659b6da72a449997dd141eb8118426

--- core/fpdfapi/font/cpdf_type3char.cpp
+++ core/fpdfapi/font/cpdf_type3char.cpp
@@ -40,29 +40,15 @@ bool CPDF_Type3Char::LoadBitmap() {
   if (m_pBitmap || !m_pForm)
     return true;
 
-  if (m_pForm->GetPageObjectCount() != 1 || m_bColored)
+  if (m_bColored)
     return false;
 
-  auto& pPageObj = *m_pForm->begin();
-  if (!pPageObj->IsImage())
+  const CPDF_ImageObject* pImageObject = m_pForm->GetSoleImageOfForm();
+  if (!pImageObject)
     return false;
 
-  m_ImageMatrix = pPageObj->AsImage()->matrix();
-  {
-    // |pSource| actually gets assigned a CPDF_DIBBase, which has pointers
-    // into objects owned by |m_pForm|. Make sure it is out of scope before
-    // clearing the form.
-    RetainPtr<CFX_DIBBase> pSource =
-        pPageObj->AsImage()->GetImage()->LoadDIBBase();
-
-    // Clone() is non-virtual, and can't be overloaded by CPDF_DIBBase to
-    // return a clone of the subclass as one would typically expect from a
-    // such a method. Instead, it only clones the CFX_DIBBase, none of whose
-    // members point to objects owned by the form. As a result, |m_pBitmap|
-    // may outlive |m_pForm|.
-    if (pSource)
-      m_pBitmap = pSource->Clone(nullptr);
-  }
+  m_pBitmap = pImageObject->GetIndependentBitmap();
+  m_ImageMatrix = pImageObject->matrix();
   m_pForm.reset();
   return true;
 }
