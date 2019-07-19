bcb79dd00a339d985d55fe25e7bd1334850dea29
Daniel Hosseinian <dhoss@chromium.org>
2019-07-18 18:02:32 -0700

    Add new experimental API function to flatten PDFs without form controls
    Add new API function FPDFPage_Flatten_No_Controls that is similar to
    the original FPDFPage_Flatten, but takes an additional argument for the
    PDF form handle. The form handle allows the flatten function to access
    the form controls and cleanly recreate the in-memory widget appearance
    streams without having to duplicate code. The original flatten function
    calls the new flatten function but passes a nullptr for the form
    handle.
    
    Consequently, the CPDF_PageExporter::ExportPage function in fpdf_ppo.cpp
    was altered to export the PDF's AcroForm object in the document root.
    This is necessary for accessing the PDF's page views and form handles.
    
    Related Chromium CL can be found at [1].
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/1702627
    
    Bug: chromium:954307
    
    Change-Id: Ia4ab238692bc55bf67f43559c05e1eb2f310b9ff

--- fpdfsdk/fpdf_flatten_embeddertest.cpp
+++ fpdfsdk/fpdf_flatten_embeddertest.cpp
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "public/fpdf_annot.h"
 #include "public/fpdf_flatten.h"
 #include "public/fpdfview.h"
 #include "testing/embedder_test.h"
@@ -69,4 +70,42 @@ TEST_F(FPDFFlattenEmbedderTest, BUG_896366) {
   UnloadPage(page);
 
   VerifySavedDocument(612, 792, md5_hash);
+}
+
+TEST_F(FPDFFlattenEmbedderTest, BUG_954307) {
+#if defined(OS_MACOSX)
+  static const char md5_hash[] = "97d8c820c2c2d93f6e8025dffa5c42eb";
+  static const char md5_hash_flat[] = "e2511495540d98fece6d8e4f80c87437";
+#elif defined(OS_WIN)
+  static const char md5_hash[] = "9b2dd769711b54c47fd54c4b292ad0e8";
+  static const char md5_hash_flat[] = "f724caf2b832880a85d46445effb288e";
+#else
+  static const char md5_hash[] = "072d074fc6fec24fbd5742a6dc747ff3";
+  static const char md5_hash_flat[] = "c64a2a7f3add23b3d7ab94e7064ea20d";
+#endif
+  EXPECT_TRUE(OpenDocument("combobox_form.pdf"));
+  FPDF_PAGE page = LoadPage(0);
+  EXPECT_TRUE(page);
+  EXPECT_NE(FPDFPage_GetAnnotCount(page), 0);
+
+  ScopedFPDFBitmap bitmap = RenderLoadedPageWithFlags(page, FPDF_ANNOT);
+  CompareBitmap(bitmap.get(), 300, 600, md5_hash);
+
+  EXPECT_EQ(FLATTEN_SUCCESS,
+            FPDFPage_Flatten_No_Controls(page, form_handle(), FLAT_PRINT));
+  EXPECT_TRUE(FPDF_SaveAsCopy(document(), this, 0));
+
+  UnloadPage(page);
+
+  EXPECT_TRUE(OpenSavedDocument());
+  FPDF_PAGE saved_page = LoadSavedPage(0);
+  EXPECT_TRUE(saved_page);
+  EXPECT_EQ(FPDFPage_GetAnnotCount(saved_page), 0);
+
+  ScopedFPDFBitmap saved_bitmap =
+      RenderSavedPageWithFlags(saved_page, FPDF_ANNOT);
+  CompareBitmap(saved_bitmap.get(), 300, 600, md5_hash_flat);
+
+  CloseSavedPage(saved_page);
+  CloseSavedDocument();
 }
