72d3dd93efd4a85f844aad23fac3c5783da34a39
Benjamin Beaudry <benjamin.beaudry@microsoft.com>
2019-07-25 16:19:04 -0700

    Get the rotation angle of a character.
    Leveraging the transformation matrix attribute of a character, we can
    easily find the angle by which it is rotated. The angle is used in a
    separate change on Chromium. For more information, see
    https://chromium-review.googlesource.com/c/chromium/src/+/1718989
    
    Bug: 985604
    Change-Id: Ifc2e46f0e1eeca8f1e10218f0d1b8ff9fcef3d29

--- fpdfsdk/fpdf_text.cpp
+++ fpdfsdk/fpdf_text.cpp
@@ -4,8 +4,13 @@
 
 // Original code copyright 2014 Foxit Software Inc. http://www.foxitsoftware.com
 
+#define _USE_MATH_DEFINES
+
+#include <math.h>
+
 #include "public/fpdf_text.h"
 
+#include
 #include <algorithm>
 #include <memory>
 #include <vector>
@@ -119,6 +124,28 @@ FPDFText_GetFontInfo(FPDF_TEXTPAGE text_page,
   if (buffer && buflen >= length)
     memcpy(buffer, basefont.c_str(), length);
   return length;
+}
+
+FPDF_EXPORT double FPDF_CALLCONV FPDFText_GetCharAngle(FPDF_TEXTPAGE text_page,
+                                                       int index) {
+  CPDF_TextPage* textpage = GetTextPageForValidIndex(text_page, index);
+  if (!textpage)
+    return -1;
+
+  FPDF_CHAR_INFO charinfo;
+  textpage->GetCharInfo(index, &charinfo);
+  // On the left is our current Matrix and on the right
+  // a generic rotation matrix for our coordinate space.
+  // | a  b  0 |    | cos(t)  -sin(t)  0 |
+  // | c  d  0 |    | sin(t)   cos(t)  0 |
+  // | e  f  1 |    |   0        0     1 |
+  // Calculate the angle of the vector
+  double angle = atan2(charinfo.m_Matrix.c, charinfo.m_Matrix.a);
+  if (angle < 0) {
+    angle = 2 * M_PI + angle;
+  }
+
+  return angle;
 }
 
 FPDF_EXPORT FPDF_BOOL FPDF_CALLCONV FPDFText_GetCharBox(FPDF_TEXTPAGE text_page,
