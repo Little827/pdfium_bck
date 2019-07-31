48f5fecc38cd5a54d33538826c8928248954a522
Tom Sepez <tsepez@chromium.org>
2019-07-31 10:39:06 -0700

    Move the UNSUPPORT_INFO interface out of CPDF_ModuleMgr.
    There's no need for the module manager mechanism, and the
    UNSUPPORT_IFNO can live as a global singleton in cpdfsdk_helpers.cpp,
    which is responsible for converting public/ types into things the
    rest of PDFium can use.
    
    A subsequent CL may remove CPDF_ModuleMgr entirely.
    
    Change-Id: I44077a6ff9922ac4ab17c77eb4b631d1df434737

--- fpdfsdk/fpdf_view.cpp
+++ fpdfsdk/fpdf_view.cpp
@@ -207,7 +207,7 @@ FPDF_EXPORT void FPDF_CALLCONV FPDF_DestroyLibrary() {
 
 FPDF_EXPORT void FPDF_CALLCONV FPDF_SetSandBoxPolicy(FPDF_DWORD policy,
                                                      FPDF_BOOL enable) {
-  return SetPDFSandboxPolicy(policy, enable);
+  return SetPDFSandBoxPolicy(policy, enable);
 }
 
 #if defined(OS_WIN)
