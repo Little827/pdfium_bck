1412d671d4a1f97c687b6b5955ec0f4990fb1bf5
Tom Sepez <tsepez@chromium.org>
2019-08-06 14:10:10 -0700

    Make CPWL_Timer simpler.
    Remove CPWL_TimerHandler, which was an object that served two
    purposes: as an interface to dispatch a timer event, and as
    an interface to obtain a systemhandler. The second isn't really
    required, and the first can be provided by a simple nested
    virtual class.
    
    Also remove the start/stop timer mechanism, timers can be cancelled
    by merely deleting them.
    
    Change-Id: I5fc1305c4e1b2693d39a74bea10b677263e9e12c

--- fpdfsdk/pwl/cpwl_button.cpp
+++ fpdfsdk/pwl/cpwl_button.cpp
@@ -4,11 +4,9 @@
 
 // Original code copyright 2014 Foxit Software Inc. http://www.foxitsoftware.com
 
-#include "fpdfsdk/pwl/cpwl_button.h"
-
 #include <utility>
 
-#include "fpdfsdk/pwl/cpwl_wnd.h"
+#include "fpdfsdk/pwl/cpwl_button.h"
 
 CPWL_Button::CPWL_Button(const CreateParams& cp,
                          std::unique_ptr<PrivateData> pAttachedData)
