7049c989b1997496c6acb5908cba7522f89255ac
Hui Yingst <nigi@chromium.org>
2019-08-13 13:58:10 -0700

    [SkiaPaths] Vertical texts were drawn in reversed direction.
    In ChS_simple.pdf, the Chinese characters are displayed vertically and
    when characters are displayed in order, the sentence direction was
    going upwards instead of downwards. This is because when the main font
    indicates vertical display("Identity-V"), its descendant font should
    inherit the vertical setup since there is only one way to display the
    text object. LoadSubstFont() is the only method to sync the descendant
    font's |m_bVertical| with the main font, but it was skipped in
    CPDF_CIDFont::Load() due to the main font is embedded.
    
    To fix this issue, need to add a method to sync the descendant font's
    |m_bVertical| value with the main font if it indicates vertical
    display(not default value), regardless whether the main font is
    embedded or not.
    
    Bug: pdfium:1376
    Change-Id: I7e935b59745b063b82ec709afd847c6bc289937d

--- core/fxge/cfx_font.cpp
+++ core/fxge/cfx_font.cpp
@@ -372,7 +372,9 @@ uint32_t CFX_Font::GetGlyphWidth(uint32_t glyph_index) {
   return EM_ADJUST(FXFT_Get_Face_UnitsPerEM(m_Face->GetRec()), horiAdvance);
 }
 
-bool CFX_Font::LoadEmbedded(pdfium::span<const uint8_t> src_span) {
+bool CFX_Font::LoadEmbedded(pdfium::span<const uint8_t> src_span,
+                            bool bVertical) {
+  m_bVertical = bVertical;
   m_pFontDataAllocation =
       std::vector<uint8_t>(src_span.begin(), src_span.end());
   m_Face = CFX_GEModule::Get()->GetFontMgr()->NewFixedFace(
