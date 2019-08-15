6a28f567fb6a00f92b0be33db4dc5fc269652c13
Lei Zhang <thestig@chromium.org>
2019-08-15 13:24:21 -0700

    Make CPDF_CMap initialization slightly more efficient.
    CPDF_CMapParser is a short lived class that initializes a CPDF_CMap. For
    a CPDF_CMap's |m_MixedFourByteLeadingRanges| vector, the parser
    initializes it by reading out of it and then appending to it. This
    requires CPDF_CMap to have 2 public methods, with the append method
    appending one element at a time.
    
    Improve upon this by removing those two methods and instead first let
    CPDF_CMapParser hold on to the vector as |m_Ranges|. Since the parser
    has direct access to both |m_Ranges| and |m_PendingRanges|, it can move
    elements into |m_Ranges| in bulk. When the parser is done, it can use
    the newly added CPDF_CMap::SetMixedFourByteLeadingRanges() to move
    |m_Ranges| to CPDF_CMap.
    
    Change-Id: I6e6e339296a08a210b925efc278b490a457f8026

--- core/fpdfapi/font/cpdf_cmapparser.h
+++ core/fpdfapi/font/cpdf_cmapparser.h
@@ -56,6 +56,7 @@ class CPDF_CMapParser {
   Status m_Status = kStart;
   int m_CodeSeq = 0;
   std::vector<CPDF_CMap::CodeRange> m_PendingRanges;
+  std::vector<CPDF_CMap::CodeRange> m_Ranges;
   std::vector<CPDF_CMap::CIDRange> m_AdditionalCharcodeToCIDMappings;
   ByteString m_LastWord;
   uint32_t m_CodePoints[4];
