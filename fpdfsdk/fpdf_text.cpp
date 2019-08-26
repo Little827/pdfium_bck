73a9663dc0c31d9aba53685457c191e354cea0a9
Ian Prest <iapres@microsoft.com>
2019-08-26 13:25:27 -0700

    New API to retrieve a character's loose bounds
    This change adds a new API `FPDFText_GetLooseCharBox` that retrieves the
    "loose" bounds of a character.  I.e., the returned bounds will cover the
    entire possible extent of the character (given its width).  The height
    of the box will always be the same as the font size.  The width will
    equal the entire "advance" of the character.
    
    Compare with `FPDFText_GetCharBox`, which returns a "tight" box around
    the actual curves that make up the glyph.
    
    Change-Id: I2860f12865b6c81e90cb2621fd8adac1ab5f2530

--- fpdfsdk/fpdf_text.cpp
+++ fpdfsdk/fpdf_text.cpp
@@ -159,6 +159,30 @@ FPDF_EXPORT FPDF_BOOL FPDF_CALLCONV FPDFText_GetCharBox(FPDF_TEXTPAGE text_page,
 }
 
 FPDF_EXPORT FPDF_BOOL FPDF_CALLCONV
+FPDFText_GetLooseCharBox(FPDF_TEXTPAGE text_page,
+                         int index,
+                         double* left,
+                         double* right,
+                         double* bottom,
+                         double* top) {
+  CPDF_TextPage* textpage = GetTextPageForValidIndex(text_page, index);
+  if (!textpage)
+    return false;
+
+  FPDF_CHAR_INFO charinfo;
+  textpage->GetCharInfo(index, &charinfo);
+  FX_RECT bbox = charinfo.m_pTextObj->GetFont()->GetFontBBox();
+  float font_scale = charinfo.m_FontSize / bbox.Height();
+  float width = charinfo.m_pTextObj->GetCharWidth(charinfo.m_Charcode);
+
+  *left = charinfo.m_Origin.x;
+  *right = charinfo.m_Origin.x + width;
+  *bottom = charinfo.m_Origin.y - bbox.bottom * font_scale;
+  *top = charinfo.m_Origin.y - bbox.top * font_scale;
+  return true;
+}
+
+FPDF_EXPORT FPDF_BOOL FPDF_CALLCONV
 FPDFText_GetCharOrigin(FPDF_TEXTPAGE text_page,
                        int index,
                        double* x,
