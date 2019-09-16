7977ec0db10dbc81f6a4b90f71c630d3d4606530
Hui Yingst <nigi@chromium.org>
2019-09-16 07:48:48 -0700

    Fix the TrueType font not rendered correctly issue.
    This CL adds an extra check on whether the font is embedded when
    determining whether no glyph index is found for a TrueType font
    character. For Simple font character, an index of 0xffff is a guaranteed
    failure. But for TrueType font (one of the Simple font types), an index
    of 0 indicates a freetype failure, which means we should either try to
    find the glyph info from the embedded font file or from a fallback font
    if there is one.
    
    Bug: pdfium:1388
    Change-Id: I8b1935a094f76408e4678723d566914c2bbf0f42

--- core/fpdfapi/render/cpdf_charposlist.cpp
+++ core/fpdfapi/render/cpdf_charposlist.cpp
@@ -40,18 +40,42 @@ CPDF_CharPosList::CPDF_CharPosList(const std::vector<uint32_t>& charCodes,
                   : charpos.m_GlyphIndex;
 #endif
     CFX_Font* pCurrentFont;
-    if (GlyphID != static_cast<uint32_t>(-1)) {
-      charpos.m_FallbackFontPosition = -1;
-      pCurrentFont = pFont->GetFont();
-    } else {
+    if (GlyphID == static_cast<uint32_t>(-1)) {
       charpos.m_FallbackFontPosition =
           pFont->FallbackFontFromCharcode(CharCode);
       charpos.m_GlyphIndex = pFont->FallbackGlyphFromCharcode(
           charpos.m_FallbackFontPosition, CharCode);
+      charpos.m_FallbackFontPosition =
+          pFont->FallbackFontFromCharcode(CharCode);
+      charpos.m_GlyphIndex = pFont->FallbackGlyphFromCharcode(
+          charpos.m_FallbackFontPosition, CharCode);
+
       pCurrentFont = pFont->GetFontFallback(charpos.m_FallbackFontPosition);
 #if defined(OS_MACOSX)
       charpos.m_ExtGID = charpos.m_GlyphIndex;
 #endif
+    } else if (GlyphID == static_cast<uint32_t>(0) && pFont->IsTrueTypeFont()) {
+      charpos.m_FallbackFontPosition =
+          pFont->FallbackFontFromCharcode(CharCode);
+      charpos.m_GlyphIndex = pFont->FallbackGlyphFromCharcode(
+          charpos.m_FallbackFontPosition, CharCode);
+      if (charpos.m_GlyphIndex == static_cast<uint32_t>(-1) &&
+          pFont->IsTrueTypeFont()) {
+        // For a TrueType font character, when failed at finding the glyph from
+        // the fallback font, switch back to using the original font.
+        if (bToUnicode)
+          charpos.m_GlyphIndex = 0;
+        charpos.m_FallbackFontPosition = -1;
+        pCurrentFont = pFont->GetFont();
+      } else {
+        pCurrentFont = pFont->GetFontFallback(charpos.m_FallbackFontPosition);
+#if defined(OS_MACOSX)
+        charpos.m_ExtGID = charpos.m_GlyphIndex;
+#endif
+      }
+    } else {
+      charpos.m_FallbackFontPosition = -1;
+      pCurrentFont = pFont->GetFont();
     }
 
     if (!pFont->IsEmbedded() && !pFont->IsCIDFont())
