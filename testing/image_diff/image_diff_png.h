63889f2fb0ed1c15d446ad60599a6965ac98f816
Lei Zhang <thestig@chromium.org>
2019-11-20 00:22:02 -0800

    Add --reverse-byte-order flag to pdfium_diff.
    Allow pdfium_diff to decode BGRA images if desired, instead of always
    assuming images are RGBA.
    
    Bug: pdfium:994
    Change-Id: I2322ffaea2b640a0d5b5f68aaccd502b367eb661

--- testing/image_diff/image_diff_png.h
+++ testing/image_diff/image_diff_png.h
@@ -11,8 +11,10 @@
 
 namespace image_diff_png {
 
-// Decode a PNG into an RGBA pixel array.
+// Decode a PNG into an RGBA pixel array, or BGRA pixel array if
+// |reverse_byte_order| is set to true.
 std::vector<unsigned char> DecodePNG(const std::vector<unsigned char>& input,
+                                     bool reverse_byte_order,
                                      int* width,
                                      int* height);
 
