7e0c5a72d5991face9997a0f0c6865c02e1ba150
Daniel Hosseinian <dhoss@chromium.org>
2019-11-22 11:13:38 -0800

    Add flags to test_runner.py to disable JS/XFA when running pdfium_test
    Bug: pdfium:1411, pdfium:1412
    Change-Id: I7ce74a26569ce5dc144dc9606e29f2305e99e41b

--- testing/tools/test_runner.py
+++ testing/tools/test_runner.py
@@ -153,12 +153,25 @@ class TestRunner:
 
     with open(txt_path, 'w') as outfile:
       cmd_to_run = [
-          self.pdfium_test_path, '--send-events', '--time=' + TEST_SEED_TIME,
-          pdf_path
+          self.pdfium_test_path, '--send-events', '--time=' + TEST_SEED_TIME
       ]
+
+      if self.disable_javascript:
+        cmd_to_run.append('--disable-javascript')
+
+      if self.disable_xfa:
+        cmd_to_run.append('--disable-xfa')
+
+      cmd_to_run.append(pdf_path)
       subprocess.check_call(cmd_to_run, stdout=outfile)
 
-    if not os.path.exists(expected_txt_path):
+    # If JavaScript is disabled, the output should be empty.
+    # If the expected file does not exist, the output is expected to be empty.
+    # However, if the test is suppressed and the above two conditions are met,
+    # do not verify that the text is empty so the suppressed test does not
+    # surprise.
+    if (self.disable_javascript or not os.path.exists(expected_txt_path)
+       ) and not self.test_suppressor.IsResultSuppressed(input_root + '.in'):
       return self._VerifyEmptyText(txt_path)
 
     cmd = [sys.executable, self.text_diff_path, expected_txt_path, txt_path]
@@ -188,6 +201,12 @@ class TestRunner:
 
     if use_ahem:
       cmd_to_run.append('--font-dir=%s' % self.font_dir)
+
+    if self.disable_javascript:
+      cmd_to_run.append('--disable-javascript')
+
+    if self.disable_xfa:
+      cmd_to_run.append('--disable-xfa')
 
     if self.options.reverse_byte_order:
       cmd_to_run.append('--reverse-byte-order')
@@ -241,6 +260,18 @@ class TestRunner:
         help='run NUM_WORKERS jobs in parallel')
 
     parser.add_option(
+        '--disable-javascript',
+        action="store_true",
+        dest="disable_javascript",
+        help='Prevents JavaScript from executing in PDF files.')
+
+    parser.add_option(
+        '--disable-xfa',
+        action="store_true",
+        dest="disable_xfa",
+        help='Prevents processing XFA forms.')
+
+    parser.add_option(
         '--gold_properties',
         default='',
         dest="gold_properties",
@@ -291,6 +322,9 @@ class TestRunner:
 
     self.options, self.args = parser.parse_args()
 
+    self.disable_javascript = self.options.disable_javascript
+    self.disable_xfa = self.options.disable_xfa
+
     if (self.options.regenerate_expected and
         self.options.regenerate_expected not in ['all', 'platform']):
       print 'FAILURE: --regenerate_expected must be "all" or "platform"'
@@ -319,7 +353,8 @@ class TestRunner:
 
     self.feature_string = subprocess.check_output(
         [self.pdfium_test_path, '--show-config'])
-    self.test_suppressor = suppressor.Suppressor(finder, self.feature_string)
+    self.test_suppressor = suppressor.Suppressor(
+        finder, self.feature_string, self.disable_javascript, self.disable_xfa)
     self.image_differ = pngdiffer.PNGDiffer(finder,
                                             self.options.reverse_byte_order)
     error_message = self.image_differ.CheckMissingTools(
