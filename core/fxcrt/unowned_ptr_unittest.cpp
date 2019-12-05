0d86d38557884bf32dacf2561c89292c22f773a5
Tom Sepez <tsepez@chromium.org>
2019-12-04 16:43:30 -0800

    Give UnownedPtr<> a move ctor and move assign operator.
    Otherwise, the examples added to the unit test will trip an
    incorrect asssertion.
    
    Change-Id: I1dc85cfdf0937036c4493e43ee9dfac03ec38767

--- core/fxcrt/unowned_ptr_unittest.cpp
+++ core/fxcrt/unowned_ptr_unittest.cpp
@@ -114,6 +114,25 @@ TEST(UnownedPtr, ReleaseOk) {
   }
 }
 
+TEST(UnownedPtr, MoveCtorOk) {
+  UnownedPtr<Clink> outer;
+  {
+    auto onwed = pdfium::MakeUnique<Clink>();
+    outer = onwed.get();
+    UnownedPtr<Clink> inner(std::move(outer));
+  }
+}
+
+TEST(UnownedPtr, MoveAssignOk) {
+  UnownedPtr<Clink> outer;
+  {
+    auto onwed = pdfium::MakeUnique<Clink>();
+    outer = onwed.get();
+    UnownedPtr<Clink> inner;
+    inner = std::move(outer);
+  }
+}
+
 TEST(UnownedPtr, ReleaseNotOk) {
 #if defined(ADDRESS_SANITIZER)
   EXPECT_DEATH(ReleaseDangling(), "");
