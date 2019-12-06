7f176db48232f692eba82763fa8844b4d54a17fd
Tom Sepez <tsepez@chromium.org>
2019-12-06 13:56:57 -0800

    Better tests for CJS_Field properties.
    Fixes some longstanding issues
    - run text widget tests on text widget, not its parent.
    - run subset of these tests on the parent itself.
    - fix test helpers to compare arrays/objects for equality.
    - update an error message to indicate the problem is with caller
      argument, not the object bound across the fxjs interface.
    
    Change-Id: I60834021b64eb275d3ec36bcdea7f71fc11d18ab

--- testing/resources/javascript/property_test_helpers.js
+++ testing/resources/javascript/property_test_helpers.js
@@ -2,10 +2,23 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+function kindaSortaEqual(arg1, arg2) {
+  if (arg1 == arg2) {
+    return true;
+  }
+  if (typeof arg1 != "object") {
+    return false;
+  }
+  if (typeof arg2 != "object") {
+    return false;
+  }
+  return arg1.toString() == arg2.toString();
+}
+
 function testReadProperty(that, prop, expected) {
   try {
     var actual = that[prop];
-    if (actual == expected) {
+    if (kindaSortaEqual(actual, expected)) {
       app.alert('PASS: ' + prop + ' = ' + actual);
     } else {
       app.alert('FAIL: ' + prop + ' = ' + actual + ', expected = ' + expected);
@@ -28,7 +41,7 @@ function testWriteProperty(that, prop, newValue) {
   try {
     that[prop] = newValue;
     var actual = that[prop];
-    if (actual == newValue) {
+    if (kindaSortaEqual(actual, newValue)) {
       app.alert('PASS: ' + prop + ' = ' + actual);
     } else {
       app.alert('FAIL: ' + prop + ' = ' + actual + ', expected = ' + newValue);
@@ -42,7 +55,7 @@ function testWriteIgnoredProperty(that, prop, expected, newValue) {
   try {
     that[prop] = newValue;
     var actual = that[prop];
-    if (actual == expected) {
+    if (kindaSortaEqual(actual, expected)) {
       app.alert('PASS: ' + prop + ' = ' + actual);
     } else {
       app.alert('FAIL: ' + prop + ' = ' + actual + ', expected = ' + expected);
