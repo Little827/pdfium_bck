f35b050650dd91c91bc78329336613379073d632
Tom Sepez <tsepez@chromium.org>
2019-12-17 12:37:48 -0800

    Remove dependence between fpdfdk/formfiller and fpdfapi/parser.
    There's a better way to get the same information through another object
    that the formfiller already holds.
    
    Change-Id: Ibba5c535096409ac64c260bb130b4e3419d95ce3

--- fpdfsdk/formfiller/cffl_interactiveformfiller.h
+++ fpdfsdk/formfiller/cffl_interactiveformfiller.h
@@ -97,9 +97,9 @@ class CFFL_InteractiveFormFiller final : public IPWL_Filler_Notify {
 
   static bool IsVisible(CPDFSDK_Widget* pWidget);
   static bool IsReadOnly(CPDFSDK_Widget* pWidget);
-  static bool IsFillingAllowed(CPDFSDK_Widget* pWidget);
   static bool IsValidAnnot(CPDFSDK_PageView* pPageView, CPDFSDK_Annot* pAnnot);
 
+  bool IsFillingAllowed(CPDFSDK_Widget* pWidget);
   bool OnKeyStrokeCommit(ObservedPtr<CPDFSDK_Annot>* pAnnot,
                          CPDFSDK_PageView* pPageView,
                          uint32_t nFlag);
