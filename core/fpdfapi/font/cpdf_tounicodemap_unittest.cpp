aad4acfc1f6e5e78523120e9a40b5c1675205065
Hui Yingst <nigi@chromium.org>
2019-12-20 10:53:40 -0800

    Add string validations in StringToCode() and StringToWideString().
    Add the following validations in CPDF_ToUnicodeMap::StringToCode():
    (1) Check whether the string has matching "<" and ">".
    (2) Check whether the string represents a valid hexadecimal number.
    (3) bfrange and bfchar should only contain hexadecimal numbers.
        Remove the steps for parsing decimal numbers.
    (4) Detect integer overflow.
    
    If any of the validations fails, use pdfium::nullopt as the return
    value to indicate error, since Acrobat allows code values which covers
    the whole uint32_t range.
    
    In CPDF_ToUnicodeMap::StringToWideString(), add a check for matching
    "<" and ">". No need to add invalid hex character check since Acrobat
    and Okular both ignore the rest of the string when encountering
    invalid characters.
    
    This CL also updates the unit test for StringToCode() and
    StringToWideString() according to the changes above.
    
    Bug: pdfium:1422
    Change-Id: Ie3608515670b8d1efd6909b6f465e2d0e2e7ebf6

--- core/fpdfapi/font/cpdf_tounicodemap_unittest.cpp
+++ core/fpdfapi/font/cpdf_tounicodemap_unittest.cpp
@@ -7,24 +7,39 @@
 #include "testing/gtest/include/gtest/gtest.h"
 
 TEST(cpdf_tounicodemap, StringToCode) {
-  EXPECT_EQ(0u, CPDF_ToUnicodeMap::StringToCode(""));
-  EXPECT_EQ(0u, CPDF_ToUnicodeMap::StringToCode("12"));
-  EXPECT_EQ(194u, CPDF_ToUnicodeMap::StringToCode("<c2"));
-  EXPECT_EQ(162u, CPDF_ToUnicodeMap::StringToCode("<A2"));
-  EXPECT_EQ(2802u, CPDF_ToUnicodeMap::StringToCode("<Af2"));
+  EXPECT_EQ(1u, CPDF_ToUnicodeMap::StringToCode("<0001>"));
+  EXPECT_EQ(194u, CPDF_ToUnicodeMap::StringToCode("<c2>"));
+  EXPECT_EQ(162u, CPDF_ToUnicodeMap::StringToCode("<A2>"));
+  EXPECT_EQ(2802u, CPDF_ToUnicodeMap::StringToCode("<Af2>"));
+  EXPECT_EQ(4294967295u, CPDF_ToUnicodeMap::StringToCode("<FFFFFFFF>"));
+
+  // Integer overflow
+  ASSERT_FALSE(CPDF_ToUnicodeMap::StringToCode("<100000000>"));
+  ASSERT_FALSE(CPDF_ToUnicodeMap::StringToCode("<1abcdFFFF>"));
+
+  // Invalid string
+  ASSERT_FALSE(CPDF_ToUnicodeMap::StringToCode(""));
+  ASSERT_FALSE(CPDF_ToUnicodeMap::StringToCode("<>"));
+  ASSERT_FALSE(CPDF_ToUnicodeMap::StringToCode("128"));
+  ASSERT_FALSE(CPDF_ToUnicodeMap::StringToCode("<12"));
+  ASSERT_FALSE(CPDF_ToUnicodeMap::StringToCode("12>"));
+  ASSERT_FALSE(CPDF_ToUnicodeMap::StringToCode("<1-7>"));
+  ASSERT_FALSE(CPDF_ToUnicodeMap::StringToCode("00AB"));
+  ASSERT_FALSE(CPDF_ToUnicodeMap::StringToCode("<00NN>"));
 }
 
 TEST(cpdf_tounicodemap, StringToWideString) {
   EXPECT_EQ(L"", CPDF_ToUnicodeMap::StringToWideString(""));
   EXPECT_EQ(L"", CPDF_ToUnicodeMap::StringToWideString("1234"));
-
-  EXPECT_EQ(L"", CPDF_ToUnicodeMap::StringToWideString("<c2"));
+  EXPECT_EQ(L"", CPDF_ToUnicodeMap::StringToWideString("<c2D2"));
+  EXPECT_EQ(L"", CPDF_ToUnicodeMap::StringToWideString("c2ab>"));
 
   WideString res = L"\xc2ab";
-  EXPECT_EQ(res, CPDF_ToUnicodeMap::StringToWideString("<c2ab"));
-  EXPECT_EQ(res, CPDF_ToUnicodeMap::StringToWideString("<c2abab"));
-  EXPECT_EQ(res, CPDF_ToUnicodeMap::StringToWideString("<c2ab 1234"));
+  EXPECT_EQ(res, CPDF_ToUnicodeMap::StringToWideString("<c2ab>"));
+  EXPECT_EQ(res, CPDF_ToUnicodeMap::StringToWideString("<c2ab123>"));
+  EXPECT_EQ(res, CPDF_ToUnicodeMap::StringToWideString("<c2abX123>"));
 
   res += L"\xfaab";
-  EXPECT_EQ(res, CPDF_ToUnicodeMap::StringToWideString("<c2abFaAb"));
+  EXPECT_EQ(res, CPDF_ToUnicodeMap::StringToWideString("<c2abFaAb>"));
+  EXPECT_EQ(res, CPDF_ToUnicodeMap::StringToWideString("<c2abFaAb12>"));
 }
