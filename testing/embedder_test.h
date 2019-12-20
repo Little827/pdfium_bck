11dd687f4842859989c7d98350e35e2a0ddb413d
Aayush Dhir <aadhir@microsoft.com>
2019-12-20 16:57:19 +0530

    Sending focused annotation update to the host
    Adding a new method FFI_SetFocusAnnot() in FPDF_FORMFILLINFO with
    version = 2 which will send the focused annotation whenever it changes.
    This will be used to bring the focused annotation into view via
    scrolling.
    
    Bug: chromium:989027
    Change-Id: Ie0d1523f14234a6738ad1f390dd1deb09b03834d

--- testing/embedder_test.h
+++ testing/embedder_test.h
@@ -64,6 +64,11 @@ class EmbedderTest : public ::testing::Test,
 
     // Equivalent to FPDF_FORMFILLINFO::FFI_DoURIAction().
     virtual void DoURIAction(FPDF_BYTESTRING uri) {}
+
+    // Equivalent to FPDF_FORMFILLINFO::FFI_SetFocusAnnot().
+    virtual void SetFocusAnnot(FPDF_FORMFILLINFO* info,
+                               FPDF_ANNOTATION annot,
+                               int page_index) {}
   };
 
   EmbedderTest();
@@ -277,6 +282,9 @@ class EmbedderTest : public ::testing::Test,
                                      int page_index);
   static void DoURIActionTrampoline(FPDF_FORMFILLINFO* info,
                                     FPDF_BYTESTRING uri);
+  static void SetFocusAnnot(FPDF_FORMFILLINFO* info,
+                            FPDF_ANNOTATION annot,
+                            int page_index);
   static int WriteBlockCallback(FPDF_FILEWRITE* pFileWrite,
                                 const void* data,
                                 unsigned long size);
