89ef22869de9c28f6ef2bdb705a850b20667b88b
Tom Sepez <tsepez@chromium.org>
2020-01-03 15:19:32 -0800

    Remove CFX_Break::SetDefaultChar()
    It is uncalled. In turn, this makes m_wDefChar a constant, which
    is only used in a comparison which is now always true.  In turn,
    this makes w_iDefChar a zero constant. In turn, this makes
    FontChanged() a no-op, so remove it.
    
    Change-Id: I8c74a786ec344ee0cfd6bbd65145abe0d8ec273a

--- xfa/fgas/layout/cfx_break.cpp
+++ xfa/fgas/layout/cfx_break.cpp
@@ -13,9 +13,6 @@
 #include "third_party/base/stl_util.h"
 #include "xfa/fgas/font/cfgas_gefont.h"
 
-const float CFX_Break::kConversionFactor = 20000.0f;
-const int CFX_Break::kMinimumTabWidth = 160000;
-
 CFX_Break::CFX_Break(uint32_t dwLayoutStyles)
     : m_dwLayoutStyles(dwLayoutStyles), m_pCurLine(&m_Lines[0]) {}
 
@@ -58,7 +55,6 @@ void CFX_Break::SetFont(const RetainPtr<CFGAS_GEFont>& pFont) {
 
   SetBreakStatus();
   m_pFont = pFont;
-  FontChanged();
 }
 
 void CFX_Break::SetFontSize(float fFontSize) {
@@ -68,7 +64,6 @@ void CFX_Break::SetFontSize(float fFontSize) {
 
   SetBreakStatus();
   m_iFontSize = iFontSize;
-  FontChanged();
 }
 
 void CFX_Break::SetBreakStatus() {
@@ -91,34 +86,12 @@ FX_CHARTYPE CFX_Break::GetUnifiedCharType(FX_CHARTYPE chartype) const {
   return chartype >= FX_CHARTYPE::kArabicAlef ? FX_CHARTYPE::kArabic : chartype;
 }
 
-void CFX_Break::FontChanged() {
-  m_iDefChar = 0;
-  if (!m_pFont || m_wDefChar == 0xFEFF)
-    return;
-
-  m_pFont->GetCharWidth(m_wDefChar, &m_iDefChar);
-  m_iDefChar *= m_iFontSize;
-}
-
 void CFX_Break::SetTabWidth(float fTabWidth) {
   // Note, the use of max here was only done in the TxtBreak code. Leaving this
   // in for the RTFBreak code for consistency. If we see issues with tab widths
   // we may need to fix this.
   m_iTabWidth =
       std::max(FXSYS_roundf(fTabWidth * kConversionFactor), kMinimumTabWidth);
-}
-
-void CFX_Break::SetDefaultChar(wchar_t wch) {
-  m_wDefChar = wch;
-  m_iDefChar = 0;
-  if (m_wDefChar == 0xFEFF || !m_pFont)
-    return;
-
-  m_pFont->GetCharWidth(m_wDefChar, &m_iDefChar);
-  if (m_iDefChar < 0)
-    m_iDefChar = 0;
-  else
-    m_iDefChar *= m_iFontSize;
 }
 
 void CFX_Break::SetParagraphBreakChar(wchar_t wch) {
