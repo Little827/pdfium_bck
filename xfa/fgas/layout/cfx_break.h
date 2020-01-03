ba8a88043c2df4928a583c148dee524efddfd127
Tom Sepez <tsepez@chromium.org>
2020-01-03 15:19:32 -0800

    Remove CFX_Break::SetDefaultChar()
    It is uncalled. In turn, this makes m_wDefChar a constant, which
    is only used in a comparison which is now always true.  In turn,
    this makes w_iDefChar a zero constant.
    
    - Move some constexprs into header while at it.
    
    Change-Id: I8c74a786ec344ee0cfd6bbd65145abe0d8ec273a

--- xfa/fgas/layout/cfx_break.h
+++ xfa/fgas/layout/cfx_break.h
@@ -51,7 +51,6 @@ class CFX_Break {
 
   void SetCharSpace(float fCharSpace);
   void SetParagraphBreakChar(wchar_t wch);
-  void SetDefaultChar(wchar_t wch);
 
   int32_t CountBreakPieces() const;
   const CFX_BreakPiece* GetBreakPieceUnstable(int32_t index) const;
@@ -63,8 +62,8 @@ class CFX_Break {
   }
 
  protected:
-  static const int kMinimumTabWidth;
-  static const float kConversionFactor;
+  static constexpr int kMinimumTabWidth = 160000;
+  static constexpr float kConversionFactor = 20000.0f;
 
   explicit CFX_Break(uint32_t dwLayoutStyles);
 
@@ -87,15 +86,10 @@ class CFX_Break {
   int32_t m_iVerticalScale = 100;
   int32_t m_iTolerance = 0;
   int32_t m_iCharSpace = 0;
-  int32_t m_iDefChar = 0;
-  wchar_t m_wDefChar = 0xFEFF;
   RetainPtr<CFGAS_GEFont> m_pFont;
   UnownedPtr<CFX_BreakLine> m_pCurLine;
   int8_t m_iReadyLineIndex = -1;
   CFX_BreakLine m_Lines[2];
-
- private:
-  void FontChanged();
 };
 
 #endif  // XFA_FGAS_LAYOUT_CFX_BREAK_H_
