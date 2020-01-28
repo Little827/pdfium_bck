4b2472edb2d71e9ef553cfc7f0b62ca7675ce859
Neha Gupta <negupta@microsoft.com>
2020-01-28 11:24:28 +0530

    Add keyboard tabbing for links & highlights
    This CL contains following changes to support tabbing over links and
    highlights.
    1. To add the capability of tabbing over links & highlights along with
    widgets, CPDFSDK_Annotiterator should now accept vector of
    annotation subtypes (CPDF_Annot::Subtype :- widget + highlight + link),
    so that it can return vector of annotations of the given subtypes in
    the right order.
    
    2. Implementation of BAAnnotHandler - Invalidating rect during killing
    focus and setting focus. OnDraw() will draw the rect over the focused
    annotation.
    
    3. List of focusable annotation subtypes will be passed from host side
    (https://crrev.com/c/1783695) and pdfium will act according to the
    input annotation subtypes.
    
    4. CL related to test cases
    (https://pdfium-review.googlesource.com/c/pdfium/+/62950).
    
    Bug: chromium:994500
    Change-Id: I09fc4d9d902884ecd659b2573582fbd4992c29d3

--- fpdfsdk/fpdf_annot_embeddertest.cpp
+++ fpdfsdk/fpdf_annot_embeddertest.cpp
@@ -10,6 +10,7 @@
 
 #include "build/build_config.h"
 #include "constants/annotation_common.h"
+#include "core/fxcrt/fx_memory.h"
 #include "core/fxcrt/fx_system.h"
 #include "public/cpp/fpdf_scopers.h"
 #include "public/fpdf_annot.h"
@@ -2383,5 +2384,79 @@ TEST_F(FPDFAnnotEmbedderTest, GetFormFieldNameComboBox) {
                                               buf.data(), length_bytes));
     EXPECT_EQ(L"Combo_Editable", GetPlatformWString(buf.data()));
   }
+  UnloadPage(page);
+}
+
+TEST_F(FPDFAnnotEmbedderTest, SetFocusableAnnotSubtypes) {
+  ASSERT_TRUE(OpenDocument("text_form.pdf"));
+  FPDF_PAGE page = LoadPage(0);
+  ASSERT_TRUE(page);
+
+  constexpr FPDF_ANNOTATION_SUBTYPE kFocusableSubtypes[] = {FPDF_ANNOT_WIDGET};
+  constexpr unsigned int kSubtypeCount = FX_ArraySize(kFocusableSubtypes);
+
+  EXPECT_FALSE(FPDFAnnot_SetFocusableSubtypes(nullptr, kFocusableSubtypes,
+                                              kSubtypeCount));
+  EXPECT_FALSE(
+      FPDFAnnot_SetFocusableSubtypes(form_handle(), nullptr, kSubtypeCount));
+
+  EXPECT_TRUE(FPDFAnnot_SetFocusableSubtypes(form_handle(), kFocusableSubtypes,
+                                             kSubtypeCount));
+
+  int count = FPDFAnnot_GetFocusableSubtypesCount(form_handle());
+  EXPECT_EQ((int)kSubtypeCount, count);
+
+  UnloadPage(page);
+}
+
+TEST_F(FPDFAnnotEmbedderTest, GetFocusableAnnotSubtypesCount) {
+  ASSERT_TRUE(OpenDocument("text_form.pdf"));
+  FPDF_PAGE page = LoadPage(0);
+  ASSERT_TRUE(page);
+
+  EXPECT_EQ(-1, FPDFAnnot_GetFocusableSubtypesCount(NULL));
+
+  // FPDF_ANNOT_WIDGET is default annot subtype which is focusable,
+  // hence 1 is default count.
+  EXPECT_EQ(1, FPDFAnnot_GetFocusableSubtypesCount(form_handle()));
+
+  constexpr FPDF_ANNOTATION_SUBTYPE kFocusableSubtypes[] = {FPDF_ANNOT_WIDGET,
+                                                            FPDF_ANNOT_LINK};
+  constexpr unsigned int kSubtypeCount = FX_ArraySize(kFocusableSubtypes);
+
+  FPDFAnnot_SetFocusableSubtypes(form_handle(), kFocusableSubtypes,
+                                 kSubtypeCount);
+
+  EXPECT_EQ((int)kSubtypeCount,
+            FPDFAnnot_GetFocusableSubtypesCount(form_handle()));
+
+  UnloadPage(page);
+}
+
+TEST_F(FPDFAnnotEmbedderTest, GetFocusableAnnotSubtypes) {
+  ASSERT_TRUE(OpenDocument("text_form.pdf"));
+  FPDF_PAGE page = LoadPage(0);
+  ASSERT_TRUE(page);
+
+  constexpr FPDF_ANNOTATION_SUBTYPE kFocusableSubtypes[] = {FPDF_ANNOT_WIDGET};
+  constexpr unsigned int kSubtypeCount = FX_ArraySize(kFocusableSubtypes);
+
+  FPDFAnnot_SetFocusableSubtypes(form_handle(), kFocusableSubtypes,
+                                 kSubtypeCount);
+
+  int count = FPDFAnnot_GetFocusableSubtypesCount(form_handle());
+  EXPECT_FALSE(FPDFAnnot_GetFocusableSubtypes(form_handle(), nullptr, count));
+
+  std::vector<FPDF_ANNOTATION_SUBTYPE> set_subtypes(count, FPDF_ANNOT_UNKNOWN);
+  EXPECT_FALSE(FPDFAnnot_GetFocusableSubtypes(form_handle(),
+                                              set_subtypes.data(), count - 1));
+
+  EXPECT_TRUE(FPDFAnnot_GetFocusableSubtypes(form_handle(), set_subtypes.data(),
+                                             count));
+
+  for (int i = 0; i < count; ++i) {
+    EXPECT_EQ(kFocusableSubtypes[i], set_subtypes[i]);
+  }
+
   UnloadPage(page);
 }
