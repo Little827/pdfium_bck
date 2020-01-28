ad9e2a4238537e5dd94d1d08772695dc902f44b0
Lei Zhang <thestig@chromium.org>
2020-01-27 16:28:00 -0800

    Rename CPDFSDK_FormFillEnvironment::GetPermissions().
    GetPermissions() returns a bool rather than the actual permission value,
    so rename it to HasPermissions().
    
    Change-Id: Ifae4fb98b3ac0bf4d91755ab6d45181d6bad0398

--- fpdfsdk/cpdfsdk_formfillenvironment.h
+++ fpdfsdk/cpdfsdk_formfillenvironment.h
@@ -79,7 +79,7 @@ class CPDFSDK_FormFillEnvironment final : public Observable,
 
   // See PDF Reference 1.7, table 3.20 for the permission bits. Returns true if
   // and only if all the bits in |flags| are set.
-  bool GetPermissions(uint32_t flags) const;
+  bool HasPermissions(uint32_t flags) const;
 
   bool GetChangeMark() const { return m_bChangeMask; }
   void SetChangeMark() { m_bChangeMask = true; }
