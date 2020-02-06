a427dbc91cde428d64e985914f8936c72a08eccc
Gourab Kundu <gourabk@microsoft.com>
2020-02-06 16:48:56 +0530

    Add public API to support forced colors mode
    The CL adds the support for a forced color scheme which is applied
    when calling the new progressive render API.
    The color scheme is currently applied to only text and path.
    
    For text rendering, the color of the text (fill and stroke) is
    changed to the color mentioned in the color scheme.
    
    Similarly colors for fill and stroke can be set for path rendering.
    In addition to this there is an option to change the fill paths to
    strokes, which is required since with fill paths using a single
    color, a graphics with multiple adjacent fill paths will not be
    decipherable.
    
    All annotations also follow the above rules, except forms which
    are not part of this change, and will be taken up separately.
    
    There also needs to be a special handling for highlight since
    the Multiply blend mode leads to the highlights not being visible
    in some color schemes, which will be taken up separately.
    
    The above is achieved by the following changes:
    
    1. Adding a struct for the various graphics objects in a pdf for
    which a color can be specified and honored. The
    corresponding colors are set by the caller. The CL adds support
    for only fill and stroke color of paths and text. Subsequent CLs
    will add support for more objects.
    2. Adding a new API in the progressive rendering public APIs to
    pass in the forced color scheme from the caller.
    3. Adding a structure in CPDF_RenderOptions to keep a cache of the
    color scheme, and a setter method for the same.
    4. Adding an overloaded method for TranslateColor which will can
    translate the color of an object based on its type and render type
    to the corresponding forced color.
    5. Modifying CPDF_RenderStatus::GetFillArgbInternal() and
    CPDF_RenderStatus::GetStrokeArgb() to call the above added method for
    translating the color for stroke and fill paths.
    6. Modifying CPDF_RenderStatus::ProcessPathPattern() to add code to
    convert fill paths to stroke.
    7. Adding embedder test for testing rendering using the forced color
    scheme in public APIs with different content types.
    
    Bug: pdfium:1391
    Change-Id: I8b3f43eb0290d11b6a1ee2d5a36dfd1f0c0993e8

--- fpdfsdk/fpdf_view.cpp
+++ fpdfsdk/fpdf_view.cpp
@@ -496,6 +496,7 @@ FPDF_EXPORT void FPDF_CALLCONV FPDF_RenderPage(HDC dc,
     pContext->m_pDevice = pdfium::MakeUnique<CPDF_WindowsRenderDevice>(dc);
     CPDFSDK_RenderPageWithContext(pContext, pPage, start_x, start_y, size_x,
                                   size_y, rotate, flags,
+                                  /*color_scheme=*/nullptr,
                                   /*need_to_restore=*/true, /*pause=*/nullptr);
     return;
   }
@@ -514,7 +515,8 @@ FPDF_EXPORT void FPDF_CALLCONV FPDF_RenderPage(HDC dc,
   }
 
   CPDFSDK_RenderPageWithContext(pContext, pPage, start_x, start_y, size_x,
-                                size_y, rotate, flags, /*need_to_restore=*/true,
+                                size_y, rotate, flags, /*color_scheme=*/nullptr,
+                                /*need_to_restore=*/true,
                                 /*pause=*/nullptr);
 
   if (!bHasMask) {
@@ -556,7 +558,8 @@ FPDF_EXPORT void FPDF_CALLCONV FPDF_RenderPage(HDC dc,
   pContext->m_pOptions->GetOptions().bBreakForMasks = true;
 
   CPDFSDK_RenderPageWithContext(pContext, pPage, start_x, start_y, size_x,
-                                size_y, rotate, flags, /*need_to_restore=*/true,
+                                size_y, rotate, flags, /*color_scheme=*/nullptr,
+                                /*need_to_restore=*/true,
                                 /*pause=*/nullptr);
 
   // Render masks
@@ -598,7 +601,8 @@ FPDF_EXPORT void FPDF_CALLCONV FPDF_RenderPageBitmap(FPDF_BITMAP bitmap,
   RetainPtr<CFX_DIBitmap> pBitmap(CFXDIBitmapFromFPDFBitmap(bitmap));
   pDevice->Attach(pBitmap, !!(flags & FPDF_REVERSE_BYTE_ORDER), nullptr, false);
   CPDFSDK_RenderPageWithContext(pContext, pPage, start_x, start_y, size_x,
-                                size_y, rotate, flags, /*need_to_restore=*/true,
+                                size_y, rotate, flags, /*color_scheme=*/nullptr,
+                                /*need_to_restore=*/true,
                                 /*pause=*/nullptr);
 
 #ifdef _SKIA_SUPPORT_PATHS_
@@ -641,7 +645,8 @@ FPDF_RenderPageBitmapWithMatrix(FPDF_BITMAP bitmap,
   CFX_Matrix transform_matrix = pPage->GetDisplayMatrix(rect, 0);
   if (matrix)
     transform_matrix *= CFXMatrixFromFSMatrix(*matrix);
-  CPDFSDK_RenderPage(pContext, pPage, transform_matrix, clip_rect, flags);
+  CPDFSDK_RenderPage(pContext, pPage, transform_matrix, clip_rect, flags,
+                     /*color_scheme=*/nullptr);
 }
 
 #ifdef _SKIA_SUPPORT_
@@ -662,7 +667,9 @@ FPDF_EXPORT FPDF_RECORDER FPDF_CALLCONV FPDF_RenderPageSkp(FPDF_PAGE page,
   pContext->m_pDevice = std::move(skDevice);
 
   CPDFSDK_RenderPageWithContext(pContext, pPage, 0, 0, size_x, size_y, 0, 0,
+                                /*color_scheme=*/nullptr,
                                 /*need_to_restore=*/true, /*pause=*/nullptr);
+
   return recorder;
 }
 #endif  // _SKIA_SUPPORT_
