b1653a392dd2f75e78974a3805a9c5a54cc47476
Tom Sepez <tsepez@chromium.org>
2020-02-06 17:15:54 -0800

    Provide test for CJX_Packet methods from JavaScript
    Change-Id: Id7b8d002214018c46544e3d1460511f0fd7d4c76

--- testing/resources/javascript/constructor.js
+++ testing/resources/javascript/constructor.js
@@ -2,6 +2,29 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+function testLegalConstructor(name, allowed) {
+  const constructorString = name + ".constructor";
+  let constructor;
+  try {
+    constructor = eval(constructorString);
+  } catch (e) {
+    app.alert("FAIL: No such " + constructorString);
+    return;
+  }
+  try {
+    constructor();
+    app.alert("FAIL: " + constructorString + "(): returned");
+  } catch (e) {
+    app.alert("PASS: " + constructorString + "(): " + e);
+  }
+  try {
+    var thing = new constructor;
+    app.alert("PASS: new " + constructorString + ": " + thing);
+  } catch (e) {
+    app.alert("FAIL: new " + constructorString + ": " + e);
+  }
+}
+
 function testIllegalConstructor(name, allowed) {
   const constructorString = name + ".constructor";
   let constructor;
