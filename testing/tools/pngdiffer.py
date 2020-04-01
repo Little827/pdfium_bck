5b6d9e84be7f78d35050eb046777e5b84786076b
Hui Yingst <nigi@chromium.org>
2020-04-01 10:14:36 -0700

    Pack image comparing process into _ImageDiffError().
    In pngdiffer.py, the process of checking the rendering result against
    the expected result could happen more than once. Pack this process into
    function _ImageDiffError() to reduce the redundancy of the code.
    
    Change-Id: I904d501fcacdf8c6eb9d1f193918c592efc7b2b8

--- testing/tools/pngdiffer.py
+++ testing/tools/pngdiffer.py
@@ -43,6 +43,17 @@ class PNGDiffer():
 
     return actual_paths
 
+  def _ImageDiffError(self, expected_path, actual_path):
+    if os.path.exists(expected_path):
+      cmd = [self.pdfium_diff_path]
+      if self.reverse_byte_order:
+        cmd.append('--reverse-byte-order')
+      cmd.extend([expected_path, actual_path])
+      error = common.RunCommand(cmd)
+    else:
+      error = 1
+    return error
+
   def HasDifferences(self, input_filename, source_dir, working_dir):
     path_templates = PathTemplates(input_filename, source_dir, working_dir)
 
@@ -64,24 +75,10 @@ class PNGDiffer():
         break
       print "Checking " + actual_path
       sys.stdout.flush()
-      if os.path.exists(expected_path):
-        cmd = [self.pdfium_diff_path]
-        if self.reverse_byte_order:
-          cmd.append('--reverse-byte-order')
-        cmd.extend([expected_path, actual_path])
-        error = common.RunCommand(cmd)
-      else:
-        error = 1
-      if error:
+      if self._ImageDiffError(expected_path, actual_path):
         # When failed, we check against platform based results.
-        if os.path.exists(platform_expected_path):
-          cmd = [self.pdfium_diff_path]
-          if self.reverse_byte_order:
-            cmd.append('--reverse-byte-order')
-          cmd.extend([platform_expected_path, actual_path])
-          error = common.RunCommand(cmd)
-        if error:
-          print "FAILURE: " + input_filename + "; " + str(error)
+        if self._ImageDiffError(platform_expected_path, actual_path):
+          print "FAILURE: " + input_filename + "; 1"
           return True
 
     return False
