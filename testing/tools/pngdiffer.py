f65c8ca2a377334496f0a89f6a0398e906e75ce8
Hui Yingst <nigi@chromium.org>
2020-04-03 19:55:20 -0700

    Update pixel/corpus tests to handle expected results for Skia.
    With pdf_use_skia enabled, the rendering results can be correct but
    slightly different compared to AGG's results. This CL allows
    run_pixel_tests.py and run_corpus_tests.py to also compared to
    acceptable rendering results generated by Skia, named in the format of
    "NAME_expected(_skia)?(_(win|mac|linux))?.pdf.#.png". So that if the
    rendering result of a PDF is not completely the same as its AGG's
    expectation, as long as it finds a match with a Skia expected rendering
    result, it won't cause a failure in pixel/corpus test. This CL also
    provides skia-based and skia-platform-based expected results for
    testing the script change.
    
    In case that using SkiaPaths and using Skia generate different but both
    correct rendering results, we will need to add matching expected results
    for SkiaPaths in the format of
    "NAME_expected(_skiapaths)?(_(win|mac|linux))?.pdf.#.png" besides the
    Skia expected results.
    
    Bug: pdfium:1501
    Change-Id: I237c70bdec2640995f9cb8602d0b32addc0598e0

--- testing/tools/pngdiffer.py
+++ testing/tools/pngdiffer.py
@@ -13,6 +13,17 @@ import sys
 import common
 
 
+class CheckMode:
+  CHECK_DEFAULT = 0
+  CHECK_SKIA = 1
+  CHECK_SKIAPATHS = 2
+
+
+class MatchFound(Exception):
+  """Raised when a match is found from expected result(s)"""
+  pass
+
+
 class NotFoundError(Exception):
   """Raised when file doesn't exist"""
   pass
@@ -20,10 +31,12 @@ class NotFoundError(Exception):
 
 class PNGDiffer():
 
-  def __init__(self, finder, reverse_byte_order):
+  def __init__(self, finder, features, reverse_byte_order):
     self.pdfium_diff_path = finder.ExecutablePath('pdfium_diff')
     self.os_name = finder.os_name
     self.reverse_byte_order = reverse_byte_order
+    self.skiapaths_enabled = 'SKIAPATHS' in features
+    self.skia_enabled = 'SKIA' in features or self.skiapaths_enabled
 
   def CheckMissingTools(self, regenerate_expected):
     if (regenerate_expected and self.os_name == 'linux' and
@@ -37,15 +50,12 @@ class PNGDiffer():
 
     for page in itertools.count():
       actual_path = path_templates.GetActualPath(page)
-      expected_path = path_templates.GetExpectedPath(page)
-      platform_expected_path = path_templates.GetPlatformExpectedPath(
-          self.os_name, page)
-      if os.path.exists(platform_expected_path):
-        expected_path = platform_expected_path
-      elif not os.path.exists(expected_path):
+      expected_paths = path_templates.GetExpectedPaths(self.os_name, page,
+                                                       self.skia_enabled)
+      if any(itertools.imap(os.path.exists, expected_paths)):
+        actual_paths.append(actual_path)
+      else:
         break
-      actual_paths.append(actual_path)
-
     return actual_paths
 
   def _RunImageDiffCommand(self, expected_path, actual_path):
@@ -58,18 +68,44 @@ class PNGDiffer():
     cmd.extend([expected_path, actual_path])
     return common.RunCommand(cmd)
 
+  def _FindMatchProcessDone(self, input_filename, path_templates, page, mode):
+    actual_path = path_templates.GetActualPath(page)
+    expected_path = path_templates.GetExpectedPathByCheckMode(page, mode)
+    platform_expected_path = path_templates.GetPlatformExpectedPathByCheckMode(
+        page, self.os_name, mode)
+
+    error = self._RunImageDiffCommand(expected_path, actual_path)
+    if not error:
+      return MatchFound()
+
+    # When failed, we check against platform based results.
+    platform_error = self._RunImageDiffCommand(platform_expected_path,
+                                               actual_path)
+    if not platform_error:
+      return MatchFound()
+
+    # Update error code. No need to overwrite the previous error code if
+    # |platform_expected_path| doesn't exist.
+    if not isinstance(platform_error, NotFoundError):
+      error = platform_error
+
+    if mode == CheckMode.CHECK_SKIAPATHS or (
+        mode == CheckMode.CHECK_DEFAULT and
+        not self.skia_enabled) or (mode == CheckMode.CHECK_SKIA and
+                                   not self.skiapaths_enabled):
+      # Reach ending conditions, print error message.
+      print "FAILURE: " + input_filename + "; " + str(error)
+      return True
+
+    return False
+
   def HasDifferences(self, input_filename, source_dir, working_dir):
     path_templates = PathTemplates(input_filename, source_dir, working_dir)
-
     for page in itertools.count():
       actual_path = path_templates.GetActualPath(page)
-      expected_path = path_templates.GetExpectedPath(page)
-      # PDFium tests should be platform independent. Platform based results are
-      # used to capture platform dependent implementations.
-      platform_expected_path = path_templates.GetPlatformExpectedPath(
-          self.os_name, page)
-      if (not os.path.exists(expected_path) and
-          not os.path.exists(platform_expected_path)):
+      expected_paths = path_templates.GetExpectedPaths(self.os_name, page,
+                                                       self.skia_enabled)
+      if not any(itertools.imap(os.path.exists, expected_paths)):
         if page == 0:
           print "WARNING: no expected results files for " + input_filename
         if os.path.exists(actual_path):
@@ -80,19 +116,24 @@ class PNGDiffer():
       print "Checking " + actual_path
       sys.stdout.flush()
 
-      error = self._RunImageDiffCommand(expected_path, actual_path)
-      if error:
-        # When failed, we check against platform based results.
-        platform_error = self._RunImageDiffCommand(platform_expected_path,
-                                                   actual_path)
-        if platform_error:
-          if not isinstance(platform_error, NotFoundError):
-            error = platform_error
-          print "FAILURE: " + input_filename + "; " + str(error)
-          return True
+      mode = CheckMode.CHECK_DEFAULT
+      # Check through regular/skia/skiapaths expected results to find a match.
+      while mode <= CheckMode.CHECK_SKIAPATHS:
+        is_done = self._FindMatchProcessDone(input_filename, path_templates,
+                                             page, mode)
+        if not is_done:
+          mode = mode + 1
+          continue
+
+        if isinstance(is_done, MatchFound):
+          break
+
+        return True
 
     return False
 
+  # TODO(nigi): Add handling for automatically generate expected result for
+  # Skia/Skiapaths
   def Regenerate(self, input_filename, source_dir, working_dir, platform_only):
     path_templates = PathTemplates(input_filename, source_dir, working_dir)
 
@@ -103,8 +144,9 @@ class PNGDiffer():
       if not os.path.isfile(actual_path):
         break
 
-      platform_expected_path = path_templates.GetPlatformExpectedPath(
-          self.os_name, page)
+      platform_expected_path = \
+          path_templates.GetPlatformExpectedPathByCheckMode(
+              page, self.os_name, CheckMode.CHECK_DEFAULT)
 
       # If there is a platform expected png, we will overwrite it. Otherwise,
       # overwrite the generic png in "all" mode, or do nothing in "platform"
@@ -112,7 +154,8 @@ class PNGDiffer():
       if os.path.exists(platform_expected_path):
         expected_path = platform_expected_path
       elif not platform_only:
-        expected_path = path_templates.GetExpectedPath(page)
+        expected_path = path_templates.GetExpectedPathByCheckMode(
+            page, CheckMode.CHECK_DEFAULT)
       else:
         continue
 
@@ -121,9 +164,6 @@ class PNGDiffer():
 
 
 ACTUAL_TEMPLATE = '.pdf.%d.png'
-EXPECTED_TEMPLATE = '_expected' + ACTUAL_TEMPLATE
-PLATFORM_EXPECTED_TEMPLATE = '_expected_%s' + ACTUAL_TEMPLATE
-
 
 class PathTemplates(object):
 
@@ -131,16 +171,47 @@ class PathTemplates(object):
     input_root, _ = os.path.splitext(input_filename)
     self.actual_path_template = os.path.join(working_dir,
                                              input_root + ACTUAL_TEMPLATE)
-    self.expected_path = os.path.join(source_dir,
-                                      input_root + EXPECTED_TEMPLATE)
-    self.platform_expected_path = os.path.join(
-        source_dir, input_root + PLATFORM_EXPECTED_TEMPLATE)
+    self.source_dir = source_dir
+    self.input_root = input_root
+
+  def _GetExpectedTemplate(self, mode, platform):
+    if mode is CheckMode.CHECK_DEFAULT:
+      expected_str = '_expected'
+    elif mode is CheckMode.CHECK_SKIA:
+      expected_str = '_expected_skia'
+    else:
+      expected_str = '_expected_skiapaths'
+
+    if platform:
+      expected_str = expected_str + '_' + platform
+    return expected_str + ACTUAL_TEMPLATE
 
   def GetActualPath(self, page):
     return self.actual_path_template % page
 
-  def GetExpectedPath(self, page):
-    return self.expected_path % page
+  def GetExpectedPathByCheckMode(self, page, mode):
+    expected_template = self._GetExpectedTemplate(mode, None)
+    path = os.path.join(self.source_dir, self.input_root + expected_template)
+    return path % page
 
-  def GetPlatformExpectedPath(self, platform, page):
-    return self.platform_expected_path % (platform, page)
+  def GetPlatformExpectedPathByCheckMode(self, page, platform, mode):
+    expected_template = self._GetExpectedTemplate(mode, platform)
+    path = os.path.join(self.source_dir, self.input_root + expected_template)
+    return path % page
+
+  def GetExpectedPaths(self, platform, page, skia_enabled):
+    expected_paths = [
+        self.GetExpectedPathByCheckMode(page, CheckMode.CHECK_DEFAULT),
+        self.GetPlatformExpectedPathByCheckMode(page, platform,
+                                                CheckMode.CHECK_DEFAULT),
+    ]
+    if skia_enabled:
+      expected_paths.extend([
+          self.GetExpectedPathByCheckMode(page, CheckMode.CHECK_SKIA),
+          self.GetPlatformExpectedPathByCheckMode(page, platform,
+                                                  CheckMode.CHECK_SKIA),
+          self.GetExpectedPathByCheckMode(page, CheckMode.CHECK_SKIAPATHS),
+          self.GetPlatformExpectedPathByCheckMode(page, platform,
+                                                  CheckMode.CHECK_SKIAPATHS)
+      ])
+    return expected_paths
