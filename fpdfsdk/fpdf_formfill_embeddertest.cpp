bba882b98723d8d6b7b94e6e4bd4df093d7a93f4
Badhri Ravikumar <bravi@microsoft.com>
2020-04-06 17:02:24 +0530

    Added URI Action handling public API and its test cases
    Support for opening a link in new tab/window exists using mouse. But
    after setting focus over link by pressing Tab key, we do not have the
    provision to open the link in new tab/window using keyboard shortcut.
    This new API carries keyboard modifier value whenever URI action
    needs to be performed.
    
    Embedder tests are added to validate the added APIs
    
    Bug: 994500
    Change-Id: Id1bb8d6dd5c607bc5194d300250d8b796c7ff03c

--- fpdfsdk/fpdf_formfill_embeddertest.cpp
+++ fpdfsdk/fpdf_formfill_embeddertest.cpp
@@ -21,6 +21,7 @@
 #include "testing/gtest/include/gtest/gtest.h"
 
 using testing::_;
+using testing::InSequence;
 using testing::NiceMock;
 
 using FPDFFormFillEmbedderTest = EmbedderTest;
@@ -540,6 +541,7 @@ TEST_F(FPDFFormFillEmbedderTest, FirstTest) {
   EXPECT_CALL(mock, KillTimer(_)).Times(0);
   EXPECT_CALL(mock, OnFocusChange(_, _, _)).Times(0);
   EXPECT_CALL(mock, DoURIAction(_)).Times(0);
+  EXPECT_CALL(mock, DoURIActionWithKeyboardModifier(_, _, _)).Times(0);
   SetDelegate(&mock);
 
   EXPECT_TRUE(OpenDocument("hello_world.pdf"));
@@ -2685,6 +2687,13 @@ class FPDFFormFillActionUriTest : public EmbedderTest {
     ASSERT_TRUE(OpenDocument("annots_action_handling.pdf"));
     page_ = LoadPage(0);
     ASSERT_TRUE(page_);
+
+    // Set Widget and Link as supported tabbable annots.
+    constexpr FPDF_ANNOTATION_SUBTYPE kFocusableSubtypes[] = {FPDF_ANNOT_WIDGET,
+                                                              FPDF_ANNOT_LINK};
+    constexpr size_t kSubtypeCount = FX_ArraySize(kFocusableSubtypes);
+    ASSERT_TRUE(FPDFAnnot_SetFocusableSubtypes(
+        form_handle(), kFocusableSubtypes, kSubtypeCount));
   }
 
   void TearDown() override {
@@ -2692,10 +2701,14 @@ class FPDFFormFillActionUriTest : public EmbedderTest {
     EmbedderTest::TearDown();
   }
 
-  void SetFocusOnFirstForm() {
+  void SetFocusOnNthAnnot(size_t n) {
+    DCHECK_NE(n, 0);
+    // Setting focus on first annot.
     FORM_OnMouseMove(form_handle(), page(), /*modifier=*/0, 100, 680);
     FORM_OnLButtonDown(form_handle(), page(), /*modifier=*/0, 100, 680);
     FORM_OnLButtonUp(form_handle(), page(), /*modifier=*/0, 100, 680);
+    for (size_t i = 1; i < n; i++)
+      ASSERT_TRUE(FORM_OnKeyDown(form_handle(), page(), FWL_VKEY_Tab, 0));
   }
 
   FPDF_PAGE page() { return page_; }
@@ -2710,7 +2723,7 @@ TEST_F(FPDFFormFillActionUriTest, ButtonActionInvokeTest) {
   EXPECT_CALL(mock, DoURIAction(_)).Times(0);
   SetDelegate(&mock);
 
-  SetFocusOnFirstForm();
+  SetFocusOnNthAnnot(1);
 
   // Tab once from first form to go to button widget.
   ASSERT_TRUE(FORM_OnKeyDown(form_handle(), page(), FWL_VKEY_Tab, 0));
@@ -2718,4 +2731,28 @@ TEST_F(FPDFFormFillActionUriTest, ButtonActionInvokeTest) {
   // TODO(crbug.com/1028991): Following should be changed to ASSERT_TRUE after
   // handling key press implementation on buttons.
   ASSERT_FALSE(FORM_OnChar(form_handle(), page(), FWL_VKEY_Return, 0));
+}
+
+TEST_F(FPDFFormFillActionUriTest, LinkActionInvokeTest) {
+  NiceMock<EmbedderTestMockDelegate> mock;
+  {
+    InSequence sequence;
+    EXPECT_CALL(mock, DoURIAction(_)).Times(0);
+    // TODO(crbug.com/994500): Following comments has to be removed.
+    // EXPECT_CALL(mock, DoURIActionWithKeyboardModifier(_, _, 0));
+    // EXPECT_CALL(mock, DoURIActionWithKeyboardModifier(_, _, 2));
+    // EXPECT_CALL(mock, DoURIActionWithKeyboardModifier(_, _, 1));
+    // EXPECT_CALL(mock, DoURIActionWithKeyboardModifier(_, _, 3));
+  }
+  SetDelegate(&mock);
+  SetFocusOnNthAnnot(3);
+  int modifier = 0;
+  // TODO(crbug.com/994500): Following asserts to be changed to ASSERT_TRUE.
+  ASSERT_FALSE(FORM_OnKeyUp(form_handle(), page(), FWL_VKEY_Return, modifier));
+  modifier = FWL_EVENTFLAG_ControlKey;
+  ASSERT_FALSE(FORM_OnKeyUp(form_handle(), page(), FWL_VKEY_Return, modifier));
+  modifier = FWL_EVENTFLAG_ShiftKey;
+  ASSERT_FALSE(FORM_OnKeyUp(form_handle(), page(), FWL_VKEY_Return, modifier));
+  modifier |= FWL_EVENTFLAG_ControlKey;
+  ASSERT_FALSE(FORM_OnKeyUp(form_handle(), page(), FWL_VKEY_Return, modifier));
 }
