35e96dadb0563025977fb2b06aefa0a6ee1bb70b
Shikha Walia <shwali@microsoft.com>
2020-04-13 13:51:57 +0530

    Add API for removing an inklist for an ink annotation.
    There is no API to remove/edit an inklist for an ink annotation.
    With this API, user can edit an inklist by removing it and adding it
    again with edited ink points.
    
    This CL adds API and relevant tests for removing an inklist for an
    ink annotation.
    
    Bug: pdfium:1512
    Change-Id: I62fc232928ea720c8cef36adfb81fa20c0b10773

--- public/fpdf_annot.h
+++ public/fpdf_annot.h
@@ -211,6 +211,16 @@ FPDF_EXPORT int FPDF_CALLCONV FPDFAnnot_AddInkStroke(FPDF_ANNOTATION annot,
                                                      size_t point_count);
 
 // Experimental API.
+// Removes an InkList in |annot|.
+// This API works only for ink annotations.
+//
+//   annot  - handle to an annotation.
+//
+// Return true if successful.
+FPDF_EXPORT FPDF_BOOL FPDF_CALLCONV
+FPDFAnnot_RemoveInkList(FPDF_ANNOTATION annot);
+
+// Experimental API.
 // Add |obj| to |annot|. |obj| must have been created by
 // FPDFPageObj_CreateNew{Path|Rect}() or FPDFPageObj_New{Text|Image}Obj(), and
 // will be owned by |annot|. Note that an |obj| cannot belong to more than one
