29c65e668a698315a76462277b6bb09c3125c7fd
Lei Zhang <thestig@chromium.org>
2020-04-27 10:38:56 -0700

    Disable deflate TIFF support.
    Acrobat does not support deflate TIFF images, so do not bother.
    
    Change-Id: I75957c933dd011ea9c3199df3178b934a2d9b435

--- third_party/libtiff/tiffconf.h
+++ third_party/libtiff/tiffconf.h
@@ -217,7 +217,7 @@
 #define THUNDER_SUPPORT 1
 
 /* Support Deflate compression */
-#define ZIP_SUPPORT 1
+/* #define ZIP_SUPPORT 1 */
 
 /* Support strip chopping (whether or not to convert single-strip uncompressed
    images to mutiple strips of ~8Kb to reduce memory usage) */
