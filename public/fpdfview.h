910e6ce8692551a7293fbc1b1d85e3cf20f54d63
Hui Yingst <nigi@chromium.org>
2020-06-01 13:17:38 -0700

    Update a comment and add an embeddertest for FPDF_LCD_TEXT.
    LCD optimization can only be enabled when anti-aliasing is allowed.
    Therefore, this CL updates the comment for rendering flag FPDF_LCD_TEXT,
    and adds an embeddertest for using both FPDF_LCD_TEXT and
    FPDF_RENDER_NO_SMOOTHTEXT for rendering text to prove that LCD
    optimization is not applied to the rendering result since
    FPDF_RENDER_NO_SMOOTHTEXT disables anti-aliasing.
    
    Change-Id: Ied7f6b6359d5d1f51ff2c5be5ba0796ee36e6b22

--- public/fpdfview.h
+++ public/fpdfview.h
@@ -736,7 +736,8 @@ FPDF_EXPORT int FPDF_CALLCONV FPDF_GetPageSizeByIndex(FPDF_DOCUMENT document,
 //
 // Set if annotations are to be rendered.
 #define FPDF_ANNOT 0x01
-// Set if using text rendering optimized for LCD display.
+// Set if using text rendering optimized for LCD display. This flag will only
+// take effect if anti-aliasing is enabled for text.
 #define FPDF_LCD_TEXT 0x02
 // Don't use the native text output available on some platforms
 #define FPDF_NO_NATIVETEXT 0x04
