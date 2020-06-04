dd9c1b482a3a7fd27ce4717359e0d343dc5ebd6f
Hui Yingst <nigi@chromium.org>
2020-06-04 16:34:24 -0700

    Create struct CFX_TextRenderOptions to represent text rendering options.
    This CL creates a new structure CFX_RenderOptions, to represent the
    following option flags which are used for text rendering:
    
      FXFONT_CIDFONT
      FXTEXT_CLEARTYPE
      FXTEXT_BGR_STRIPE
      FXTEXT_PRINTGRAPHICTEXT
      FXTEXT_NO_NATIVETEXT
      FXTEXT_PRINTIMAGETEXT
      FXTEXT_NOSMOOTH
    
    This will help remove integer flag |text_flags| in DrawNormalText(),
    reduce related bitwise operations, and make it easier to change
    rendering options in the future.
    
    Bug: pdfium:1531
    Change-Id: I21cc9c57fcf745a01c0b9f519f01cbd9a674941c

---
+++ core/fxge/cfx_textrenderoptions.h
@@ -0,0 +1,45 @@
+// Copyright 2020 PDFium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CORE_FXGE_CFX_TEXTRENDEROPTIONS_H_
+#define CORE_FXGE_CFX_TEXTRENDEROPTIONS_H_
+
+struct CFX_TextRenderOptions {
+  // EdgingType defines the options for drawing text edge pixels. The values
+  // are defined in an incrementing order due to the latter edging type's
+  // dependency on the previous one.
+  enum EdgingType {
+    // No transparent pixels on glyph edges.
+    kAliasing = 0,
+
+    // May have transparent pixels on glyph edges.
+    kAntiAliasing,
+
+    // LCD optimization, can be enabled when anti-aliasing is allowed.
+    kLcd,
+
+    // BGR stripe optimization, can be enabled when LCD optimazation is enabled.
+    kBgrStripe,
+  };
+
+  CFX_TextRenderOptions();
+  CFX_TextRenderOptions(const CFX_TextRenderOptions& other);
+  ~CFX_TextRenderOptions();
+
+  // Font is CID font. False by default.
+  bool font_is_cid;
+
+  // Using the native text output available on some platforms. True by default.
+  bool native_text;
+
+  // Subpixel rendering option. kAntiAliasing by default.
+  EdgingType edging_type;
+
+  // Enabling LCD optimization for text rendering. This will automatically turn
+  // on anti aliasing. This method takes no effect if LCD optimiazation is
+  // already in use.
+  void EnableLcd();
+};
+
+#endif  // CORE_FXGE_CFX_TEXTRENDEROPTIONS_H_
