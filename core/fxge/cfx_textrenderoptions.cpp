54eeb23563e0d53b147fd79d9af1a8fed9cd7975
Hui Yingst <nigi@chromium.org>
2020-06-04 17:02:33 -0700

    Create struct CFX_TextRenderOptions to represent text rendering options.
    This CL creates a new structure CFX_RenderOptions, to represent the
    following option flags which are used for text rendering:
    
      FXFONT_CIDFONT
      FXTEXT_CLEARTYPE
      FXTEXT_BGR_STRIPE
      FXTEXT_NO_NATIVETEXT
      FXTEXT_NOSMOOTH
    
    This will help remove integer flag |text_flags| in DrawNormalText(),
    reduce related bitwise operations, and make it easier to change
    rendering options in the future.
    
    Bug: pdfium:1531
    Change-Id: I21cc9c57fcf745a01c0b9f519f01cbd9a674941c

---
+++ core/fxge/cfx_textrenderoptions.cpp
@@ -0,0 +1,16 @@
+// Copyright 2020 PDFium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/fxge/cfx_textrenderoptions.h"
+
+CFX_TextRenderOptions::CFX_TextRenderOptions()
+    : font_is_cid(false), native_text(true), edging_type(kAntiAliasing) {}
+
+CFX_TextRenderOptions::CFX_TextRenderOptions(EdgingType type)
+    : font_is_cid(false), native_text(true), edging_type(type) {}
+
+CFX_TextRenderOptions::CFX_TextRenderOptions(
+    const CFX_TextRenderOptions& other) = default;
+
+CFX_TextRenderOptions::~CFX_TextRenderOptions() = default;
