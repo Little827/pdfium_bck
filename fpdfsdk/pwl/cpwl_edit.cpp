1f556d5dac6bffad3b0aa756cf93831957fa558e
Hui Yingst <nigi@chromium.org>
2020-06-09 15:30:31 -0700

    Test CL
    Create class CFX_RenderOptions to replace some text rendering options.
    
    This CL creates a new class CFX_RenderOptions, which contains sub
    structs TextOptions and FontOptions, to represent the following
    rendering option flags which are used for text rendering:
    
    FXFONT_CIDFONT
    FXTEXT_CLEARTYPE
    FXTEXT_BGR_STRIPE
    FXTEXT_PRINTGRAPHICTEXT
    FXTEXT_NO_NATIVETEXT
    FXTEXT_PRINTIMAGETEXT
    FXTEXT_NOSMOOTH
    
    Change-Id: I8e46e2e8f4eb143bb5804e1af4413734b6844609

--- fpdfsdk/pwl/cpwl_edit.cpp
+++ fpdfsdk/pwl/cpwl_edit.cpp
@@ -195,7 +195,8 @@ void CPWL_Edit::DrawThisAppearance(CFX_RenderDevice* pDevice,
     }
     if (!path.GetPoints().empty()) {
       pDevice->DrawPath(&path, &mtUser2Device, &gsd, 0,
-                        GetBorderColor().ToFXColor(255), FXFILL_ALTERNATE);
+                        GetBorderColor().ToFXColor(255),
+                        CFX_FillRenderOptions(FXFILL_ALTERNATE));
     }
   }
 
