7741f5d48dacc9fab83a58b61405826b684d9edd
Tom Sepez <tsepez@chromium.org>
2020-06-15 12:00:36 -0700

    Add next Oilpan cppgc heap test.
    Create an object and track that it is destroyed by GC.
    
    Change-Id: I573787d0e413a40d2b0816f3bb2b93c5b1bdd6a9

--- fxjs/gc/heap_embeddertest.cpp
+++ fxjs/gc/heap_embeddertest.cpp
@@ -5,11 +5,44 @@
 #include "fxjs/gc/heap.h"
 
 #include <memory>
+#include <set>
 
 #include "testing/embedder_test.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/base/stl_util.h"
+#include "v8/include/cppgc/allocation.h"
 
-TEST_F(EmbedderTest, SeveralHeaps) {
+namespace {
+
+class PseudoCollectible;
+std::set<const PseudoCollectible*> s_live_;
+std::set<const PseudoCollectible*> s_dead_;
+
+class PseudoCollectible : public cppgc::GarbageCollected<PseudoCollectible> {
+ public:
+  static void Clear() {
+    s_live_.clear();
+    s_dead_.clear();
+  }
+  static size_t LiveCount() { return s_live_.size(); }
+  static size_t DeadCount() { return s_dead_.size(); }
+
+  PseudoCollectible() { s_live_.insert(this); }
+  virtual ~PseudoCollectible() {
+    s_live_.erase(this);
+    s_dead_.insert(this);
+  }
+
+  bool IsLive() const { return pdfium::Contains(s_live_, this); }
+
+  virtual void Trace(cppgc::Visitor*) const {}
+};
+
+}  // namespace
+
+class HeapEmbedderTest : public EmbedderTest {};
+
+TEST_F(HeapEmbedderTest, SeveralHeaps) {
   FXGCScopedHeap heap1 = FXGC_CreateHeap();
   EXPECT_TRUE(heap1);
 
@@ -18,4 +51,21 @@ TEST_F(EmbedderTest, SeveralHeaps) {
 
   FXGCScopedHeap heap3 = FXGC_CreateHeap();
   EXPECT_TRUE(heap2);
+}
+
+TEST_F(HeapEmbedderTest, NoReferences) {
+  FXGCScopedHeap heap1 = FXGC_CreateHeap();
+  ASSERT_TRUE(heap1);
+
+  PseudoCollectible* p =
+      cppgc::MakeGarbageCollected<PseudoCollectible>(heap1.get());
+  EXPECT_TRUE(p->IsLive());
+  EXPECT_EQ(1u, PseudoCollectible::LiveCount());
+  EXPECT_EQ(0u, PseudoCollectible::DeadCount());
+
+  p = nullptr;
+  heap1->ForceGarbageCollectionSlow("HeapEmbedderTest.NoReferences", "test");
+  EXPECT_EQ(0u, PseudoCollectible::LiveCount());
+  EXPECT_EQ(1u, PseudoCollectible::DeadCount());
+  PseudoCollectible::Clear();
 }
