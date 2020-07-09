33e09b65ae79a46303eca8d0db1ba20dc7cf1ce8
Tom Sepez <tsepez@chromium.org>
2020-07-08 17:12:38 -0700

    Make GCedEmbedderTest share resources with JSEmbedderTest.
    Remove duplicate setup and contention over same resources. Found
    while looking at chromium:1101152, but not its cause.
    
    Do the same for XFAJSEmbedderTest, moving some unusual setup to
    a new intermediate class.
    
    Change-Id: I1b7e664b9ec62114208a306899598660bf3207a0

---
+++ testing/external_engine_embedder_test.h
@@ -0,0 +1,32 @@
+// Copyright 2020 PDFium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef TESTING_EXTERNAL_ENGINE_EMBEDDER_TEST_H_
+#define TESTING_EXTERNAL_ENGINE_EMBEDDER_TEST_H_
+
+#include <memory>
+
+#include "fxjs/cfx_v8.h"
+#include "testing/js_embedder_test.h"
+#include "v8/include/v8.h"
+
+class CFXJS_Engine;
+
+class ExternalEngineEmbedderTest : public JSEmbedderTest {
+ public:
+  ExternalEngineEmbedderTest();
+  ~ExternalEngineEmbedderTest() override;
+
+  // EmbedderTest:
+  void SetUp() override;
+  void TearDown() override;
+
+  CFXJS_Engine* engine() const { return m_Engine.get(); }
+  v8::Local<v8::Context> GetV8Context();
+
+ private:
+  std::unique_ptr<CFXJS_Engine> m_Engine;
+};
+
+#endif  // TESTING_EXTERNAL_ENGINE_EMBEDDER_TEST_H_
