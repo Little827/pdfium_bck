a7d1ce91531d887267e59a9d80b62b91d7847f3f
Tom Sepez <tsepez@chromium.org>
2020-07-17 16:15:47 -0700

    Make ownership of CXFA_ThisProxy explicit (and sane).
    Keep a reference to it in the CXFJSE_Context for later clean-up
    rather than trying to retrieve it though bound V8 objects.
    
    -- Make constructor private while we're at it.
    
    Change-Id: Id80b7cb53b33ec30ed349b4fa0d01afe5f5456b9

--- fxjs/xfa/cfxjse_engine.cpp
+++ fxjs/xfa/cfxjse_engine.cpp
@@ -108,7 +108,8 @@ CFXJSE_Engine::CFXJSE_Engine(CXFA_Document* pDocument,
       m_pDocument(pDocument),
       m_JsContext(CFXJSE_Context::Create(fxjs_runtime->GetIsolate(),
                                          &GlobalClassDescriptor,
-                                         pDocument->GetRoot()->JSObject())),
+                                         pDocument->GetRoot()->JSObject(),
+                                         nullptr)),
       m_ResolveProcessor(std::make_unique<CFXJSE_ResolveProcessor>()) {
   RemoveBuiltInObjs(m_JsContext.get());
   m_JsContext->EnableCompatibleMode();
@@ -499,12 +500,10 @@ CFXJSE_Context* CFXJSE_Engine::CreateVariablesContext(CXFA_Node* pScriptNode,
   if (!pScriptNode || !pSubform)
     return nullptr;
 
-  // Ownership of |proxy| is maintained through v8 bindings, and is
-  // manually freed in ~CFXJE_Context() after re-obtaining the binding
-  // from v8.
-  auto* proxy = new CXFA_ThisProxy(pSubform, pScriptNode);
-  auto pNewContext = CFXJSE_Context::Create(
-      GetIsolate(), &VariablesClassDescriptor, proxy->JSObject());
+  auto proxy = std::make_unique<CXFA_ThisProxy>(pSubform, pScriptNode);
+  auto pNewContext =
+      CFXJSE_Context::Create(GetIsolate(), &VariablesClassDescriptor,
+                             proxy->JSObject(), std::move(proxy));
   RemoveBuiltInObjs(pNewContext.get());
   pNewContext->EnableCompatibleMode();
   CFXJSE_Context* pResult = pNewContext.get();
