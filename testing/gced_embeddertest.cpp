b368892f97f3a0c2d1e46c95da54314f316aaea2
Tom Sepez <tsepez@chromium.org>
2020-07-21 11:35:17 -0700

    Move class EmbedderTestEnvironment to standalone V8TestEnvironment.
    Change-Id: I25c205f3a9dc12854230ed851aebf7d3cb553ace

--- testing/gced_embeddertest.cpp
+++ testing/gced_embeddertest.cpp
@@ -5,13 +5,14 @@
 #include "testing/gced_embeddertest.h"
 
 #include "public/fpdfview.h"
+#include "testing/v8_test_environment.h"
 #include "v8/include/cppgc/allocation.h"
 #include "v8/include/cppgc/persistent.h"
 #include "v8/include/libplatform/libplatform.h"
 #include "v8/include/v8.h"
 
 void GCedEmbedderTest::PumpPlatformMessageLoop() {
-  v8::Platform* platform = EmbedderTestEnvironment::GetInstance()->platform();
+  v8::Platform* platform = V8TestEnvironment::GetInstance()->platform();
   while (v8::platform::PumpMessageLoop(platform, isolate()))
     continue;
 }
