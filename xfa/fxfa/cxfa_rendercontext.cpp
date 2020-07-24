1fa87269f2f0e239528ea3c947eb9118aaaaea86
Tom Sepez <tsepez@chromium.org>
2020-07-24 16:21:42 -0700

    Remove CXFA_RenderContext:m_pWidget.
    Unowned pointers to CXFA_FFWidgets have a habit of going stale, so
    avoid keeping one in this class, since we don't really need this
    state outside of a loop.
    
    Change-Id: I1ed8537de0a0a3b7168ea4cff4510f08b29814f2

--- xfa/fxfa/cxfa_rendercontext.cpp
+++ xfa/fxfa/cxfa_rendercontext.cpp
@@ -14,20 +14,21 @@ CXFA_RenderContext::CXFA_RenderContext(CXFA_FFPageView* pPageView,
                                        const CFX_Matrix& matrix)
     : m_pWidgetIterator(pPageView->CreateFormWidgetIterator(
           XFA_WidgetStatus_Visible | XFA_WidgetStatus_Viewable)),
-      m_pWidget(m_pWidgetIterator->MoveToNext()),
       m_matrix(matrix),
       m_ClipRect(clipRect) {}
 
 CXFA_RenderContext::~CXFA_RenderContext() = default;
 
 void CXFA_RenderContext::DoRender(CXFA_Graphics* gs) {
-  while (m_pWidget) {
-    CFX_RectF rtWidgetBox = m_pWidget->GetBBox(CXFA_FFWidget::kDoNotDrawFocus);
+  while (1) {
+    CXFA_FFWidget* pWidget = m_pWidgetIterator->MoveToNext();
+    if (!pWidget)
+      break;
+
+    CFX_RectF rtWidgetBox = pWidget->GetBBox(CXFA_FFWidget::kDoNotDrawFocus);
     ++rtWidgetBox.width;
     ++rtWidgetBox.height;
     if (rtWidgetBox.IntersectWith(m_ClipRect))
-      m_pWidget->RenderWidget(gs, m_matrix, CXFA_FFWidget::kHighlight);
-
-    m_pWidget = m_pWidgetIterator->MoveToNext();
+      pWidget->RenderWidget(gs, m_matrix, CXFA_FFWidget::kHighlight);
   }
 }
