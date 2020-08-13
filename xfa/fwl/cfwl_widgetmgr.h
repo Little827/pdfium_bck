686b5415db3917657d29600664e3c616a15270ec
Tom Sepez <tsepez@chromium.org>
2020-08-13 09:23:05 -0700

    Remove member CFWL_WidgetMgr::Item::pOffscreen.
    It is unused.
    
    Change-Id: I6553abc0836e70a4726f8caf2b545c87a57ad500

--- xfa/fwl/cfwl_widgetmgr.h
+++ xfa/fwl/cfwl_widgetmgr.h
@@ -70,9 +70,8 @@ class CFWL_WidgetMgr {
     explicit Item(CFWL_Widget* widget);
     ~Item() final;
 
-    Item* pOwner = nullptr;
+    Item* const pOwner = nullptr;
     CFWL_Widget* const pWidget;
-    std::unique_ptr<CXFA_Graphics> pOffscreen;
     int32_t iRedrawCounter = 0;
   };
 
