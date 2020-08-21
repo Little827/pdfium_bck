07c9398f88a99322f79d36aed3ef42d45e080d92
Tom Sepez <tsepez@chromium.org>
2020-08-21 12:01:34 -0700

    Introduce class EmbedderTestEnvironment
    Then initialize the library once per binary execution rather than
    once per test. Should a clean library initialization be required for
    a specific test, then unload/reload the library in that specific test.
    
    Consequently, isolate creation takes place in EmbedderTestEnvironment,
    rather than in EmbedderTest.
    
    This lays the groundwork for making some of the XFA tests faster,
    but more importantly will allow for a split in CFGAS between a
    per-module singleton and a GC'd font mgr down the road.
    
    Change-Id: I528005fbecd061453060ff33f077414020f5987f

---
+++ testing/embedder_test_environment.h
@@ -0,0 +1,20 @@
+// Copyright 2020 PDFium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef TESTING_EMBEDDER_TEST_ENVIRONMENT_H_
+#define TESTING_EMBEDDER_TEST_ENVIRONMENT_H_
+
+#include "public/fpdfview.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+class EmbedderTestEnvironment : public testing::Environment {
+ public:
+  EmbedderTestEnvironment();
+  ~EmbedderTestEnvironment() override;
+
+  void SetUp() override;
+  void TearDown() override;
+};
+
+#endif  // TESTING_EMBEDDER_TEST_ENVIRONMENT_H_
