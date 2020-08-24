15b49d588aab6e49f41048a9e15a3ee5f8201759
Tom Sepez <tsepez@chromium.org>
2020-08-24 11:56:12 -0700

    Introduce class EmbedderTestEnvironment
    Then initialize the library once per binary execution rather than
    once per test. Should a clean library initialization be required for
    a specific test, then unload/reload the library in that specific test.
    
    Consequently, isolate creation takes place in EmbedderTestEnvironment,
    rather than in EmbedderTest.
    
    This lays the groundwork for making some of the XFA tests faster,
    but more importantly will allow for a split in CFGAS between a
    per-module singleton and a GC'd font mgr down the road.
    
    -- Make fx_win32_device_embeddertest a proper embedder test
    
    Change-Id: I528005fbecd061453060ff33f077414020f5987f

--- testing/js_embedder_test.h
+++ testing/js_embedder_test.h
@@ -7,7 +7,6 @@
 
 #include <memory>
 
-#include "fxjs/cfx_v8.h"
 #include "testing/embedder_test.h"
 #include "v8/include/v8.h"
 
@@ -16,15 +15,7 @@ class JSEmbedderTest : public EmbedderTest {
   JSEmbedderTest();
   ~JSEmbedderTest() override;
 
-  // EmbedderTest:
-  void SetUp() override;
-  void TearDown() override;
-
-  v8::Isolate* isolate() const { return m_pIsolate.get(); }
-
- private:
-  std::unique_ptr<CFX_V8ArrayBufferAllocator> m_pArrayBufferAllocator;
-  std::unique_ptr<v8::Isolate, CFX_V8IsolateDeleter> m_pIsolate;
+  v8::Isolate* isolate() const;
 };
 
 #endif  // TESTING_JS_EMBEDDER_TEST_H_
