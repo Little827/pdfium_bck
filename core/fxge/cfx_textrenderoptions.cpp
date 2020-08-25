23dc65b93c95c191d207c301b9276bed805a201e
Lei Zhang <thestig@chromium.org>
2020-08-25 10:49:01 -0700

    Construct LcdOptions() return value at compile time.
    CFX_TextRenderOptions::LcdOptions() currently constructs its return
    value on first use. Instead, construct it at compile time by adding a
    constexpr specifier to the one-param CFX_TextRenderOptions ctor.
    
    Change-Id: Iae7024fba988bf7118053e1ec46ad520eb690fab

--- core/fxge/cfx_textrenderoptions.cpp
+++ core/fxge/cfx_textrenderoptions.cpp
@@ -4,18 +4,13 @@
 
 #include "core/fxge/cfx_textrenderoptions.h"
 
-#include "third_party/base/no_destructor.h"
-
 // static
 const CFX_TextRenderOptions& CFX_TextRenderOptions::LcdOptions() {
-  static pdfium::base::NoDestructor<CFX_TextRenderOptions> instance(kLcd);
-  return *instance;
+  static constexpr CFX_TextRenderOptions kInstance(kLcd);
+  return kInstance;
 }
 
 CFX_TextRenderOptions::CFX_TextRenderOptions() = default;
-
-CFX_TextRenderOptions::CFX_TextRenderOptions(AliasingType type)
-    : aliasing_type(type) {}
 
 CFX_TextRenderOptions::CFX_TextRenderOptions(
     const CFX_TextRenderOptions& other) = default;
