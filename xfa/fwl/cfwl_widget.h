99cd5fb6fc6c1a0baeed725ec0c2e55281112de8
Tom Sepez <tsepez@chromium.org>
2020-08-28 16:14:40 -0700

    Convert more FWL objects over to Oilpan GC.
    In turn, some CFXA objects above them need to become GC'd too in
    order to avoid cycles back through Persistent<>.
    
    -- Fix some headers/IWYU to make msvc happy.
    
    Bug: pdfium:1563
    Change-Id: Ib238be056c908e96cbd04104520778740f6a9b0f

--- xfa/fwl/cfwl_widget.h
+++ xfa/fwl/cfwl_widget.h
@@ -53,7 +53,6 @@ enum class FWL_Type {
 
 // NOTE: CFWL_Widget serves as its own delegate until replaced at runtime.
 class CFWL_Widget : public cppgc::GarbageCollected<CFWL_Widget>,
-                    public Observable,
                     public IFWL_WidgetDelegate {
   CPPGC_USING_PRE_FINALIZER(CFWL_Widget, PreFinalize);
 
@@ -118,7 +117,7 @@ class CFWL_Widget : public cppgc::GarbageCollected<CFWL_Widget>,
   bool IsPopup() const;
   bool IsChild() const;
 
-  CFWL_WidgetMgr* GetWidgetMgr() const { return m_pWidgetMgr.Get(); }
+  CFWL_WidgetMgr* GetWidgetMgr() const { return m_pWidgetMgr; }
   CFWL_Widget* GetOuter() const { return m_pOuter; }
   CFWL_Widget* GetOutmost() const;
 
@@ -137,7 +136,7 @@ class CFWL_Widget : public cppgc::GarbageCollected<CFWL_Widget>,
     return m_pDelegate ? m_pDelegate.Get() : this;
   }
 
-  const CFWL_App* GetFWLApp() const { return m_pFWLApp.Get(); }
+  CFWL_App* GetFWLApp() const { return m_pFWLApp.Get(); }
   uint64_t GetEventKey() const { return m_nEventKey; }
   void SetEventKey(uint64_t key) { m_nEventKey = key; }
 
@@ -146,9 +145,7 @@ class CFWL_Widget : public cppgc::GarbageCollected<CFWL_Widget>,
   void RepaintRect(const CFX_RectF& pRect);
 
  protected:
-  CFWL_Widget(const CFWL_App* app,
-              const Properties& properties,
-              CFWL_Widget* pOuter);
+  CFWL_Widget(CFWL_App* app, const Properties& properties, CFWL_Widget* pOuter);
 
   bool IsEnabled() const;
   bool IsLocked() const { return m_iLock > 0; }
@@ -191,8 +188,8 @@ class CFWL_Widget : public cppgc::GarbageCollected<CFWL_Widget>,
 
   int32_t m_iLock = 0;
   uint64_t m_nEventKey = 0;
-  UnownedPtr<const CFWL_App> const m_pFWLApp;
-  UnownedPtr<CFWL_WidgetMgr> const m_pWidgetMgr;
+  cppgc::Member<CFWL_App> const m_pFWLApp;
+  cppgc::Member<CFWL_WidgetMgr> const m_pWidgetMgr;
   cppgc::Member<CFWL_Widget> const m_pOuter;
   AdapterIface* m_pAdapterIface = nullptr;
   cppgc::Member<IFWL_WidgetDelegate> m_pDelegate;
