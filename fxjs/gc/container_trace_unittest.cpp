db1adbbc2093c07cd5a61b2488d6ff5ef013179f
Tom Sepez <tsepez@chromium.org>
2020-09-01 12:59:52 -0700

    Add specialized templates to help tracing stl containers.
    We need to be able to trace zero-initialized memory, it would seem.
    
    Change-Id: Ifd68f0ac3f361078f8868ee58a48c4276dc233b6

---
+++ fxjs/gc/container_trace_unittest.cpp
@@ -0,0 +1,149 @@
+// Copyright 2020 PDFium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "fxjs/gc/container_trace.h"
+
+#include <list>
+#include <map>
+#include <set>
+#include <vector>
+
+#include "testing/gtest/include/gtest/gtest.h"
+#include "v8/include/cppgc/member.h"
+
+namespace {
+
+class Thing : public cppgc::GarbageCollected<Thing> {
+ public:
+  void Trace(cppgc::Visitor* visitor) const {}
+};
+
+class CountingVisitor {
+ public:
+  CountingVisitor() = default;
+
+  void Trace(const void* that) { ++call_count_; }
+  int call_count() const { return call_count_; }
+
+ private:
+  int call_count_ = 0;
+};
+
+}  // namespace
+
+TEST(ContainerTrace, ZeroedListQuirk) {
+  std::list<cppgc::Member<Thing>> thing;
+  memset(&thing, 0, sizeof(thing));
+
+  // Can trust emtpy().
+  EXPECT_TRUE(thing.empty());
+
+  // Can't trust iterators.
+  EXPECT_NE(thing.begin(), thing.end());
+}
+
+TEST(ContainerTrace, ZeroedMapQuirk) {
+  std::map<int, cppgc::Member<Thing>> thing;
+  memset(&thing, 0, sizeof(thing));
+
+  // Can trust emtpy().
+  EXPECT_TRUE(thing.empty());
+
+  // Can't trust iterators.
+  EXPECT_NE(thing.begin(), thing.end());
+}
+
+TEST(ContainerTrace, ZeroedSetQuirk) {
+  std::set<cppgc::Member<Thing>> thing;
+  memset(&thing, 0, sizeof(thing));
+
+  // Can trust emtpy().
+  EXPECT_TRUE(thing.empty());
+
+  // Can't trust iterators.
+  EXPECT_NE(thing.begin(), thing.end());
+}
+
+TEST(ContainerTrace, ZeroedVectorQuirk) {
+  std::vector<cppgc::Member<Thing>> thing;
+  memset(&thing, 0, sizeof(thing));
+
+  // Can trust emtpy().
+  EXPECT_TRUE(thing.empty());
+
+  // Can trust vector iterators.
+  EXPECT_EQ(thing.begin(), thing.end());
+}
+
+TEST(ContainerTrace, ZeroedListTrace) {
+  std::list<cppgc::Member<Thing>> thing;
+  memset(&thing, 0, sizeof(thing));
+
+  CountingVisitor cv;
+  ContainerTrace(&cv, thing);
+  EXPECT_EQ(0, cv.call_count());
+}
+
+TEST(ContainerTrace, ZeroedMapTrace) {
+  std::map<int, cppgc::Member<Thing>> thing;
+  memset(&thing, 0, sizeof(thing));
+
+  CountingVisitor cv;
+  ContainerTrace(&cv, thing);
+  EXPECT_EQ(0, cv.call_count());
+}
+
+TEST(ContainerTrace, ZeroedSetTrace) {
+  std::set<cppgc::Member<Thing>> thing;
+  memset(&thing, 0, sizeof(thing));
+
+  CountingVisitor cv;
+  ContainerTrace(&cv, thing);
+  EXPECT_EQ(0, cv.call_count());
+}
+
+TEST(ContainerTrace, ZeroedVectorTrace) {
+  std::vector<cppgc::Member<Thing>> thing;
+  memset(&thing, 0, sizeof(thing));
+
+  CountingVisitor cv;
+  ContainerTrace(&cv, thing);
+  EXPECT_EQ(0, cv.call_count());
+}
+
+TEST(ContainerTrace, ActualListTrace) {
+  std::list<cppgc::Member<Thing>> thing;
+  thing.emplace_back(nullptr);
+
+  CountingVisitor cv;
+  ContainerTrace(&cv, thing);
+  EXPECT_EQ(1, cv.call_count());
+}
+
+TEST(ContainerTrace, ActualMapTrace) {
+  std::map<int, cppgc::Member<Thing>> thing;
+  thing[42] = nullptr;
+
+  CountingVisitor cv;
+  ContainerTrace(&cv, thing);
+  EXPECT_EQ(1, cv.call_count());
+}
+
+TEST(ContainerTrace, ActualSetTrace) {
+  std::set<cppgc::Member<Thing>> thing;
+  thing.insert(nullptr);
+
+  CountingVisitor cv;
+  ContainerTrace(&cv, thing);
+  EXPECT_EQ(1, cv.call_count());
+}
+
+TEST(ContainerTrace, ActualVectorTrace) {
+  std::vector<cppgc::Member<Thing>> thing;
+  thing.emplace_back(nullptr);
+
+  CountingVisitor cv;
+  ContainerTrace(&cv, thing);
+  EXPECT_EQ(1, cv.call_count());
+}
