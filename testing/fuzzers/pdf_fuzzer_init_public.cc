1ff86f75887154bbbbde0e0d47a56441e2511d86
Tom Sepez <tsepez@chromium.org>
2020-09-14 11:10:10 -0700

    Rework pdf_fuzzer_init_public.cc
    Allow fuzzers to access globally-created resources, specifically
    the cppgc garbage collector heap, for future fuzzing of GC'd
    objects. This is complicated by the component build, where the
    fuzzers's LLVMFuzzerTestOneInput() entry points live in the pdfium.so
    library under different names, with the public init occurring in a
    small shim above that.
    
    -- Introduce a .h file for use by the fuzzers.
    -- Rename class to match file (actual name unimportant)
    -- Create an external heap when XFA is present.
    -- Remove wrong comment about static variables being the same.
    
    Change-Id: I97ba63dd19f5317f1635cbf3711c3551c3794852

--- testing/fuzzers/pdf_fuzzer_init_public.cc
+++ testing/fuzzers/pdf_fuzzer_init_public.cc
@@ -2,18 +2,16 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "testing/fuzzers/pdf_fuzzer_init_public.h"
+
 #include <string.h>
 
-#include <memory>
-
-#include "public/fpdf_ext.h"
+#include "testing/fuzzers/pdfium_fuzzer_util.h"
 
 #ifdef PDF_ENABLE_V8
 #include "testing/free_deleter.h"
 #include "testing/v8_initializer.h"
-#include "v8/include/v8-platform.h"
-#include "v8/include/v8.h"
-#endif
+#endif  // PDF_ENABLE_V8
 
 #ifdef _WIN32
 #include <windows.h>
@@ -26,10 +24,13 @@
 
 namespace {
 
+// pdf_fuzzer_init.cc and pdfium_fuzzer_init_public.cc are mutually exclusive
+// and should not be built together.
+PDFFuzzerInitPublic* g_instance = new PDFFuzzerInitPublic();
+
 #ifdef PDF_ENABLE_V8
 std::string ProgramPath() {
   std::string result;
-
 #ifdef _WIN32
   char path[MAX_PATH];
   DWORD len = GetModuleFileNameA(nullptr, path, MAX_PATH);
@@ -56,44 +57,41 @@ std::string ProgramPath() {
 
 }  // namespace
 
-// Initialize the library once for all runs of the fuzzer.
-struct TestCase {
-  TestCase() {
+PDFFuzzerPublic::PDFFuzzerPublic() {
 #ifdef PDF_ENABLE_V8
 #ifdef V8_USE_EXTERNAL_STARTUP_DATA
-    platform = InitializeV8ForPDFiumWithStartupData(
-        ProgramPath(), std::string(), std::string(), &snapshot_blob);
-#else
-    platform = InitializeV8ForPDFium(ProgramPath(), std::string());
+  platform = InitializeV8ForPDFiumWithStartupData(
+      ProgramPath(), std::string(), std::string(), &snapshot_blob);
+#else   // V8_USE_EXTERNAL_STARTUP_DATA
+  platform = InitializeV8ForPDFium(ProgramPath(), std::string());
 #endif  // V8_USE_EXTERNAL_STARTUP_DATA
+#ifdef PDF_ENABLE_XFA
+  FXGC_Initialize(platform.get(), nullptr);
+  heap = FXGC_CreateHeap();
+#endif  // PDF_ENABLE_XFA
 #endif  // PDF_ENABLE_V8
 
-    memset(&config, '\0', sizeof(config));
-    config.version = 3;
-    config.m_pUserFontPaths = nullptr;
-    config.m_pIsolate = nullptr;
-    config.m_v8EmbedderSlot = 0;
+  memset(&config, '\0', sizeof(config));
+  config.version = 3;
+  config.m_pUserFontPaths = nullptr;
+  config.m_pIsolate = nullptr;
+  config.m_v8EmbedderSlot = 0;
 #ifdef PDF_ENABLE_V8
-    config.m_pPlatform = platform.get();
+  config.m_pPlatform = platform.get();
 #endif  // PDF_ENABLE_V8
-    FPDF_InitLibraryWithConfig(&config);
+  FPDF_InitLibraryWithConfig(&config);
 
-    memset(&unsupport_info, '\0', sizeof(unsupport_info));
-    unsupport_info.version = 1;
-    unsupport_info.FSDK_UnSupport_Handler = [](UNSUPPORT_INFO*, int) {};
-    FSDK_SetUnSpObjProcessHandler(&unsupport_info);
-  }
+  memset(&unsupport_info, '\0', sizeof(unsupport_info));
+  unsupport_info.version = 1;
+  unsupport_info.FSDK_UnSupport_Handler = [](UNSUPPORT_INFO*, int) {};
+  FSDK_SetUnSpObjProcessHandler(&unsupport_info);
+}
 
-#ifdef PDF_ENABLE_V8
-  std::unique_ptr<v8::Platform> platform;
-  v8::StartupData snapshot_blob;
-#endif
+PDFFuzzerPublic::~PDFFuzzerPublic() = default;
 
-  FPDF_LIBRARY_CONFIG config;
-  UNSUPPORT_INFO unsupport_info;
-};
+PDFFuzzerInitPublic::PDFFuzzerInitPublic()
+    : context_(std::make_unique<PDFFuzzerPublic>()) {
+  FPDF_SetFuzzerPerProcessState(context_.get());
+}
 
-// pdf_fuzzer_init.cc and pdfium_fuzzer_init_public.cc are mutually exclusive
-// and should not be built together. They deliberately have the same global
-// variable.
-static TestCase* g_test_case = new TestCase();
+PDFFuzzerInitPublic::~PDFFuzzerInitPublic() = default;
