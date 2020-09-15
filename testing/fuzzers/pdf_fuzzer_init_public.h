82b9b99d9a718f906af793b3a50928929901e8ab
Tom Sepez <tsepez@chromium.org>
2020-09-14 17:08:49 -0700

    Make CXFA_FMExpression and its subclasses be garbage collected.
    Bug: pdfium:1563
    Change-Id: I32c494387c6b311f65c9dbcedd42e409fb0a9cc9

---
+++ testing/fuzzers/pdf_fuzzer_init_public.h
@@ -0,0 +1,52 @@
+// Copyright 2020 The PDFium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef TESTING_FUZZERS_PDF_FUZZER_INIT_PUBLIC_H_
+#define TESTING_FUZZERS_PDF_FUZZER_INIT_PUBLIC_H_
+
+#include <memory>
+
+#include "public/fpdf_ext.h"
+#include "public/fpdfview.h"
+
+#ifdef PDF_ENABLE_V8
+#include "v8/include/v8-platform.h"
+#include "v8/include/v8.h"
+#ifdef PDF_ENABLE_XFA
+#include "fxjs/gc/heap.h"
+#endif  // PDF_ENABLE_XFA
+#endif  // PDF_ENABLE_V8
+
+// Context for all runs of the fuzzer.
+struct PDFFuzzerPublic {
+  PDFFuzzerPublic();
+  ~PDFFuzzerPublic();
+
+#ifdef PDF_ENABLE_V8
+#ifdef PDF_ENABLE_XFA
+  cppgc::Heap* GetHeap() { return heap.get(); }
+#endif  // PDF_ENABLE_XFA
+#endif  // PDF_ENABLE_V8
+
+ private:
+  FPDF_LIBRARY_CONFIG config;
+  UNSUPPORT_INFO unsupport_info;
+#ifdef PDF_ENABLE_V8
+  std::unique_ptr<v8::Platform> platform;
+  v8::StartupData snapshot_blob;
+#ifdef PDF_ENABLE_XFA
+  FXGCScopedHeap heap;
+#endif  // PDF_ENABLE_XFA
+#endif  // PDF_ENABLE_V8
+};
+
+// Initializes the library once for all runs of the fuzzer.
+struct PDFFuzzerInitPublic {
+  PDFFuzzerInitPublic();
+  ~PDFFuzzerInitPublic();
+
+  std::unique_ptr<PDFFuzzerPublic> context_;
+};
+
+#endif  // TESTING_FUZZERS_PDF_FUZZER_INIT_PUBLIC_H_
