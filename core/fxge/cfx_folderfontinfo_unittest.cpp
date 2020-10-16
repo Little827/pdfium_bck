bcc9eec544ed79cdef273d7b6a9fe59853d6fa02
Hui Yingst <nigi@chromium.org>
2020-10-15 23:02:02 -0700

    Improve font family name matching method.
    When looking for substitute fonts for a given font family name,
    CFX_FolderFontInfo::FindFont() simply checks whether this family name
    is a substring of any installed font to decide whether an installed
    font is a potential candidate. This will cause false matches when the
    given family name is a substring of the installed font's family name.
    For example, "Univers" is a substring of "Universal" but they are not
    in the same font family.
    
    This CL adds a helper function FindFamilyNameMatch() to check whether
    the next following character after the matching substring is lower case
    to decide whether the installed font's family name is fully covered. If
    the next character is lower case, then consider it a family name
    mismatch.
    
    Bug: chromium:1135949
    Change-Id: If63c9eeb459671c084c0502154219738754f1ff8

--- core/fxge/cfx_folderfontinfo_unittest.cpp
+++ core/fxge/cfx_folderfontinfo_unittest.cpp
@@ -17,6 +17,13 @@ constexpr char kSymbol[] = "Symbol";
 constexpr char kBookshelfSymbol7[] = "Bookshelf Symbol 7";
 constexpr char kCalibri[] = "Calibri";
 constexpr char kBookshelf[] = "Bookshelf";
+constexpr char kBook[] = "Book";
+constexpr char kTofuBold[] = "Tofu, Bold Italic";
+constexpr char kTofu[] = "Tofu";
+constexpr char kLatoUltraBold[] = "Lato Ultra-Bold";
+constexpr char kLato[] = "Lato";
+constexpr char kOxygenSansSansBold[] = "Oxygen-Sans Sans-Bold";
+constexpr char kOxygenSans[] = "Oxygen-Sans";
 
 }  // namespace
 
@@ -40,12 +47,31 @@ class CFX_FolderFontInfoTest : public ::testing::Test {
         /*filePath=*/"", kSymbol, /*fontTables=*/"",
         /*fontOffset=*/0, /*fileSize=*/0);
     symbol_info->m_Charsets = 2;
+    auto tofu_bold_info = std::make_unique<CFX_FolderFontInfo::FontFaceInfo>(
+        /*filePath=*/"", kTofuBold, /*fontTables=*/"",
+        /*fontOffset=*/0, /*fileSize=*/0);
+    tofu_bold_info->m_Charsets = 2;
+    auto lato_ultra_bold_info =
+        std::make_unique<CFX_FolderFontInfo::FontFaceInfo>(
+            /*filePath=*/"", kLatoUltraBold, /*fontTables=*/"",
+            /*fontOffset=*/0, /*fileSize=*/0);
+    lato_ultra_bold_info->m_Charsets = 2;
+
+    auto oxygen_sans_sans_bold_info =
+        std::make_unique<CFX_FolderFontInfo::FontFaceInfo>(
+            /*filePath=*/"", kOxygenSansSansBold, /*fontTables=*/"",
+            /*fontOffset=*/0, /*fileSize=*/0);
+    oxygen_sans_sans_bold_info->m_Charsets = 2;
 
     font_info_.m_FontList[kArial] = std::move(arial_info);
     font_info_.m_FontList[kTimesNewRoman] = std::move(times_new_roman_info);
     font_info_.m_FontList[kBookshelfSymbol7] =
         std::move(bookshelf_symbol7_info);
     font_info_.m_FontList[kSymbol] = std::move(symbol_info);
+    font_info_.m_FontList[kTofuBold] = std::move(tofu_bold_info);
+    font_info_.m_FontList[kLatoUltraBold] = std::move(lato_ultra_bold_info);
+    font_info_.m_FontList[kOxygenSansSansBold] =
+        std::move(oxygen_sans_sans_bold_info);
   }
 
   void* FindFont(int weight,
@@ -78,12 +104,29 @@ TEST_F(CFX_FolderFontInfoTest, TestFindFont) {
                         /*pitch_family=*/2, kCalibri,
                         /*bMatchName=*/true));
 
-  // Find the closest matching font to "Bookself" font that is present in the
+  // Find the closest matching font to "Bookshelf" font that is present in the
   // installed fonts
   font = FindFont(/*weight=*/0, /*bItalic=*/false, /*charset=*/2,
                   /*pitch_family=*/2, kBookshelf, /*bMatchName=*/true);
   ASSERT_TRUE(font);
   EXPECT_EQ(GetFaceName(font), kBookshelfSymbol7);
+
+  // Find "Book" font is expected to fail, because none of the installed fonts
+  // is in the same font family..
+  EXPECT_FALSE(FindFont(/*weight=*/0, /*bItalic=*/false, /*charset=*/2,
+                        /*pitch_family=*/2, kBook, /*bMatchName=*/true));
+
+  // Find the closest matching font for "Tofu" in the installed fonts.
+  EXPECT_TRUE(FindFont(/*weight=*/0, /*bItalic=*/false, /*charset=*/2,
+                       /*pitch_family=*/2, kTofu, /*bMatchName=*/true));
+
+  // Find the closest matching font for "Lato" in the installed fonts.
+  EXPECT_TRUE(FindFont(/*weight=*/0, /*bItalic=*/false, /*charset=*/2,
+                       /*pitch_family=*/2, kLato, /*bMatchName=*/true));
+
+  // Find the closest mathcing font for "OxygenSans" in the installed fonts.
+  EXPECT_TRUE(FindFont(/*weight=*/0, /*bItalic=*/false, /*charset=*/2,
+                       /*pitch_family=*/2, kOxygenSans, /*bMatchName=*/true));
 
   // Find "Symbol" font when name matching is false
   font = FindFont(/*weight=*/0, /*bItalic=*/false, /*charset=*/2,
