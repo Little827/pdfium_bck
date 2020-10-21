f79eba1113bb6cccfed37fc519a1ec6c43d38e3b
Tom Sepez <tsepez@chromium.org>
2020-10-21 13:29:02 -0700

    Remove channel argument from CFX_DIBitmap::LoadChannel()
    Change-Id: I58a3a5795cca224c08de1a34311790a5e8b1f88d

--- core/fxge/dib/cfx_dibitmap.cpp
+++ core/fxge/dib/cfx_dibitmap.cpp
@@ -364,57 +364,34 @@ bool CFX_DIBitmap::LoadChannelFromAlpha(
   return true;
 }
 
-bool CFX_DIBitmap::LoadChannel(Channel destChannel, int value) {
+bool CFX_DIBitmap::LoadChannel(int value) {
   if (!m_pBuffer)
     return false;
 
   int destOffset;
-  if (destChannel == CFX_DIBitmap::Channel::kAlpha) {
-    if (IsMask()) {
-      if (!ConvertFormat(FXDIB_Format::k8bppMask)) {
-        return false;
-      }
-      destOffset = 0;
-    } else {
-      destOffset = 0;
-      if (!ConvertFormat(IsCmykImage() ? FXDIB_Format::kCmyka
-                                       : FXDIB_Format::kArgb)) {
-        return false;
-      }
-      if (GetFormat() == FXDIB_Format::kArgb) {
-        destOffset = 3;
-      }
-    }
-  } else {
-    if (IsMask()) {
+
+  if (IsMask()) {
+    if (!ConvertFormat(FXDIB_Format::k8bppMask)) {
       return false;
     }
-    if (GetBPP() < 24) {
-      if (HasAlpha()) {
-        if (!ConvertFormat(IsCmykImage() ? FXDIB_Format::kCmyka
-                                         : FXDIB_Format::kArgb)) {
-          return false;
-        }
-      } else {
-#if defined(OS_APPLE)
-        constexpr FXDIB_Format kPlatformFormat = FXDIB_Format::kRgb;
-#else
-        constexpr FXDIB_Format kPlatformFormat = FXDIB_Format::kRgb32;
-#endif
-        if (!ConvertFormat(IsCmykImage() ? FXDIB_Format::kCmyk
-                                         : kPlatformFormat)) {
-          return false;
-        }
-      }
+    destOffset = 0;
+  } else {
+    destOffset = 0;
+    if (!ConvertFormat(IsCmykImage() ? FXDIB_Format::kCmyka
+                                     : FXDIB_Format::kArgb)) {
+      return false;
     }
-    destOffset = kChannelOffset[static_cast<size_t>(destChannel)];
+    if (GetFormat() == FXDIB_Format::kArgb) {
+      destOffset = 3;
+    }
   }
+
   int Bpp = GetBPP() / 8;
   if (Bpp == 1) {
     memset(m_pBuffer.Get(), value, m_Height * m_Pitch);
     return true;
   }
-  if (destChannel == CFX_DIBitmap::Channel::kAlpha && m_pAlphaMask) {
+  if (m_pAlphaMask) {
     memset(m_pAlphaMask->GetBuffer(), value,
            m_pAlphaMask->GetHeight() * m_pAlphaMask->GetPitch());
     return true;
