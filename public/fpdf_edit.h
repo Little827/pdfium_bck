dbf14b2b88032abc9441a74af2f438a313c93814
Roman Wüger <roman.wueger@gmail.com>
2021-02-16 21:15:57 +0100

    1468: Extract ICC profile
    Change-Id: Id1940cb5568a343f64f67340b01561bb10dc73a4

--- public/fpdf_edit.h
+++ public/fpdf_edit.h
@@ -84,7 +84,7 @@ typedef struct FPDF_IMAGEOBJ_METADATA {
   // The number of bits used to represent each pixel.
   unsigned int bits_per_pixel;
   // The image's colorspace. See above for the list of FPDF_COLORSPACE_*.
-  int colorspace;
+  void* colorspace;
   // The image's marked content ID. Useful for pairing with associated alt-text.
   // A value of -1 indicates no ID.
   int marked_content_id;
