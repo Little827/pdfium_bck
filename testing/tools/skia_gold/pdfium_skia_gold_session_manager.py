739c4d62b417b64bb05c54f91fbbf5425add041c
Stephanie Kim <kimstephanie@google.com>
2021-02-16 20:29:46 -0800

    Skia gold in pdfium
    Bug: pdfium:1642
    Change-Id: Ie38dcf45e526c81aae2179ca62c672ce8f38a6a1

---
+++ testing/tools/skia_gold/pdfium_skia_gold_session_manager.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# Copyright 2021 The PDFium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""PDFium implementation of //build/skia_gold_common/skia_gold_session_manager.py."""
+
+import os
+import subprocess
+import sys
+
+import path_utils
+path_utils.AddDirToPathIfNeeded(path_utils.GetPDFiumDir(), 'build')
+from skia_gold_common import output_managerless_skia_gold_session
+from skia_gold_common import skia_gold_session_manager as sgsm
+
+SKIA_PDF_INSTANCE = 'pdfium'
+
+
+class PDFiumSkiaGoldSessionManager(sgsm.SkiaGoldSessionManager):
+
+  @staticmethod
+  def GetSessionClass():
+    return output_managerless_skia_gold_session\
+        .OutputManagerlessSkiaGoldSession
+
+  @staticmethod
+  def _GetDefaultInstance():
+    return SKIA_PDF_INSTANCE
