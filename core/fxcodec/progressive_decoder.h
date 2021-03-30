b01c3b706455942d85ded2b34eaf0e497237cd52
Tom Sepez <tsepez@chromium.org>
2021-03-30 14:22:08 -0700

    Still still more UnownedPtr<> conversions.
    This uncovered a dangling reference in CPWL_EditCtrl, so add an
    OnDestroy() method to "pre-finalize".
    
    This also uncovered a stack-variable ordering issue in CFWL_ListBox,
    so shuffle around and comment.
    
    -- Move some now non-trivial destructors out of line.
    
    Change-Id: I86b4d121c1851b3384db9ec091c51d6a6265c95d

--- core/fxcodec/progressive_decoder.h
+++ core/fxcodec/progressive_decoder.h
@@ -17,6 +17,7 @@
 #include "core/fxcrt/fx_memory_wrappers.h"
 #include "core/fxcrt/fx_system.h"
 #include "core/fxcrt/retain_ptr.h"
+#include "core/fxcrt/unowned_ptr.h"
 #include "core/fxge/dib/fx_dib.h"
 
 #ifdef PDF_ENABLE_XFA_BMP
