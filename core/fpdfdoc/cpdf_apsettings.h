d320d921f7a895c9f2a2440408aef68b1b058577
Tom Sepez <tsepez@chromium.org>
2021-05-18 10:11:48 -0700

    Remove out parameter from CPDF_ApSettings::GetColorARGB().
    Same for methods in CPDF_FormControl. Change return result from
    CPDF_DefaultAppearance method for similariy.
    
    -- put some constructors into .cpp file while at it.
    
    Change-Id: I0543b85dc72b17ae6da1bbcd8cb82928bc7045ec

--- core/fpdfdoc/cpdf_apsettings.h
+++ core/fpdfdoc/cpdf_apsettings.h
@@ -47,7 +47,9 @@ class CPDF_ApSettings {
   // Returns one of the TEXTPOS_* values above.
   int GetTextPosition() const;
 
-  FX_ARGB GetColorARGB(int& iColorType, const ByteString& csEntry) const;
+  std::pair<CFX_Color::Type, FX_ARGB> GetColorARGB(
+      const ByteString& csEntry) const;
+
   float GetOriginalColorComponent(int index, const ByteString& csEntry) const;
   CFX_Color GetOriginalColor(const ByteString& csEntry) const;
 
