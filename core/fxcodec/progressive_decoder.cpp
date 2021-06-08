c4d5a27f47d4a4313874c1ec70455446fc4cc5a3
Tom Sepez <tsepez@chromium.org>
2021-06-08 15:25:19 -0700

    Begin consolidation of various weight tables.
    Make pixel weights table slightly saner.
    
    Change-Id: I0d7dec5703d8d263d5e9745fe577ee4dd0dd873d

--- core/fxcodec/progressive_decoder.cpp
+++ core/fxcodec/progressive_decoder.cpp
@@ -70,9 +70,8 @@ ProgressiveDecoder::WeightTable::~WeightTable() = default;
 void ProgressiveDecoder::WeightTable::Calc(int dest_len, int src_len) {
   double scale = static_cast<double>(src_len) / dest_len;
   double base = dest_len < 0 ? src_len : 0.0;
-  m_ItemSize = (int)(sizeof(int) * 2 + sizeof(int) * (ceil(fabs(scale)) + 1));
-  m_DestMin = 0;
-  m_pWeightTables.resize(dest_len * m_ItemSize + 4);
+  const size_t weight_count = static_cast<size_t>(ceil(fabs(scale))) + 1;
+  AllocateSpace(0, dest_len, weight_count);
   if (fabs(scale) < 1.0) {
     for (int dest_pixel = 0; dest_pixel < dest_len; dest_pixel++) {
       PixelWeight& pixel_weights = *GetPixelWeight(dest_pixel);
@@ -143,9 +142,7 @@ ProgressiveDecoder::HorzTable::~HorzTable() = default;
 
 void ProgressiveDecoder::HorzTable::Calc(int dest_len, int src_len) {
   double scale = (double)dest_len / (double)src_len;
-  m_ItemSize = sizeof(int) * 4;
-  int size = dest_len * m_ItemSize + 4;
-  m_pWeightTables.resize(size, 0);
+  AllocateSpace(0, dest_len, 4);
   if (scale > 1) {
     int pre_dest_col = 0;
     for (int src_col = 0; src_col < src_len; src_col++) {
@@ -196,9 +193,7 @@ ProgressiveDecoder::VertTable::~VertTable() = default;
 
 void ProgressiveDecoder::VertTable::Calc(int dest_len, int src_len) {
   double scale = (double)dest_len / (double)src_len;
-  m_ItemSize = sizeof(int) * 4;
-  int size = dest_len * m_ItemSize + 4;
-  m_pWeightTables.resize(size, 0);
+  AllocateSpace(0, dest_len, 4);
   if (scale <= 1) {
     for (int dest_row = 0; dest_row < dest_len; dest_row++) {
       PixelWeight* pWeight = GetPixelWeight(dest_row);
