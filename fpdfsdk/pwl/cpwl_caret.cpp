7926a225b7502d8246a07d704f10ce7bb4ab8c92
Lei Zhang <thestig@chromium.org>
2021-06-11 16:13:57 -0700

    Rename CFX_PathData to CFX_Path.
    Shorten the name. Do the same for CFX_RetainablePath, method names, and
    variable names.
    
    Change-Id: I4129c840e96a2fc97e10b104f850a7c28402e9ac

--- fpdfsdk/pwl/cpwl_caret.cpp
+++ fpdfsdk/pwl/cpwl_caret.cpp
@@ -11,7 +11,7 @@
 
 #include "core/fxge/cfx_fillrenderoptions.h"
 #include "core/fxge/cfx_graphstatedata.h"
-#include "core/fxge/cfx_pathdata.h"
+#include "core/fxge/cfx_path.h"
 #include "core/fxge/cfx_renderdevice.h"
 
 CPWL_Caret::CPWL_Caret(
@@ -28,7 +28,7 @@ void CPWL_Caret::DrawThisAppearance(CFX_RenderDevice* pDevice,
 
   CFX_FloatRect rcRect = GetCaretRect();
   CFX_FloatRect rcClip = GetClipRect();
-  CFX_PathData path;
+  CFX_Path path;
 
   float fCaretX = rcRect.left + m_fWidth * 0.5f;
   float fCaretTop = rcRect.top;
