bfa32dfb77f868c3aa52e81e102e8b52c909a638
Tom Sepez <tsepez@chromium.org>
2021-06-16 14:22:14 -0700

    Fix overly-strong ASSERT in WeightTable::SetStartEnd()
    A zero-length range at zero represented by [0, -1]  will be fine so long
    as we don't actually try to reference the range. Stop short of switching
    over to the simpler [0, 0) representation. Note that `weight_count` is
    bounded by earlier size checks and fits in an int.
    
    -- Add another ASSERT to earlier tests.
    
    Bug: chromium:1219974
    Change-Id: I26b077d11e54465b861070e887b3efde8f2178da

--- core/fxge/dib/cstretchengine.cpp
+++ core/fxge/dib/cstretchengine.cpp
@@ -105,7 +105,8 @@ bool CStretchEngine::WeightTable::CalculateWeights(
         src_start = std::max(src_start, src_min);
         src_end = std::min(src_end, src_max - 1);
         pixel_weights.SetStartEnd(src_start, src_end, weight_count);
-        if (pixel_weights.m_SrcStart == pixel_weights.m_SrcEnd) {
+        if (pixel_weights.m_SrcStart >= pixel_weights.m_SrcEnd) {
+          // Always room for one weight per size calculation.
           pixel_weights.m_Weights[0] = kFixedPointOne;
         } else {
           pixel_weights.m_Weights[1] = FixedFromFloat(
