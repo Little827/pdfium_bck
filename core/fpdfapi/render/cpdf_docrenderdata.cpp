0dee09da225a163206fd96cf885c530527659c0f
Tom Sepez <tsepez@chromium.org>
2021-06-24 16:27:20 -0700

    Use span<float> in CPDF_Function::v_Call().
    Get some better bounds checking as a result.
    
    -- Use Optional in place of out param.
    -- Prefer std::fill(0.0f) over memset(0) for float arrays.
    
    Change-Id: I982345a3099f7e9c06dd2c909e415a6c27007587

--- core/fpdfapi/render/cpdf_docrenderdata.cpp
+++ core/fpdfapi/render/cpdf_docrenderdata.cpp
@@ -79,9 +79,8 @@ RetainPtr<CPDF_TransferFunc> CPDF_DocRenderData::CreateTransferFunc(
       return nullptr;
   }
 
-  int noutput;
   float output[kMaxOutputs];
-  memset(output, 0, sizeof(output));
+  std::fill(std::begin(output), std::end(output), 0.0f);
 
   bool bIdentity = true;
   std::vector<uint8_t, FxAllocAllocator<uint8_t>> samples_r(
@@ -100,7 +99,7 @@ RetainPtr<CPDF_TransferFunc> CPDF_DocRenderData::CreateTransferFunc(
           samples[i][v] = v;
           continue;
         }
-        pFuncs[i]->Call(&input, 1, output, &noutput);
+        pFuncs[i]->Call({&input, 1}, output);
         size_t o = FXSYS_roundf(output[0] * 255);
         if (o != v)
           bIdentity = false;
@@ -109,7 +108,7 @@ RetainPtr<CPDF_TransferFunc> CPDF_DocRenderData::CreateTransferFunc(
       continue;
     }
     if (pFuncs[0]->CountOutputs() <= kMaxOutputs)
-      pFuncs[0]->Call(&input, 1, output, &noutput);
+      pFuncs[0]->Call({&input, 1}, output);
     size_t o = FXSYS_roundf(output[0] * 255);
     if (o != v)
       bIdentity = false;
