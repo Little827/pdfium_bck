397f786ee2802ce76dd492016f4c9946b81fe326
Lei Zhang <thestig@chromium.org>
2021-06-28 17:44:11 -0700

    Remove fx_system.h include from some headers.
    Directly include system headers like <stdint.h> instead.
    
    Change-Id: Ie97e9025570575f634273dd6b073ba90dc554ad2

--- core/fxcrt/widestring.h
+++ core/fxcrt/widestring.h
@@ -8,17 +8,20 @@
 #define CORE_FXCRT_WIDESTRING_H_
 
 #include <stdarg.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <wchar.h>
 
 #include <functional>
 #include <iosfwd>
 #include <iterator>
 #include <utility>
 
-#include "core/fxcrt/fx_system.h"
 #include "core/fxcrt/retain_ptr.h"
 #include "core/fxcrt/string_data_template.h"
 #include "core/fxcrt/string_view_template.h"
 #include "third_party/base/check.h"
+#include "third_party/base/compiler_specific.h"
 #include "third_party/base/optional.h"
 #include "third_party/base/span.h"
 
