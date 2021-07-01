f72adf7e07cf6f3aa7ca447a2631d7cd850dcf10
Tom Sepez <tsepez@chromium.org>
2021-07-01 10:44:21 -0700

    Split fx_types.h off from fx_system.h.
    Many header files will just require the smaller fx_types.h rather than
    the full-up fx_system.h, which drags in a number of other system library
    headers.
    
    -- remove fx_system.h from fx_safe_types.h
    -- fix one missing fx_system.h include.
    
    Change-Id: I5e731d1cf8a979dad92abcc95346a405d076c0b4

--- core/fxcodec/jpeg/jpegmodule.cpp
+++ core/fxcodec/jpeg/jpegmodule.cpp
@@ -18,6 +18,7 @@
 #include "core/fxcodec/scanlinedecoder.h"
 #include "core/fxcrt/fx_memory_wrappers.h"
 #include "core/fxcrt/fx_safe_types.h"
+#include "core/fxcrt/fx_system.h"
 #include "core/fxge/dib/cfx_dibbase.h"
 #include "core/fxge/dib/fx_dib.h"
 #include "third_party/base/check.h"
