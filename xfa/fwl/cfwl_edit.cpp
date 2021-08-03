97e7aa0c2bbdee5326bf404432b86906d1e82327
Tom Sepez <tsepez@chromium.org>
2021-08-03 09:53:24 -0700

    Clean up some type usage in CFWL_Message heirarchy
    -- Move FWL_KeyFlag defintion to cfwl_message.h
    -- Use FWL_Keyflag in another message subclass.
    -- Rename enum class CFWL_MessageKey::Type to not conflict with superclass
       enum class CFWL_Message::Type.
    
    Change-Id: Ie4571b46d13c528d90d4cc14ee720d548a5f07cf

--- xfa/fwl/cfwl_edit.cpp
+++ xfa/fwl/cfwl_edit.cpp
@@ -817,9 +817,9 @@ void CFWL_Edit::OnProcessMessage(CFWL_Message* pMessage) {
     }
     case CFWL_Message::Type::kKey: {
       CFWL_MessageKey* pKey = static_cast<CFWL_MessageKey*>(pMessage);
-      if (pKey->m_dwCmd == CFWL_MessageKey::Type::kKeyDown)
+      if (pKey->m_dwSubType == CFWL_MessageKey::SubType::kKeyDown)
         OnKeyDown(pKey);
-      else if (pKey->m_dwCmd == CFWL_MessageKey::Type::kChar)
+      else if (pKey->m_dwSubType == CFWL_MessageKey::SubType::kChar)
         OnChar(pKey);
       break;
     }
