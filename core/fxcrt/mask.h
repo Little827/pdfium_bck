fb3ba745791bbbfd58ae37ac128e42f6865d0570
Tom Sepez <tsepez@chromium.org>
2021-08-06 14:43:43 -0700

    Introduce fxcrt::Mask<E> template.
    Because C++ just isn't strict enough to catch everything otherwise.
    
    -- Convert one sample usage.
    
    Change-Id: Ife65ff01f54f3e620c788686798359aa741059a9

---
+++ core/fxcrt/mask.h
@@ -0,0 +1,81 @@
+// Copyright 2021 PDFium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CORE_FXCRT_MASK_H_
+#define CORE_FXCRT_MASK_H_
+
+#include <initializer_list>
+#include <type_traits>
+
+namespace fxcrt {
+
+// Provides extremely strict type-checking on masks of enum class bitflags,
+// for code where flags may not be passed consistently.
+template <typename E>
+class Mask {
+ public:
+  using UnderlyingType = typename std::underlying_type<E>::type;
+
+  // Escape hatch for when value comes aross an API, say.
+  static Mask FromUnderlyingUnchecked(UnderlyingType val) { return Mask(val); }
+
+  constexpr Mask() = default;
+  constexpr Mask(const Mask& that) = default;
+
+  // NOLINTNEXTLINE(runtime/explicit)
+  constexpr Mask(E val) : val_(static_cast<UnderlyingType>(val)) {}
+
+  // Unfortunately, std::initializer_list<> can't be used in constexpr
+  // methods per C++ standards, so this can't be constexpr.
+  Mask(const std::initializer_list<E>& list) {
+    for (const auto& item : list)
+      val_ |= static_cast<UnderlyingType>(item);
+  }
+  explicit operator bool() const { return !!val_; }
+  Mask operator~() const { return Mask(~val_); }
+  constexpr Mask operator|(const Mask& that) const {
+    return Mask(val_ | that.val_);
+  }
+  constexpr Mask operator&(const Mask& that) const {
+    return Mask(val_ & that.val_);
+  }
+  constexpr Mask operator^(const Mask& that) const {
+    return Mask(val_ ^ that.val_);
+  }
+  Mask& operator=(const Mask& that) {
+    val_ = that.val_;
+    return *this;
+  }
+  Mask& operator|=(const Mask& that) {
+    val_ |= that.val_;
+    return *this;
+  }
+  Mask& operator&=(const Mask& that) {
+    val_ &= that.val_;
+    return *this;
+  }
+  Mask& operator^=(const Mask& that) {
+    val_ ^= that.val_;
+    return *this;
+  }
+  bool operator==(const Mask& that) const { return val_ == that.val_; }
+  bool operator!=(const Mask& that) const { return val_ != that.val_; }
+
+  // Because ~ can't be applied to enum class without castsing.
+  void Clear(const Mask& that) { val_ &= ~that.val_; }
+
+  // Escape hatch, usage should be minimized.
+  UnderlyingType Value() const { return val_; }
+
+ private:
+  explicit constexpr Mask(UnderlyingType val) : val_(val) {}
+
+  UnderlyingType val_ = 0;
+};
+
+}  // namespace fxcrt
+
+using fxcrt::Mask;
+
+#endif  // CORE_FXCRT_MASK_H_
---
+++ core/fxcrt/mask_unittest.cpp
@@ -0,0 +1,99 @@
+// Copyright 2021 PDFium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/fxcrt/mask.h"
+
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace fxcrt {
+namespace {
+
+enum class Privilege : uint8_t {
+  kDriving = 1 << 0,
+  kScubaDiving = 1 << 1,
+  kStayingUpLate = 1 << 2,
+};
+
+const Mask<Privilege> kAllMask = {
+    Privilege::kDriving,
+    Privilege::kScubaDiving,
+    Privilege::kStayingUpLate,
+};
+
+}  // namespace
+
+static_assert(sizeof(Mask<Privilege>) == sizeof(Privilege),
+              "Mask size must be the same as enum");
+
+TEST(Mask, Empty) {
+  constexpr Mask<Privilege> privs;
+  EXPECT_EQ(0u, privs.Value());
+  EXPECT_FALSE(privs & Privilege::kDriving);
+  EXPECT_FALSE(privs & Privilege::kScubaDiving);
+  EXPECT_FALSE(privs & Privilege::kStayingUpLate);
+  EXPECT_FALSE(privs & kAllMask);
+}
+
+TEST(Mask, FromOne) {
+  Mask<Privilege> privs = Privilege::kDriving;
+  EXPECT_EQ(1u, privs.Value());
+  EXPECT_TRUE(privs & Privilege::kDriving);
+  EXPECT_FALSE(privs & Privilege::kScubaDiving);
+  EXPECT_FALSE(privs & Privilege::kStayingUpLate);
+  EXPECT_TRUE(privs & kAllMask);
+}
+
+TEST(Mask, FromMany) {
+  Mask<Privilege> privs = {Privilege::kDriving, Privilege::kStayingUpLate};
+  EXPECT_EQ(5u, privs.Value());
+  EXPECT_TRUE(privs & Privilege::kDriving);
+  EXPECT_FALSE(privs & Privilege::kScubaDiving);
+  EXPECT_TRUE(privs & Privilege::kStayingUpLate);
+  EXPECT_TRUE(privs & kAllMask);
+}
+
+TEST(Mask, FromUnderlying) {
+  auto privs = Mask<Privilege>::FromUnderlyingUnchecked(5);
+  EXPECT_EQ(5u, privs.Value());
+  EXPECT_TRUE(privs & Privilege::kDriving);
+  EXPECT_FALSE(privs & Privilege::kScubaDiving);
+  EXPECT_TRUE(privs & Privilege::kStayingUpLate);
+}
+
+TEST(Mask, AssignAndEQ) {
+  Mask<Privilege> source = {Privilege::kDriving, Privilege::kStayingUpLate};
+  Mask<Privilege> other = Privilege::kDriving;
+  Mask<Privilege> dest;
+  dest = source;
+  EXPECT_EQ(5u, dest.Value());
+  EXPECT_EQ(source, dest);
+  EXPECT_NE(other, dest);
+}
+
+TEST(Mask, OrAndAnd) {
+  Mask<Privilege> source = {Privilege::kDriving, Privilege::kStayingUpLate};
+  Mask<Privilege> or_result =
+      source | Mask<Privilege>{Privilege::kDriving, Privilege::kScubaDiving};
+  Mask<Privilege> and_result =
+      source & Mask<Privilege>{Privilege::kDriving, Privilege::kScubaDiving};
+  EXPECT_EQ(or_result, kAllMask);
+  EXPECT_EQ(and_result, Privilege::kDriving);
+}
+
+TEST(Mask, OrEqualsAndAndEquals) {
+  Mask<Privilege> source_or = {Privilege::kDriving, Privilege::kStayingUpLate};
+  Mask<Privilege> source_and = {Privilege::kDriving, Privilege::kStayingUpLate};
+  source_or |= {Privilege::kDriving, Privilege::kScubaDiving};
+  source_and &= {Privilege::kDriving, Privilege::kScubaDiving};
+  EXPECT_EQ(source_or, kAllMask);
+  EXPECT_EQ(source_and, Privilege::kDriving);
+}
+
+TEST(Mask, Clear) {
+  Mask<Privilege> source = kAllMask;
+  source.Clear({Privilege::kDriving, Privilege::kScubaDiving});
+  EXPECT_EQ(source, Privilege::kStayingUpLate);
+}
+
+}  // namespace fxcrt
