285bbd6cbb69cc0616f7ef9838da6fb95a4ed75d
Tom Sepez <tsepez@chromium.org>
2021-08-11 09:36:23 -0700

    more masks
    Change-Id: If26132186a22dd62d60ab4faf4800dd1fb151707

--- fxjs/xfa/cfxjse_resolveprocessor.cpp
+++ fxjs/xfa/cfxjse_resolveprocessor.cpp
@@ -521,8 +521,9 @@ bool CFXJSE_ResolveProcessor::ResolveNormal(v8::Isolate* pIsolate,
 
 bool CFXJSE_ResolveProcessor::ResolveAsterisk(CFXJSE_ResolveNodeData& rnd) {
   CXFA_Node* curNode = ToNode(rnd.m_CurObject.Get());
-  std::vector<CXFA_Node*> array = curNode->GetNodeListWithFilter(
-      XFA_NodeFilter_Children | XFA_NodeFilter_Properties);
+  std::vector<CXFA_Node*> array =
+      curNode->GetNodeListWithFilter(Mask<XFA_NodeFilter>{
+          XFA_NodeFilter::kChildren, XFA_NodeFilter::kProperties});
   rnd.m_Result.objects.insert(rnd.m_Result.objects.end(), array.begin(),
                               array.end());
   return !rnd.m_Result.objects.empty();
