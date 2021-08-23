2b38767beae7df414c29e5e563059c656dd315e2
Tom Sepez <tsepez@chromium.org>
2021-08-23 15:03:42 -0700

    Clean up enums in BC_Library.h
    Stop short of making these enum classes.
    
    -- Unify BC_TYPE and BCFORMAT since they are 1:1 with each other,
       just in a different order.
    -- Add BC_ prefix to char encoding enum for consistency.
    -- Convert some int arguments to BC_CHAR_FORMAT.
    
    Change-Id: Ic924f60a0c4f3bc8646a77fb73da3777034d5f14

--- fxbarcode/oned/BC_OnedEAN13Writer.h
+++ fxbarcode/oned/BC_OnedEAN13Writer.h
@@ -21,7 +21,7 @@ class CBC_OnedEAN13Writer final : public CBC_OneDimEANWriter {
 
   // CBC_OneDimEANWriter:
   uint8_t* EncodeWithHint(const ByteString& contents,
-                          BCFORMAT format,
+                          BC_TYPE format,
                           int32_t& outWidth,
                           int32_t& outHeight,
                           int32_t hints) override;
