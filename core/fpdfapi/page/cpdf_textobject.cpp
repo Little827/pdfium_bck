654eb91984d9a2c8d8e274f47790ba83dd6e964f
Tom Sepez <tsepez@chromium.org>
2021-08-27 11:26:48 -0700

    Use span<> in place of const float* in fpdfapi.
    Change-Id: Idf56fdda3a6f7d5111ac59ed70a4f86c74eec762

--- core/fpdfapi/page/cpdf_textobject.cpp
+++ core/fpdfapi/page/cpdf_textobject.cpp
@@ -172,13 +172,13 @@ const CPDF_TextObject* CPDF_TextObject::AsText() const {
 }
 
 CFX_Matrix CPDF_TextObject::GetTextMatrix() const {
-  const float* pTextMatrix = m_TextState.GetMatrix();
+  pdfium::span<const float> pTextMatrix = m_TextState.GetMatrix();
   return CFX_Matrix(pTextMatrix[0], pTextMatrix[2], pTextMatrix[1],
                     pTextMatrix[3], m_Pos.x, m_Pos.y);
 }
 
 void CPDF_TextObject::SetTextMatrix(const CFX_Matrix& matrix) {
-  float* pTextMatrix = m_TextState.GetMutableMatrix();
+  pdfium::span<float> pTextMatrix = m_TextState.GetMutableMatrix();
   pTextMatrix[0] = matrix.a;
   pTextMatrix[1] = matrix.c;
   pTextMatrix[2] = matrix.b;
