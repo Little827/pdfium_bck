e3ee2208024649a33b60363b82ddd3203a5eaa61
Tom Sepez <tsepez@chromium.org>
2021-09-17 09:18:51 -0700

    Rework/simplify the CPWL_Button heirarchy.
    Since CPWL_Button is never instantiated itself, it looked like its
    On*() methods might be unreachable, since its sub-classes override
    them -- except for CPWL_PushButton. So move these to the pushbutton
    itself since it is the only class that uses this.
    
    As a result, pushbutton is now self-contained and can inherit directly
    from CPWL_Wnd. The remaing two buttons are special (i.e. stateful), so
    create a common stateful button superclass to collect duplicated code.
    
    Then rename files to match the contents.
    
    -- remove unused m_bButtonDown member.
    -- duplicate some code in OnLButtonDown() for unchanged behavior.
    
    Change-Id: I6de36b13f75db1eda8339fc7005122f25d068542

--- fpdfsdk/pwl/cpwl_stateful_button.h
+++ fpdfsdk/pwl/cpwl_stateful_button.h
@@ -4,58 +4,53 @@
 
 // Original code copyright 2014 Foxit Software Inc. http://www.foxitsoftware.com
 
-#ifndef FPDFSDK_PWL_CPWL_SPECIAL_BUTTON_H_
-#define FPDFSDK_PWL_CPWL_SPECIAL_BUTTON_H_
+#ifndef FPDFSDK_PWL_CPWL_STATEFUL_BUTTON_H_
+#define FPDFSDK_PWL_CPWL_STATEFUL_BUTTON_H_
 
 #include <memory>
 
-#include "fpdfsdk/pwl/cpwl_button.h"
+#include "fpdfsdk/pwl/cpwl_pushbutton.h"
 
-class CPWL_PushButton final : public CPWL_Button {
+class CPWL_StatefulButton : public CPWL_Wnd {
  public:
-  CPWL_PushButton(
+  CPWL_StatefulButton(
       const CreateParams& cp,
       std::unique_ptr<IPWL_SystemHandler::PerWindowData> pAttachedData);
-  ~CPWL_PushButton() override;
+  ~CPWL_StatefulButton() override;
 
-  // CPWL_Button:
-  CFX_FloatRect GetFocusRect() const override;
+  // CPWL_Wnd:
+  bool OnLButtonDown(Mask<FWL_EVENTFLAG> nFlag,
+                     const CFX_PointF& point) override;
+
+  bool IsChecked() const { return m_bChecked; }
+  bool SetCheck(bool bCheck);
+
+ private:
+  bool m_bChecked = false;
 };
 
-class CPWL_CheckBox final : public CPWL_Button {
+class CPWL_CheckBox final : public CPWL_StatefulButton {
  public:
   CPWL_CheckBox(
       const CreateParams& cp,
       std::unique_ptr<IPWL_SystemHandler::PerWindowData> pAttachedData);
   ~CPWL_CheckBox() override;
 
-  // CPWL_Button:
+  // CPWL_StatefulButton:
   bool OnLButtonUp(Mask<FWL_EVENTFLAG> nFlag, const CFX_PointF& point) override;
   bool OnChar(uint16_t nChar, Mask<FWL_EVENTFLAG> nFlag) override;
-
-  bool IsChecked() const { return m_bChecked; }
-  void SetCheck(bool bCheck) { m_bChecked = bCheck; }
-
- private:
-  bool m_bChecked = false;
 };
 
-class CPWL_RadioButton final : public CPWL_Button {
+class CPWL_RadioButton final : public CPWL_StatefulButton {
  public:
   CPWL_RadioButton(
       const CreateParams& cp,
       std::unique_ptr<IPWL_SystemHandler::PerWindowData> pAttachedData);
   ~CPWL_RadioButton() override;
 
-  // CPWL_Button
+  // CPWL_StatefulButton:
   bool OnLButtonUp(Mask<FWL_EVENTFLAG> nFlag, const CFX_PointF& point) override;
   bool OnChar(uint16_t nChar, Mask<FWL_EVENTFLAG> nFlag) override;
-
-  bool IsChecked() const { return m_bChecked; }
-  void SetCheck(bool bCheck) { m_bChecked = bCheck; }
-
- private:
-  bool m_bChecked = false;
 };
 
-#endif  // FPDFSDK_PWL_CPWL_SPECIAL_BUTTON_H_
+#endif  // FPDFSDK_PWL_CPWL_STATEFUL_BUTTON_H_
