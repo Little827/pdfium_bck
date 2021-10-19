5fb617f28ef6c0dcbc471dbd214b418e6e142f2b
Tom Anderson <thomasanderson@chromium.org>
2021-10-19 10:55:34 -0700

    Run tests on Linux with hermetic font configuration
    Bug: chromium:1250250
    Change-Id: I6b8400fd5e76f3b4ac4755ddd5ae8f84a7ca7cd2
    R=thestig

--- testing/pdf_test_environment.cpp
+++ testing/pdf_test_environment.cpp
@@ -6,13 +6,42 @@
 
 #include "core/fxge/cfx_gemodule.h"
 
+#if defined(OS_LINUX)
+#include <libgen.h>
+#include <unistd.h>
+#endif
+
+namespace {
+
+#if defined(OS_LINUX) || defined(OS_CHROMEOS)
+// This is duplicated from
+// third_party/test_fonts/fontconfig/fontconfig_util_linux.cc to avoid adding
+// a dependency on fontconfig.
+std::string GetSysrootDir() {
+  char buf[PATH_MAX + 1];
+  auto count = readlink("/proc/self/exe", buf, PATH_MAX);
+  assert(count > 0);
+  buf[count] = '\0';
+  return dirname(buf);
+}
+#endif
+
+}  // namespace
+
 PDFTestEnvironment::PDFTestEnvironment() = default;
 
 PDFTestEnvironment::~PDFTestEnvironment() = default;
 
 // testing::Environment:
 void PDFTestEnvironment::SetUp() {
+#if defined(OS_LINUX) || defined(OS_CHROMEOS)
+  font_path_ = GetSysrootDir() + "/test_fonts";
+  font_paths_[0] = font_path_.c_str();
+  font_paths_[1] = nullptr;
+  CFX_GEModule::Create(font_paths_);
+#else
   CFX_GEModule::Create(nullptr);
+#endif
 }
 
 void PDFTestEnvironment::TearDown() {
