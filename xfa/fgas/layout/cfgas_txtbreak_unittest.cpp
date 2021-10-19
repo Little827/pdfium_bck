e6ba3c54db9fa15aeae826c6e5828f9d3f6447de
Tom Anderson <thomasanderson@chromium.org>
2021-10-19 10:26:47 -0700

    Run tests on Linux with hermetic font configuration
    Bug: chromium:1250250
    Change-Id: I6b8400fd5e76f3b4ac4755ddd5ae8f84a7ca7cd2
    R=thestig

--- xfa/fgas/layout/cfgas_txtbreak_unittest.cpp
+++ xfa/fgas/layout/cfgas_txtbreak_unittest.cpp
@@ -17,7 +17,12 @@
 class CFGAS_TxtBreakTest : public testing::Test {
  public:
   void SetUp() override {
-    font_ = CFGAS_GEFont::LoadFont(L"Arial Black", 0, FX_CodePage::kDefANSI);
+#if defined(OS_LINUX) || defined(OS_CHROMEOS)
+    const wchar_t font_family[] = L"Arimo Bold";
+#else
+    const wchar_t font_family[] = L"Arial Bold";
+#endif
+    font_ = CFGAS_GEFont::LoadFont(font_family, 0, FX_CodePage::kDefANSI);
     ASSERT_TRUE(font_);
   }
 
