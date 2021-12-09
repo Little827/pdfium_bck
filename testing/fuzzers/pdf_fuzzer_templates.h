db797b778cf5d019a2326a3be2d9c6739c74d8aa
David Korczynski <david@adalogics.com>
2021-12-09 13:38:19 +0000

    Add XDP-specification fuzzer
    Add fuzzer that wraps arounds a PDF with multiple objects. The fuzzer
    creates a full PDF file which includes a valid xref table and will wrap
    random fuzz-data inside of valid XDP-tags. The tags were derived from
    the XFA specification.
    
    Bug: chromium: 1276950
    Change-Id: I1bd1515dcff4c4de29635ba19ad72df59dc9fa4c

--- testing/fuzzers/pdf_fuzzer_templates.h
+++ testing/fuzzers/pdf_fuzzer_templates.h
@@ -27,4 +27,22 @@ trailer
 <</Root 1 0 R /Size 31>>
 %%EOF)";
 
+constexpr char kSimplePdfHeader[] = R""(%PDF-1.7
+%âãÏÓ)"";
+
+constexpr char kSimpleAcroForm[] = R""(<</AcroForm 2 0 R /Extensions
+  <</ADBE <</BaseVersion /1.7 /ExtensionLevel 8>>>> /NeedsRendering true
+  /Pages 3 0 R /Type /Catalog>>)"";
+
+constexpr char kSimpleXfaObjWrapper[] = R""(<</XFA
+  [(preamble) 5 0 R ($1) 6 0 R ($2) 7 0 R ($3) 8 0 R
+  (postamble) 9 0 R]>>)"";
+
+constexpr char kSimpleObj1[] = R""(<</Count 1 /Kids [4 0 R] /Type /Pages>>)"";
+constexpr char kSimpleMediabox[] =
+    R""(<</MediaBox [0 0 612 792] /Parent 2 0 R /Type /Page>>)"";
+constexpr char kSimplePreamble[] =
+    R""(<xdp:xdp xmlns:xdp="http://ns.adobe.com/xdp/" timeStamp="2021-12-14T14:14:14Z" uuid="11111111-1ab1-11b1-aa1a-1aaaaaaa11a1">)"";
+constexpr char kSimplePostamble[] = R""("</xdp:xdp>")"";
+
 #endif  // TESTING_FUZZERS_PDF_FUZZER_TEMPLATES_H_
