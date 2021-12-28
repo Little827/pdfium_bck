2f28b5ee7180a852e4a7f11b367ca1f08023dbaa
geisserml <geisserml@gmail.com>
2021-12-28 21:02:59 +0100

    Add a method to get bookmark state
    Implement a new public method `FPDFBookmark_IsClosed` in fpdf_doc with
    corresponding backend in cpdf_bookmark that enables PDFium users to
    determine whether an outline item is opened or closed.
    pdfium:bug 1758
    
    Change-Id: I9ebff418ff873f5c2da5edb11ae62038a7f7ddc8

--- fpdfsdk/fpdf_doc.cpp
+++ fpdfsdk/fpdf_doc.cpp
@@ -110,6 +110,15 @@ FPDFBookmark_GetTitle(FPDF_BOOKMARK bookmark,
   return Utf16EncodeMaybeCopyAndReturnLength(title, buffer, buflen);
 }
 
+FPDF_EXPORT FPDF_BOOL FPDF_CALLCONV
+FPDFBookmark_IsClosed(FPDF_BOOKMARK bookmark) {
+    if (!bookmark)
+        return 0;
+    CPDF_Bookmark cBookmark(CPDFDictionaryFromFPDFBookmark(bookmark));
+    FPDF_BOOL is_closed = cBookmark.IsClosed();
+    return is_closed;
+}
+
 FPDF_EXPORT FPDF_BOOKMARK FPDF_CALLCONV
 FPDFBookmark_Find(FPDF_DOCUMENT document, FPDF_WIDESTRING title) {
   CPDF_Document* pDoc = CPDFDocumentFromFPDFDocument(document);
