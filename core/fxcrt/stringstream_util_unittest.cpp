9a9b6bd1430aeaf61f527a061a77f4dba164caf7
Tom Sepez <tsepez@chromium.org>
2022-01-10 12:50:44 -0800

    Introduce fxcrt::ostringstream
    The definition of std::ostringstream is flexible enough that we can
    build a wrapper that allocates it character buffers from a Parttion
    Alloc partition.
    
    Change-Id: I4e15d5baaea649bb376609661d9a6ec58c3ff451

---
+++ core/fxcrt/stringstream_util_unittest.cpp
@@ -0,0 +1,15 @@
+// Copyright 2022 PDFium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/fxcrt/stringstream_util.h"
+
+#include <sstream>
+
+#include "testing/gtest/include/gtest/gtest.h"
+
+TEST(StringStreamUtil, OStringStream) {
+  fxcrt::ostringstream buf;
+  buf << 22 << "C is " << 71.6f << 'F';
+  EXPECT_STREQ("22C is 71.6F", buf.str().c_str());
+}
