aae3b81c5e4084a06bd83ffc67c31cd093cdf0ea
Puneet Jain <jpuneet@google.com>
2022-02-11 12:04:13 -0800

    Make pdfium_test_dump_helper more readable, similar to Acrobat
    Current dump for struct tree does not print keys. It also doesn't print
    newly added fields. Improving it to match Acrobats printing.
    
    Change-Id: I482cecc1449370d5b6aa4796b14aef36c2dd0e97

--- samples/pdfium_test_dump_helper.cc
+++ samples/pdfium_test_dump_helper.cc
@@ -42,24 +42,137 @@ void DumpBoxInfo(GetBoxInfoFunc func,
          rect.left, rect.bottom, rect.right, rect.top);
 }
 
+void DumpStructureElementAttributes(FPDF_STRUCTELEMENT_ATTR attr, int indent) {
+  int count = FPDF_StructElement_Attr_GetCount(attr);
+  printf("%*s A[%d]:\n", indent, "", i);
+  indent = indent + 2;
+  for (int j = 0; j < count; j++) {
+    char attr_name[kBufSize];
+    memset(attr_name, 0, sizeof(attr_name));
+    unsigned long attr_len = -1;
+    if (!FPDF_StructElement_Attr_GetName(attr, j, attr_name, sizeof(attr_name),
+                                         &attr_len)) {
+      printf("%*s FPDF_StructElement_Attr_GetName failed for %d\n", indent, "",
+             j);
+      continue;
+    }
+    std::wstring attr_name_wstring =
+        ConvertToWString(reinterpret_cast<unsigned short*>(attr_name), attr_len)
+            .c_str();
+    std::string name_str(attr_name, attr_len);
+    const char* name = name_str.c_str();
+    FPDF_OBJECT_TYPE type = FPDF_StructElement_Attr_GetType(attr, name);
+
+    if (type == FPDF_OBJECT_BOOLEAN) {
+      int value;
+      if (!FPDF_StructElement_Attr_GetIntValue(attr, name, &value)) {
+        printf("%*s %ls: Failed FPDF_StructElement_Attr_GetIntValue\n", indent,
+               "", attr_name_wstring.c_str());
+        continue;
+      }
+      printf("%*s %ls: %d\n", indent, "", attr_name_wstring.c_str(), value);
+    } else if (type == FPDF_OBJECT_NUMBER) {
+      float value;
+      if (!FPDF_StructElement_Attr_GetNumberValue(attr, name, &value)) {
+        printf("%*s %ls: Failed FPDF_StructElement_Attr_GetNumberValue\n",
+               indent, "", attr_name_wstring.c_str());
+        continue;
+      }
+      printf("%*s %ls: %f\n", indent, "", attr_name_wstring.c_str(), value);
+    } else if (type == FPDF_OBJECT_STRING || type == FPDF_OBJECT_NAME) {
+      char string_val[kBufSize];
+      memset(string_val, 0, sizeof(string_val));
+      if (!FPDF_StructElement_Attr_GetStringValue(attr, name, string_val,
+                                                  sizeof(string_val), &len)) {
+        printf("%*s %ls: Failed FPDF_StructElement_Attr_GetStringValue\n",
+               indent, "", attr_name_wstring.c_str());
+        continue;
+      }
+      printf(
+          "%*s %ls: %ls\n", indent, "", attr_name_wstring.c_str(),
+          ConvertToWString(reinterpret_cast<unsigned short*>(string_val), len)
+              .c_str());
+    } else if (type == FPDF_OBJECT_UNKNOWN) {
+      printf("%*s %ls: FPDF_OBJECT_UNKNOWN\n", indent, "",
+             attr_name_wstring.c_str());
+    } else {
+      printf("%*s %ls: NOT_YET_IMPLEMENTED: %d\n", indent, "",
+             attr_name_wstring.c_str(), type);
+    }
+  }
+}
+
 }  // namespace
 
 void DumpChildStructure(FPDF_STRUCTELEMENT child, int indent) {
   static const size_t kBufSize = 1024;
   unsigned short buf[kBufSize];
   unsigned long len = FPDF_StructElement_GetType(child, buf, kBufSize);
-  printf("%*s%ls", indent * 2, "", ConvertToWString(buf, len).c_str());
+  if (len > 0)
+    printf("%*s S: %ls\n", indent * 2, "", ConvertToWString(buf, len).c_str());
 
   memset(buf, 0, sizeof(buf));
   len = FPDF_StructElement_GetTitle(child, buf, kBufSize);
-  if (len > 0)
-    printf(": '%ls'", ConvertToWString(buf, len).c_str());
+  if (len > 0) {
+    printf("%*s Title: %ls\n", indent * 2, "",
+           ConvertToWString(buf, len).c_str());
+  }
 
   memset(buf, 0, sizeof(buf));
   len = FPDF_StructElement_GetAltText(child, buf, kBufSize);
+  if (len > 0) {
+    printf("%*s AltText: %ls\n", indent * 2, "",
+           ConvertToWString(buf, len).c_str());
+  }
+
+  memset(buf, 0, sizeof(buf));
+  len = FPDF_StructElement_GetActualText(child, buf, kBufSize);
+  if (len > 0) {
+    printf("%*s ActualText: %ls\n", indent * 2, "",
+           ConvertToWString(buf, len).c_str());
+  }
+
+  memset(buf, 0, sizeof(buf));
+  len = FPDF_StructElement_GetID(child, buf, kBufSize);
   if (len > 0)
-    printf(" (%ls)", ConvertToWString(buf, len).c_str());
-  printf("\n");
+    printf("%*s ID: %ls\n", indent * 2, "", ConvertToWString(buf, len).c_str());
+
+  int mcid = FPDF_StructElement_GetMarkedContentID(child);
+  if (mcid != -1)
+    printf("%*s MCID: %d\n", indent * 2, "", mcid);
+
+  memset(buf, 0, sizeof(buf));
+  len = FPDF_StructElement_GetObjType(child, buf, kBufSize);
+  if (len > 0) {
+    printf("%*s Type: %ls\n", indent * 2, "",
+           ConvertToWString(buf, len).c_str());
+  }
+
+  memset(buf, 0, sizeof(buf));
+  len = FPDF_StructElement_GetLang(child, buf, kBufSize);
+  if (len > 0) {
+    printf("%*s Lang: %ls\n", indent * 2, "",
+           ConvertToWString(buf, len).c_str());
+  }
+
+  int attr_count = FPDF_StructElement_GetAttributeCount(child);
+  for (int i = 0; i < attr_count; i++) {
+    FPDF_STRUCTELEMENT_ATTR child_attr =
+        FPDF_StructElement_GetAttributeAtIndex(child, i);
+    if (!child_attr)
+      continue;
+    DumpStructureElementAttributes(attr, indent * 2);
+  }
+
+  FPDF_STRUCTELEMENT parent = FPDF_StructElement_GetParent(child);
+  if (parent) {
+    memset(buf, 0, sizeof(buf));
+    len = FPDF_StructElement_GetID(parent, buf, kBufSize);
+    if (len > 0) {
+      printf("%*s Parent ID: %ls\n", indent * 2, "",
+             ConvertToWString(buf, len).c_str());
+    }
+  }
 
   for (int i = 0; i < FPDF_StructElement_CountChildren(child); ++i) {
     FPDF_STRUCTELEMENT sub_child = FPDF_StructElement_GetChildAtIndex(child, i);
