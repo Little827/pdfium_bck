b75e7d219ae99857431b4deb468a02679ff32610
Tom Sepez <tsepez@chromium.org>
2022-02-24 09:19:40 -0800

    Fix some arithmetic nits in CBC_CommonBitMatrix.
    - make m_bits unsigned to avoid bitwise ops on signed values.
    - avoid possibility of UB with signed 1 shifted by 31.
    - avoid OOB writes in Set().
    
    Change-Id: I8b9f518af34e4ac9a1638f54523797014dc0bfe9

--- fxbarcode/common/BC_CommonBitMatrix.cpp
+++ fxbarcode/common/BC_CommonBitMatrix.cpp
@@ -28,10 +28,16 @@
 CBC_CommonBitMatrix::CBC_CommonBitMatrix() = default;
 
 void CBC_CommonBitMatrix::Init(int32_t width, int32_t height) {
+  CHECK(width >= 0);
+  CHECK(height >= 0);
+
+  static constexpr int32_t kMaxBits = 1024 * 1024 * 1024;  // 1 Gb.
+  CHECK(width < kMaxBits / height);
+
   m_width = width;
   m_height = height;
   m_rowSize = (width + 31) >> 5;
-  m_bits = fxcrt::Vector2D<int32_t>(m_rowSize, m_height);
+  m_bits = fxcrt::Vector2D<uint32_t>(m_rowSize, m_height);
 }
 
 CBC_CommonBitMatrix::~CBC_CommonBitMatrix() = default;
@@ -40,12 +46,12 @@ bool CBC_CommonBitMatrix::Get(int32_t x, int32_t y) const {
   int32_t offset = y * m_rowSize + (x >> 5);
   if (offset >= m_rowSize * m_height || offset < 0)
     return false;
-  return ((((uint32_t)m_bits[offset]) >> (x & 0x1f)) & 1) != 0;
+  return ((m_bits[offset] >> (x & 0x1f)) & 1) != 0;
 }
 
 void CBC_CommonBitMatrix::Set(int32_t x, int32_t y) {
   int32_t offset = y * m_rowSize + (x >> 5);
-  DCHECK(offset >= 0);
-  DCHECK(offset < m_rowSize * m_height);
-  m_bits[offset] |= 1 << (x & 0x1f);
+  if (offset >= m_rowSize * m_height || offset < 0)
+    return;
+  m_bits[offset] |= 1u << (x & 0x1f);
 }
