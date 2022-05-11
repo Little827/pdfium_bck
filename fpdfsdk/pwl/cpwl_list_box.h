591a2c079d3bc17bd30d0fa64c16d5fdf2305e7b
Tom Sepez <tsepez@chromium.org>
2022-05-11 17:31:40 +0000

    Remove SetFillerNotify() from CWPL_Wnd sub-classes
    Previous CL shows it is redundant. Avoid conditionals since we
    know it is set in the CPWL_Wnd itself at creation.
    
    Change-Id: If0bca94fee8889f4b8bebde6c4f9d67d340c88d6

--- fpdfsdk/pwl/cpwl_list_box.h
+++ fpdfsdk/pwl/cpwl_list_box.h
@@ -76,16 +76,10 @@ class CPWL_ListBox : public CPWL_Wnd, public CPWL_ListCtrl::NotifyIface {
   float GetFirstHeight() const;
   CFX_FloatRect GetListRect() const;
 
-  void SetFillerNotify(IPWL_FillerNotify* pNotify) {
-    DCHECK_EQ(pNotify, GetCreationParams()->pFillerNotify);
-    m_pFillerNotify = pNotify;
-  }
-
  protected:
   bool m_bMouseDown = false;
   bool m_bHoverSel = false;
   std::unique_ptr<CPWL_ListCtrl> m_pListCtrl;
-  UnownedPtr<IPWL_FillerNotify> m_pFillerNotify;
 };
 
 #endif  // FPDFSDK_PWL_CPWL_LIST_BOX_H_
