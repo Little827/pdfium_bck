9d4ba0d1bc5949ea78f6d3971f03fcdebc10a10e
Tom Sepez <tsepez@chromium.org>
2022-05-19 23:53:43 +0000

    [xfa] Cover CFWL_MonthCalendar as part of embedder tests
    Precise clicks in a date time picker plus rendering the page in
    the intermediate states is required to draw the calendar, pick
    a date, and stuff that date into the field.
    
    Change-Id: I2eab7906583b031d8cc8ca9d4ce9e69aaa7938ce

--- xfa/fwl/cfwl_edit_embeddertest.cpp
+++ xfa/fwl/cfwl_edit_embeddertest.cpp
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 #include "core/fxcrt/widestring.h"
+#include "public/fpdf_ext.h"
 #include "public/fpdf_formfill.h"
 #include "public/fpdf_fwlevent.h"
 #include "testing/embedder_test.h"
@@ -22,9 +23,13 @@ class CFWLEditEmbedderTest : public XFAJSEmbedderTest {
   void SetUp() override {
     EmbedderTest::SetUp();
     SetDelegate(&delegate_);
+    FSDK_SetTimeFunction([]() -> time_t { return 1600000000; });
+    FSDK_SetLocaltimeFunction([](const time_t* t) { return gmtime(t); });
   }
 
   void TearDown() override {
+    FSDK_SetTimeFunction(nullptr);
+    FSDK_SetLocaltimeFunction(nullptr);
     UnloadPage(page());
     EmbedderTest::TearDown();
   }
@@ -207,8 +212,26 @@ TEST_F(CFWLEditEmbedderTest, DISABLED_FillWithNewLineWithMultiline) {
 TEST_F(CFWLEditEmbedderTest, MAYBE_DateTimePickerTest) {
   CreateAndInitializeFormPDF("xfa/xfa_date_time_edit.pdf");
   FORM_OnLButtonDown(form_handle(), page(), 0, 115, 58);
+  FORM_OnLButtonUp(form_handle(), page(), 0, 115, 58);
+  const char kSelectedMD5[] = "1036b8837a9dba75c6bd8f9347ae2eb2";
+  {
+    ScopedFPDFBitmap page_bitmap =
+        RenderLoadedPageWithFlags(page(), FPDF_ANNOT);
+    CompareBitmap(page_bitmap.get(), 612, 792, kSelectedMD5);
+  }
 
-  const char kFilledMD5[] = "1036b8837a9dba75c6bd8f9347ae2eb2";
+  FORM_OnLButtonDown(form_handle(), page(), 0, 446, 54);
+  FORM_OnLButtonUp(form_handle(), page(), 0, 446, 54);
+  const char kCalendarOpenMD5[] = "f7a29cf668056e209c695ff00fe48f15";
+  {
+    ScopedFPDFBitmap page_bitmap =
+        RenderLoadedPageWithFlags(page(), FPDF_ANNOT);
+    CompareBitmap(page_bitmap.get(), 612, 792, kCalendarOpenMD5);
+  }
+
+  FORM_OnLButtonDown(form_handle(), page(), 0, 100, 162);
+  FORM_OnLButtonUp(form_handle(), page(), 0, 100, 162);
+  const char kFilledMD5[] = "1bce66c11f1c87b8d639ce0076ac36d3";
   {
     ScopedFPDFBitmap page_bitmap =
         RenderLoadedPageWithFlags(page(), FPDF_ANNOT);
