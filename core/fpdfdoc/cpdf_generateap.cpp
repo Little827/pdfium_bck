f390e478a6b6881dffddda09ab4f8c311877609e
Tom Sepez <tsepez@chromium.org>
2022-07-29 00:15:31 +0000

    De-duplicate CPVT_FontMap creation in  CPDF_GenerateAP::GenerateFormAP()
    Change-Id: I88725921cc62ff17d1f552b73fdd1fb7078f28f8

--- core/fpdfdoc/cpdf_generateap.cpp
+++ core/fpdfdoc/cpdf_generateap.cpp
@@ -1096,6 +1096,14 @@ void CPDF_GenerateAP::GenerateFormAP(CPDF_Document* pDoc,
     pStreamDict->SetMatrixFor("Matrix", matrix);
     pStreamDict->SetRectFor("BBox", rcBBox);
   }
+  CPVT_FontMap map(
+      pDoc,
+      pStreamDict ? pStreamDict->GetMutableDictFor("Resources").Get() : nullptr,
+      pDefFont, font_name);
+  CPVT_VariableText::Provider prd(&map);
+  CPVT_VariableText vt;
+  vt.SetProvider(&prd);
+
   switch (type) {
     case CPDF_GenerateAP::kTextField: {
       const CPDF_Object* pV =
@@ -1109,14 +1117,6 @@ void CPDF_GenerateAP::GenerateFormAP(CPDF_Document* pDoc,
       const CPDF_Object* pMaxLen =
           CPDF_FormField::GetFieldAttr(pAnnotDict, "MaxLen");
       uint32_t dwMaxLen = pMaxLen ? pMaxLen->GetInteger() : 0;
-      CPVT_FontMap map(pDoc,
-                       pStreamDict
-                           ? pStreamDict->GetMutableDictFor("Resources").Get()
-                           : nullptr,
-                       pDefFont, font_name);
-      CPVT_VariableText::Provider prd(&map);
-      CPVT_VariableText vt;
-      vt.SetProvider(&prd);
       vt.SetPlateRect(rcBody);
       vt.SetAlignment(nAlign);
       if (FXSYS_IsFloatZero(fFontSize))
@@ -1171,14 +1171,6 @@ void CPDF_GenerateAP::GenerateFormAP(CPDF_Document* pDoc,
       const CPDF_Object* pV =
           CPDF_FormField::GetFieldAttr(pAnnotDict, pdfium::form_fields::kV);
       WideString swValue = pV ? pV->GetUnicodeText() : WideString();
-      CPVT_FontMap map(pDoc,
-                       pStreamDict
-                           ? pStreamDict->GetMutableDictFor("Resources").Get()
-                           : nullptr,
-                       pDefFont, font_name);
-      CPVT_VariableText::Provider prd(&map);
-      CPVT_VariableText vt;
-      vt.SetProvider(&prd);
       CFX_FloatRect rcButton = rcBody;
       rcButton.left = rcButton.right - 13;
       rcButton.Normalize();
@@ -1242,12 +1234,6 @@ void CPDF_GenerateAP::GenerateFormAP(CPDF_Document* pDoc,
       break;
     }
     case CPDF_GenerateAP::kListBox: {
-      CPVT_FontMap map(pDoc,
-                       pStreamDict
-                           ? pStreamDict->GetMutableDictFor("Resources").Get()
-                           : nullptr,
-                       pDefFont, font_name);
-      CPVT_VariableText::Provider prd(&map);
       CPDF_Array* pOpts =
           ToArray(CPDF_FormField::GetFieldAttr(pAnnotDict, "Opt"));
       CPDF_Array* pSels =
@@ -1280,15 +1266,13 @@ void CPDF_GenerateAP::GenerateFormAP(CPDF_Document* pDoc,
                 }
               }
             }
-            CPVT_VariableText vt;
-            vt.SetProvider(&prd);
             vt.SetPlateRect(
                 CFX_FloatRect(rcBody.left, 0.0f, rcBody.right, 0.0f));
             vt.SetFontSize(FXSYS_IsFloatZero(fFontSize) ? 12.0f : fFontSize);
-
             vt.Initialize();
             vt.SetText(swItem);
             vt.RearrangeAll();
+
             float fItemHeight = vt.GetContentRect().Height();
             if (bSelected) {
               CFX_FloatRect rcItem = CFX_FloatRect(
