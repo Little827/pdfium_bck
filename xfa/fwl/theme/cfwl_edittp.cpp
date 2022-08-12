aeb36d9907acb77db470ecd737a0030d73a7162b
Tom Sepez <tsepez@chromium.org>
2022-08-12 17:02:59 +0000

    Introduce CFGAS_GEGraphics::StateRestorer
    Change-Id: Iaf8561ff6219156b337cd09d81ae16ca3d47d8bd

--- xfa/fwl/theme/cfwl_edittp.cpp
+++ xfa/fwl/theme/cfwl_edittp.cpp
@@ -26,14 +26,13 @@ void CFWL_EditTP::DrawBackground(const CFWL_ThemeBackground& pParams) {
     case CFWL_ThemePart::Part::kBackground: {
       if (pParams.m_pPath) {
         CFGAS_GEGraphics* pGraphics = pParams.GetGraphics();
-        pGraphics->SaveGraphState();
+        CFGAS_GEGraphics::StateRestorer restorer(pGraphics);
         pGraphics->SetFillColor(CFGAS_GEColor(FWLTHEME_COLOR_BKSelected));
         if (pParams.m_pPath) {
           pGraphics->FillPath(*pParams.m_pPath,
                               CFX_FillRenderOptions::FillType::kWinding,
                               pParams.m_matrix);
         }
-        pGraphics->RestoreGraphState();
       } else {
         CFGAS_GEPath path;
         path.AddRectangle(pParams.m_PartRect.left, pParams.m_PartRect.top,
@@ -47,11 +46,10 @@ void CFWL_EditTP::DrawBackground(const CFWL_ThemeBackground& pParams) {
           else
             cr = CFGAS_GEColor(0xFFFFFFFF);
         }
-        pParams.GetGraphics()->SaveGraphState();
+        CFGAS_GEGraphics::StateRestorer restorer(pParams.GetGraphics());
         pParams.GetGraphics()->SetFillColor(cr);
         pParams.GetGraphics()->FillPath(
             path, CFX_FillRenderOptions::FillType::kWinding, pParams.m_matrix);
-        pParams.GetGraphics()->RestoreGraphState();
       }
       break;
     }
