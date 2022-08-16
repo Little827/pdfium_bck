a596a07c90c117fd67aec493eb9c26c8f24f79c7
Tom Sepez <tsepez@chromium.org>
2022-08-16 23:03:05 +0000

    Make CFWL_ThemePart::m_iPart const and private
    Bug: pdfium:1680
    Change-Id: I45cca06b38a92fe26848e5a1b20060fe6563728d

--- xfa/fwl/cfwl_combobox.cpp
+++ xfa/fwl/cfwl_combobox.cpp
@@ -107,8 +107,8 @@ void CFWL_ComboBox::DrawWidget(CFGAS_GEGraphics* pGraphics,
     CFGAS_GEGraphics::StateRestorer restorer(pGraphics);
     pGraphics->ConcatMatrix(matrix);
     if (!m_BtnRect.IsEmpty(0.1f)) {
-      CFWL_ThemeBackground param(this, pGraphics);
-      param.m_iPart = CFWL_ThemePart::Part::kDropDownButton;
+      CFWL_ThemeBackground param(CFWL_ThemePart::Part::kDropDownButton, this,
+                                 pGraphics);
       param.m_dwStates = m_iBtnState;
       param.m_PartRect = m_BtnRect;
       GetThemeProvider()->DrawBackground(param);
@@ -281,7 +281,7 @@ void CFWL_ComboBox::Layout() {
                   fBtn - borderWidth, m_ClientRect.height - 2 * borderWidth);
   }
 
-  CFWL_ThemePart part(this);
+  CFWL_ThemePart part(CFWL_ThemePart::Part::kNone, this);
   CFX_RectF pUIMargin = theme->GetUIMargin(part);
   m_ContentRect.Deflate(pUIMargin.left, pUIMargin.top, pUIMargin.width,
                         pUIMargin.height);
