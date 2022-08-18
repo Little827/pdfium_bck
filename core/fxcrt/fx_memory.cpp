11453e4612ceec987b5375a0bf52d4e1062540a2
Tom Sepez <tsepez@chromium.org>
2022-08-18 21:49:40 +0000

    Conditionalize Partition Alloc header includes for MSVC
    Change-Id: I3cd6442a1125db465f884a737f7d107b14a20ab5

--- core/fxcrt/fx_memory.cpp
+++ core/fxcrt/fx_memory.cpp
@@ -13,7 +13,6 @@
 
 #include "build/build_config.h"
 #include "core/fxcrt/fx_safe_types.h"
-#include "third_party/base/allocator/partition_allocator/partition_alloc.h"
 #include "third_party/base/debug/alias.h"
 #include "third_party/base/no_destructor.h"
 
@@ -27,6 +26,8 @@
 #endif
 
 #if defined(FX_MEMORY_USE_PA)
+#include "third_party/base/allocator/partition_allocator/partition_alloc.h"
+
 namespace {
 
 pdfium::base::PartitionAllocatorGeneric& GetArrayBufferPartitionAllocator() {
@@ -48,6 +49,8 @@ pdfium::base::PartitionAllocatorGeneric& GetStringPartitionAllocator() {
 }
 
 }  // namespace
+#else
+#error "no soup for you"
 #endif  // defined(FX_MEMORY_USE_PA)
 
 void FX_InitializeMemoryAllocators() {
