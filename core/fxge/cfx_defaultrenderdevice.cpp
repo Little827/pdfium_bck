40d33d7d4f3b073135537afffb23d89e857bfedf
Alan Screen <awscreen@chromium.org>
2022-08-19 00:23:32 -0700

    Rename render create and attach implementations
    Update the Skia and AGG specific methods for AttachImpl() and Create()
    to have unique names, so that they can coexist in a build.  Introduce
    an extra layer which then selects Skia or AGG based upon the current
    build definitions.  This is a step towards allowing to choose between
    these at runtime.
    
    Bug: pdfium:11
    Change-Id: I866d35d599f0525d832cc4adbb775e213e362054

--- core/fxge/cfx_defaultrenderdevice.cpp
+++ core/fxge/cfx_defaultrenderdevice.cpp
@@ -8,6 +8,14 @@
 
 #include "core/fxge/dib/cfx_dibitmap.h"
 
+CFX_DefaultRenderDevice::CFX_DefaultRenderDevice() = default;
+
+CFX_DefaultRenderDevice::~CFX_DefaultRenderDevice() {
+#if defined(_SKIA_SUPPORT_) || defined(_SKIA_SUPPORT_PATHS_)
+  Flush(true);
+#endif
+}
+
 bool CFX_DefaultRenderDevice::Attach(RetainPtr<CFX_DIBitmap> pBitmap) {
   return AttachWithRgbByteOrder(std::move(pBitmap), false);
 }
@@ -24,4 +32,28 @@ bool CFX_DefaultRenderDevice::AttachWithBackdropAndGroupKnockout(
     bool bGroupKnockout) {
   return AttachImpl(std::move(pBitmap), false, std::move(pBackdropBitmap),
                     bGroupKnockout);
+}
+
+bool CFX_DefaultRenderDevice::CFX_DefaultRenderDevice::AttachImpl(
+    RetainPtr<CFX_DIBitmap> pBitmap,
+    bool bRgbByteOrder,
+    RetainPtr<CFX_DIBitmap> pBackdropBitmap,
+    bool bGroupKnockout) {
+#if defined(_SKIA_SUPPORT_) || defined(_SKIA_SUPPORT_PATHS_)
+  return AttachSkiaImpl(pBitmap, bRgbByteOrder, pBackdropBitmap,
+                        bGroupKnockout);
+#else
+  return AttachAggImpl(pBitmap, bRgbByteOrder, pBackdropBitmap, bGroupKnockout);
+#endif
+}
+
+bool CFX_DefaultRenderDevice::Create(int width,
+                                     int height,
+                                     FXDIB_Format format,
+                                     RetainPtr<CFX_DIBitmap> pBackdropBitmap) {
+#if defined(_SKIA_SUPPORT_) || defined(_SKIA_SUPPORT_PATHS_)
+  return CreateSkia(width, height, format, pBackdropBitmap);
+#else
+  return CreateAgg(width, height, format, pBackdropBitmap);
+#endif
 }
