49ccea72340568cead6dafae3c4b37d552ae8d94
Tom Sepez <tsepez@chromium.org>
2022-09-02 23:47:20 +0000

    Return retained objects from indirect object holder, part 3.
    Pass retained results back thru CPDF_Object::GetDirect().
    
    -- flip virtual method and non-virtual wrapper to avoid WrapRetain().
    
    Change-Id: I2cb348b02e34d90dd331f8e39e6878950d7a73aa

--- core/fpdfapi/parser/cpdf_object.h
+++ core/fpdfapi/parser/cpdf_object.h
@@ -58,7 +58,7 @@ class CPDF_Object : public Retainable {
   // copied to the object it points to directly.
   virtual RetainPtr<CPDF_Object> CloneDirectObject() const;
 
-  virtual const CPDF_Object* GetDirect() const;
+  virtual RetainPtr<CPDF_Object> GetMutableDirect();
   virtual ByteString GetString() const;
   virtual WideString GetUnicodeText() const;
   virtual float GetNumber() const;
@@ -112,8 +112,8 @@ class CPDF_Object : public Retainable {
   virtual RetainPtr<CPDF_Reference> MakeReference(
       CPDF_IndirectObjectHolder* holder) const;
 
-  RetainPtr<CPDF_Object> GetMutableDirect();    // Wraps virtual method.
-  RetainPtr<CPDF_Dictionary> GetMutableDict();  // Wraps virtual method.
+  RetainPtr<const CPDF_Object> GetDirect() const;  // Wraps virtual method.
+  RetainPtr<CPDF_Dictionary> GetMutableDict();     // Wraps virtual method.
 
  protected:
   CPDF_Object() = default;
