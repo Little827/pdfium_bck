04060d582e92118f02e53e75e6cb5e503f71399d
Lei Zhang <thestig@chromium.org>
2022-09-02 17:49:01 -0700

    Use DataVector in CPDF_FlateEncoder.
    Switch CPDF_FlateEncoder to use DataVector instead of std::unique_ptr.
    Then the OwnedData struct which holds the std::unique_ptr and associated
    size_t can go away. To avoid having to copy data between the 2 types of
    data containers:
    
    1) Switch FlateEncode() and FlateModule::Encode() to return DataVector.
    2) Switch another use case to just use the data in the CPDF_StreamAcc
       that CPDF_FlateEncoder owns, instead of taking the data. This may
       save another data copy, as CPDF_StreamAcc sometimes has to make a
       copy when giving up its data.
    
    Change-Id: I2d1752dbc6725a45726cf8c88cd14a7fdc1da38b

--- core/fpdfapi/parser/cpdf_flateencoder.cpp
+++ core/fpdfapi/parser/cpdf_flateencoder.cpp
@@ -19,13 +19,6 @@
 #include "third_party/base/check.h"
 #include "third_party/base/numerics/safe_conversions.h"
 
-CPDF_FlateEncoder::OwnedData::OwnedData(
-    std::unique_ptr<uint8_t, FxFreeDeleter> buffer,
-    uint32_t size)
-    : buffer(std::move(buffer)), size(size) {}
-
-CPDF_FlateEncoder::OwnedData::~OwnedData() = default;
-
 CPDF_FlateEncoder::CPDF_FlateEncoder(const CPDF_Stream* pStream,
                                      bool bFlateEncode)
     : m_pAcc(pdfium::MakeRetain<CPDF_StreamAcc>(pStream)) {
@@ -36,7 +29,7 @@ CPDF_FlateEncoder::CPDF_FlateEncoder(const CPDF_Stream* pStream,
     auto pDestAcc = pdfium::MakeRetain<CPDF_StreamAcc>(pStream);
     pDestAcc->LoadAllDataFiltered();
 
-    m_Data.emplace<OwnedData>(pDestAcc->DetachData(), pDestAcc->GetSize());
+    m_Data = m_pAcc->GetSpan();
     m_pClonedDict = ToDictionary(pStream->GetDict()->Clone());
     m_pClonedDict->RemoveFor("Filter");
     DCHECK(!m_pDict);
@@ -49,15 +42,11 @@ CPDF_FlateEncoder::CPDF_FlateEncoder(const CPDF_Stream* pStream,
     return;
   }
 
-  // TODO(thestig): Move to Init() and check return value.
-  std::unique_ptr<uint8_t, FxFreeDeleter> buffer;
-  uint32_t buffer_size;
-  ::FlateEncode(m_pAcc->GetSpan(), &buffer, &buffer_size);
-
-  m_Data.emplace<OwnedData>(std::move(buffer), buffer_size);
+  // TODO(thestig): Move to Init() and check for empty return value?
+  m_Data = ::FlateEncode(m_pAcc->GetSpan());
   m_pClonedDict = ToDictionary(pStream->GetDict()->Clone());
   m_pClonedDict->SetNewFor<CPDF_Number>(
-      "Length", pdfium::base::checked_cast<int>(buffer_size));
+      "Length", pdfium::base::checked_cast<int>(GetSpan().size()));
   m_pClonedDict->SetNewFor<CPDF_Name>("Filter", "FlateDecode");
   m_pClonedDict->RemoveFor(pdfium::stream::kDecodeParms);
   DCHECK(!m_pDict);
@@ -91,9 +80,7 @@ const CPDF_Dictionary* CPDF_FlateEncoder::GetDict() const {
 }
 
 pdfium::span<const uint8_t> CPDF_FlateEncoder::GetSpan() const {
-  if (is_owned()) {
-    const OwnedData& data = absl::get<OwnedData>(m_Data);
-    return pdfium::make_span(data.buffer.get(), data.size);
-  }
+  if (is_owned())
+    return pdfium::make_span(absl::get<DataVector<uint8_t>>(m_Data));
   return absl::get<pdfium::span<const uint8_t>>(m_Data);
 }
