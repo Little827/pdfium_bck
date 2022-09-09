f6afad6b144dd19fa9d7a5f7780f7e689616b537
Alan Screen <awscreen@chromium.org>
2022-09-08 17:23:20 -0700

    Add a flag for selecting renderer at runtime
    Introduce an experimental config flag FPDF_LIBRARY_CONFIG to allow for
    changing the renderer at runtime. This flag only takes effect if the
    config version is 4 or higher. When using a config version of 4, it is
    necessary to specify a valid renderer type that will be used to specify
    the renderer type at runtime. This is required even if a build does not
    include Skia definitions and only has AGG as the available renderer
    type.
    
    Expose this flag in the pdfium_test sample, via the --use-renderer
    command line option. This option is only available for builds which
    are Skia enabled.
    
    Bug: pdfium:1878
    Change-Id: I971f8f7d420015a919316b1b3d57fe3d7bea4ce4

--- core/fxge/cfx_defaultrenderdevice.h
+++ core/fxge/cfx_defaultrenderdevice.h
@@ -49,6 +49,24 @@ class CFX_DefaultRenderDevice final : public CFX_RenderDevice {
   // Runtime check to see if SkiaPaths is the renderer variant in use.
   static bool SkiaPathsIsDefaultRenderer();
 
+#if defined(_SKIA_SUPPORT_) || defined(_SKIA_SUPPORT_PATHS_)
+  // These values need to stay updated with respect to public type
+  // `FPDF_RENDERER_TYPE`, so that each of that type's value has an appropriate
+  // mapping to a value here.
+  enum class RendererType {
+    kAgg = 0,
+#if defined(_SKIA_SUPPORT_)
+    kSkia = 1,
+#endif
+#if defined(_SKIA_SUPPORT_PATHS_)
+    kSkiaPaths = 2,
+#endif
+  };
+
+  // Update default renderer.
+  static void SetDefaultRenderer(RendererType renderer_type);
+#endif
+
  private:
   bool AttachImpl(RetainPtr<CFX_DIBitmap> pBitmap,
                   bool bRgbByteOrder,
