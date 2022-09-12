ea52a96f67f792288575275b1c65270b0e6a318e
Tom Sepez <tsepez@chromium.org>
2022-09-12 19:13:24 +0000

    Add additional dictionary locker methods.
    Allow returning unretained references to the full complement of
    objects from locked dictionaries.
    
    -- re-write other dictionary methods in terms of new internal ones.
    -- add tests for new cases.
    
    Change-Id: Ie44fbf17ab5a1689e39e1e593d9456b6ce505273

--- core/fpdfapi/parser/cpdf_dictionary_unittest.cpp
+++ core/fpdfapi/parser/cpdf_dictionary_unittest.cpp
@@ -7,18 +7,45 @@
 #include <utility>
 
 #include "core/fpdfapi/parser/cpdf_array.h"
+#include "core/fpdfapi/parser/cpdf_number.h"
+#include "core/fpdfapi/parser/cpdf_stream.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 TEST(DictionaryTest, LockerGetters) {
   auto dict = pdfium::MakeRetain<CPDF_Dictionary>();
   dict->SetNewFor<CPDF_Dictionary>("A");
   dict->SetNewFor<CPDF_Array>("B");
+  dict->SetNewFor<CPDF_Stream>("C");
+  dict->SetNewFor<CPDF_Number>("D", 42);
 
   CPDF_DictionaryLocker locked_dict(std::move(dict));
   EXPECT_TRUE(locked_dict.GetObjectFor("A"));
   EXPECT_FALSE(locked_dict.GetArrayFor("A"));
   EXPECT_TRUE(locked_dict.GetDictFor("A"));
+  EXPECT_FALSE(locked_dict.GetStreamFor("A"));
+  EXPECT_FALSE(locked_dict.GetNumberFor("A"));
+
   EXPECT_TRUE(locked_dict.GetObjectFor("B"));
   EXPECT_TRUE(locked_dict.GetArrayFor("B"));
   EXPECT_FALSE(locked_dict.GetDictFor("B"));
+  EXPECT_FALSE(locked_dict.GetStreamFor("B"));
+  EXPECT_FALSE(locked_dict.GetNumberFor("B"));
+
+  EXPECT_TRUE(locked_dict.GetObjectFor("C"));
+  EXPECT_FALSE(locked_dict.GetArrayFor("C"));
+  EXPECT_FALSE(locked_dict.GetDictFor("C"));
+  EXPECT_TRUE(locked_dict.GetStreamFor("C"));
+  EXPECT_FALSE(locked_dict.GetNumberFor("C"));
+
+  EXPECT_TRUE(locked_dict.GetObjectFor("D"));
+  EXPECT_FALSE(locked_dict.GetArrayFor("D"));
+  EXPECT_FALSE(locked_dict.GetDictFor("D"));
+  EXPECT_FALSE(locked_dict.GetStreamFor("D"));
+  EXPECT_TRUE(locked_dict.GetNumberFor("D"));
+
+  EXPECT_FALSE(locked_dict.GetObjectFor("E"));
+  EXPECT_FALSE(locked_dict.GetArrayFor("E"));
+  EXPECT_FALSE(locked_dict.GetDictFor("E"));
+  EXPECT_FALSE(locked_dict.GetStreamFor("E"));
+  EXPECT_FALSE(locked_dict.GetNumberFor("E"));
 }
