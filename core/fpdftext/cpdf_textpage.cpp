f91e29fedb297552e0c58df9a85a007db27ca2b8
Tom Sepez <tsepez@chromium.org>
2022-09-14 20:05:24 +0000

    Re-introduce CPDF_{Array,Dictionary}::GetStringAt() returning CPDF_String
    Change-Id: I98011349c3ee4c004492af7e77a03e1d34b34205

--- core/fpdftext/cpdf_textpage.cpp
+++ core/fpdftext/cpdf_textpage.cpp
@@ -831,7 +831,7 @@ CPDF_TextPage::MarkedContentState CPDF_TextPage::PreMarkedContent(
     pDict = item->GetParam();
     if (!pDict)
       continue;
-    const CPDF_String* temp = ToString(pDict->GetObjectFor("ActualText"));
+    RetainPtr<const CPDF_String> temp = pDict->GetStringFor("ActualText");
     if (temp) {
       bExist = true;
       actText = temp->GetUnicodeText();
