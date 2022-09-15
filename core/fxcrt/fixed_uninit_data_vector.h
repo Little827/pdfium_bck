4ffa7e409b4e8cd5e751d994349b5855bf81192f
Lei Zhang <thestig@chromium.org>
2022-09-15 10:51:08 -0700

    Use FixedUninitDataVector in CFX_FontMapper and CFX_FontMgr.
    This is a reland of https://pdfium-review.googlesource.com/97691.
    By introducing a simple FixedUninitDataVector that does not have slow
    downs from initializing its data, this avoids the performance issues
    that affected the first iteration of this CL.
    
    Also removed the now unused CFX_MemoryStream ctor.
    
    Bug: pdfium:1872
    Change-Id: If1e97be6991f033ad6395ad79596cbd3f64767bd

---
+++ core/fxcrt/fixed_uninit_data_vector.h
@@ -0,0 +1,62 @@
+// Copyright 2022 PDFium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CORE_FXCRT_FIXED_UNINIT_DATA_VECTOR_H_
+#define CORE_FXCRT_FIXED_UNINIT_DATA_VECTOR_H_
+
+#include <stddef.h>
+
+#include <memory>
+#include <utility>
+
+#include "core/fxcrt/fx_memory_wrappers.h"
+#include "third_party/base/span.h"
+
+namespace fxcrt {
+
+// A simple data container that has a fixed size.
+// Unlike std::vector, its data starts out uninitialized.
+// Access its data using spans.
+template <typename T>
+class FixedUniniDataVector {
+ public:
+  FixedUniniDataVector() : FixedUniniDataVector(0) {}
+  explicit FixedUniniDataVector(size_t size)
+      : data_(size ? FX_AllocUninit(T, size) : nullptr), size_(size) {}
+  FixedUniniDataVector(const FixedUniniDataVector&) = delete;
+  FixedUniniDataVector& operator=(const FixedUniniDataVector&) = delete;
+  FixedUniniDataVector(FixedUniniDataVector&& that) noexcept {
+    data_ = std::move(that.data_);
+    size_ = that.size_;
+    that.size_ = 0;
+  }
+  FixedUniniDataVector& operator=(FixedUniniDataVector&& that) noexcept {
+    data_ = std::move(that.data_);
+    size_ = that.size_;
+    that.size_ = 0;
+    return *this;
+  }
+  ~FixedUniniDataVector() = default;
+
+  pdfium::span<T> writable_span() {
+    return pdfium::make_span(data_.get(), size_);
+  }
+
+  pdfium::span<const T> span() const {
+    return pdfium::make_span(data_.get(), size_);
+  }
+
+  size_t size() const { return size_; }
+  bool empty() const { return size_ == 0; }
+
+ private:
+  std::unique_ptr<T, FxFreeDeleter> data_;
+  size_t size_;
+};
+
+}  // namespace fxcrt
+
+using fxcrt::FixedUniniDataVector;
+
+#endif  // CORE_FXCRT_FIXED_UNINIT_DATA_VECTOR_H_
