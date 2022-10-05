d270f7b853346cfd92b1632364e535f059aceadc
Lei Zhang <thestig@chromium.org>
2022-10-05 14:09:01 -0700

    Delete unused CPDFSDK_AnnotReverseIteration.
    Remove CPDFSDK_AnnotReverseIteration since it has no users now. Fold
    CPDFSDK_AnnotIteration into CPDFSDK_AnnotForwardIteration.
    
    Change-Id: Ibd3c20b531d6349843dfe9855bda8965d87b9574

--- fpdfsdk/cpdfsdk_annotiteration.h
+++ fpdfsdk/cpdfsdk_annotiteration.h
@@ -13,32 +13,19 @@
 
 class CPDFSDK_PageView;
 
-class CPDFSDK_AnnotIteration {
+class CPDFSDK_AnnotForwardIteration {
  public:
   using const_iterator =
       std::vector<ObservedPtr<CPDFSDK_Annot>>::const_iterator;
 
+  explicit CPDFSDK_AnnotForwardIteration(CPDFSDK_PageView* pPageView);
+  ~CPDFSDK_AnnotForwardIteration();
+
   const_iterator begin() const { return m_List.begin(); }
   const_iterator end() const { return m_List.end(); }
 
- protected:
-  CPDFSDK_AnnotIteration(CPDFSDK_PageView* pPageView, bool bReverse);
-  ~CPDFSDK_AnnotIteration();
-
  private:
   std::vector<ObservedPtr<CPDFSDK_Annot>> m_List;
-};
-
-class CPDFSDK_AnnotForwardIteration : public CPDFSDK_AnnotIteration {
- public:
-  explicit CPDFSDK_AnnotForwardIteration(CPDFSDK_PageView* pPageView);
-  ~CPDFSDK_AnnotForwardIteration();
-};
-
-class CPDFSDK_AnnotReverseIteration : public CPDFSDK_AnnotIteration {
- public:
-  explicit CPDFSDK_AnnotReverseIteration(CPDFSDK_PageView* pPageView);
-  ~CPDFSDK_AnnotReverseIteration();
 };
 
 #endif  // FPDFSDK_CPDFSDK_ANNOTITERATION_H_
