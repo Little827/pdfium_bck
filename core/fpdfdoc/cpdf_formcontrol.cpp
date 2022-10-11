87f31bec6fdeb4ef6ea617348745c29aa718cfca
Tom Sepez <tsepez@chromium.org>
2022-10-11 01:09:55 +0000

    Remove CPDF_InteractiveForm::GetDocument().
    Provide equivalent means for getting a font without vending a
    document.
    
    Change-Id: Ibb9ad37e56fe18b8068b7fdf17359e51ef75e984

--- core/fpdfdoc/cpdf_formcontrol.cpp
+++ core/fpdfdoc/cpdf_formcontrol.cpp
@@ -215,8 +215,7 @@ RetainPtr<CPDF_Font> CPDF_FormControl::GetDefaultControlFont() const {
       RetainPtr<CPDF_Dictionary> pElement =
           pFonts->GetMutableDictFor(csFontNameTag.value());
       if (pElement) {
-        auto* pData = CPDF_DocPageData::FromDocument(m_pForm->GetDocument());
-        RetainPtr<CPDF_Font> pFont = pData->GetFont(pElement);
+        RetainPtr<CPDF_Font> pFont = m_pForm->GetFontForElement(pElement);
         if (pFont)
           return pFont;
       }
@@ -241,8 +240,7 @@ RetainPtr<CPDF_Font> CPDF_FormControl::GetDefaultControlFont() const {
   if (!pElement)
     return nullptr;
 
-  auto* pDocPageData = CPDF_DocPageData::FromDocument(m_pForm->GetDocument());
-  return pDocPageData->GetFont(pElement);
+  return m_pForm->GetFontForElement(pElement);
 }
 
 int CPDF_FormControl::GetControlAlignment() const {
