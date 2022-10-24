a82cab67312ec1bcea7cf1ad5bc54ed30b1c2cc3
Tom Sepez <tsepez@chromium.org>
2022-10-24 23:45:47 +0000

    Hide mumble from CFFL_ComboBox
    Change-Id: I31ee8f00a94f6e6360e076162db990d4c77d5ba9

--- fpdfsdk/cpdfsdk_widget.h
+++ fpdfsdk/cpdfsdk_widget.h
@@ -88,6 +88,8 @@ class CPDFSDK_Widget final : public CPDFSDK_BAAnnot {
   WideString GetValue() const;
   WideString GetExportValue() const;
   WideString GetOptionLabel(int nIndex) const;
+  WideString GetSelectExportText(int nIndex) const;
+
   int CountOptions() const;
   bool IsOptionSelected(int nIndex) const;
   int GetTopVisibleIndex() const;
@@ -134,7 +136,8 @@ class CPDFSDK_Widget final : public CPDFSDK_BAAnnot {
   uint32_t GetAppearanceAge() const { return m_nAppearanceAge; }
   uint32_t GetValueAge() const { return m_nValueAge; }
 
-  bool IsWidgetAppearanceValid(CPDF_Annot::AppearanceMode mode);
+  bool IsWidgetAppearanceValid(CPDF_Annot::AppearanceMode mode) const;
+  bool IsPushHighlighted() const;
   CFX_Matrix GetMatrix() const;
   CFX_FloatRect GetClientRect() const;
   CFX_FloatRect GetRotatedRect() const;
