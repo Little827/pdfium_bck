ed1dd1d39ebcfcb402cb1e3ddb40bc59d92a9f47
Tom Sepez <tsepez@chromium.org>
2022-10-25 16:37:26 +0000

    Add embedder test for FFL_ComboBox::GetSelectExportText()
    Change-Id: If4dfd8d187ccf0b0a5ef9e52adfc7a67cfaf6409

--- fpdfsdk/formfiller/cffl_combobox_embeddertest.cpp
+++ fpdfsdk/formfiller/cffl_combobox_embeddertest.cpp
@@ -9,7 +9,8 @@
 
 class CFFLComboBoxEmbedderTest : public CPWLComboBoxEmbedderTest {};
 
-TEST_F(CFFLComboBoxEmbedderTest, PlaceHolder) {
+TEST_F(CFFLComboBoxEmbedderTest, ExportText) {
   FormFillerAndWindowSetup(GetCPDFSDKAnnotNormal());
-  EXPECT_TRUE(GetCFFLFormField());
+  auto* pComboBox = static_cast<CFFL_ComboBox*>(GetCFFLFormField());
+  EXPECT_EQ(L"Banana", pComboBox->GetSelectExportText());
 }
