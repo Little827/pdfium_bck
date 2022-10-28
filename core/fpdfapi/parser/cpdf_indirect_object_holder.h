c531175bf372fe225dd65328270ff174067dd147
Tom Sepez <tsepez@chromium.org>
2022-10-28 19:15:09 +0000

    Update comments about ownership in cpdf_indirect_object_holder.h
    Change-Id: Ie652450ecca20ba87cbd80f87ed69a9b29340a22

--- core/fpdfapi/parser/cpdf_indirect_object_holder.h
+++ core/fpdfapi/parser/cpdf_indirect_object_holder.h
@@ -31,7 +31,7 @@ class CPDF_IndirectObjectHolder {
   RetainPtr<CPDF_Object> GetMutableIndirectObject(uint32_t objnum);
   void DeleteIndirectObject(uint32_t objnum);
 
-  // Creates and adds a new object owned by the indirect object holder,
+  // Creates and adds a new object retained by the indirect object holder,
   // and returns a retained pointer to it.  We have a special case to
   // handle objects that can intern strings from our ByteStringPool.
   template <typename T, typename... Args>
@@ -48,7 +48,7 @@ class CPDF_IndirectObjectHolder {
             m_pByteStringPool, std::forward<Args>(args)...))));
   }
 
-  // Creates and adds a new object not owned by the indirect object holder,
+  // Creates and adds a new object not retained by the indirect object holder,
   // but which can intern strings from it.
   template <typename T, typename... Args>
   typename std::enable_if<CanInternStrings<T>::value, RetainPtr<T>>::type New(
