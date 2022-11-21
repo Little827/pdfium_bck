137f4d7e1ad792c36ca50e5a50d7458f230aed48
Tom Sepez <tsepez@chromium.org>
2022-11-21 21:58:57 +0000

    Move Fx2DSizeOrDie() to a new fx_2d_size.h header file.
    Then make it a template function and try to get inlining.
    
    Change-Id: I3ba409578b6a6a966d6f6446b6794ef2d01b8e7f

--- core/fxge/skia/fx_skia_device.cpp
+++ core/fxge/skia/fx_skia_device.cpp
@@ -25,6 +25,7 @@
 #include "core/fpdfapi/parser/cpdf_stream_acc.h"
 #include "core/fxcrt/cfx_bitstream.h"
 #include "core/fxcrt/data_vector.h"
+#include "core/fxcrt/fx_2d_size.h"
 #include "core/fxcrt/fx_memory.h"
 #include "core/fxcrt/fx_memory_wrappers.h"
 #include "core/fxcrt/fx_system.h"
