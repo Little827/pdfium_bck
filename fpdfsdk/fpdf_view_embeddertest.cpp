b514b9d742cd11b223b70457ae827dc1146e2a71
Karim <abdelkarim.se@gmail.com>
2023-01-24 12:36:09 +0100

    Fix overlapping glyphs rendering for aliased text
    Bug: pdfium:1919
    
    Change-Id: I241ccb6ab67ec7b5eb36afb0f090439848cc21d9

--- fpdfsdk/fpdf_view_embeddertest.cpp
+++ fpdfsdk/fpdf_view_embeddertest.cpp
@@ -38,7 +38,7 @@
 #include "third_party/skia/include/core/SkRefCnt.h"           // nogncheck
 #include "third_party/skia/include/core/SkSize.h"             // nogncheck
 #include "third_party/skia/include/core/SkSurface.h"          // nogncheck
-#endif  // _SKIA_SUPPORT_
+#endif                                                        // _SKIA_SUPPORT_
 
 using pdfium::ManyRectanglesChecksum;
 
@@ -2032,3 +2032,13 @@ TEST_F(FPDFViewEmbedderTest, RenderXfaPageToSkp) {
   UnloadPage(page);
 }
 #endif  // _SKIA_SUPPORT_
+
+TEST_F(FPDFViewEmbedderTest, NoSmoothTextItalicOverlappingGlyphs) {
+  ASSERT_TRUE(OpenDocument("bug_1919.pdf"));
+  FPDF_PAGE page = LoadPage(0);
+  ASSERT_TRUE(page);
+
+  TestRenderPageBitmapWithFlags(page, FPDF_RENDER_NO_SMOOTHTEXT,
+                                "4ef1f65ab1ac76acb97a3540dcb10b4e");
+  UnloadPage(page);
+}
