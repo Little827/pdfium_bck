3d6834faa42f88b05aa674bf25b631d7961c1953
Lei Zhang <thestig@chromium.org>
2023-01-26 10:45:17 -0800

    Call a Skia initialization function from pdfium_test.
    Since pdfium_test can be built in a Chromium checkout with a different
    version of Skia, it needs to be able to do some initialization work for
    that version of Skia. On the Chromium side, https://crrev.com/c/4196855
    implements InitializeDiscardableMemoryAllocatorForTesting(). Here,
    pdfium_test calls the function.
    
    To make standalone PDFium builds works, provide a dummy
    InitializeDiscardableMemoryAllocatorForTesting() in PDFium's Skia code.
    
    Bug: chromium:1383711
    Change-Id: I330e696f0298859d4b72d11577443202b543dec6

---
+++ skia/ext/skia_discardable_memory_allocator.cc
@@ -0,0 +1,11 @@
+// Copyright 2023 The PDFium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "skia/ext/skia_discardable_memory_allocator.h"
+
+namespace skia {
+
+void InitializeDiscardableMemoryAllocatorForTesting() {}
+
+}  // namespace skia
---
+++ skia/ext/skia_discardable_memory_allocator.h
@@ -0,0 +1,15 @@
+// Copyright 2023 The PDFium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SKIA_EXT_SKIA_DISCARDABLE_MEMORY_ALLOCATOR_H_
+#define SKIA_EXT_SKIA_DISCARDABLE_MEMORY_ALLOCATOR_H_
+
+namespace skia {
+
+// Dummy function exists for compatibility with Chromium's Skia.
+void InitializeDiscardableMemoryAllocatorForTesting();
+
+}  // namespace skia
+
+#endif  // SKIA_EXT_SKIA_DISCARDABLE_MEMORY_ALLOCATOR_H_
