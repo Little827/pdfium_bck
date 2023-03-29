0db49faca473539ab3e52ae8d992b305ae08f653
Lei Zhang <thestig@chromium.org>
2023-03-29 16:13:27 -0700

    Add object tree traversal utility functions
    Add GetObjectsWithMultipleReferences() to figure out which objects have
    multiple references. Then code that manipulates PDFs can use this
    function to determine if it is safe to edit an existing object. If it is
    unsafe, the code can clone before editing instead.
    
    Also add GetObjectsWithReference() to figure out which objects are
    referenced. This can be used to determine which objects have no
    references and can therefore be deleted.
    
    Bug: chromium:1428724,pdfium:1409,pdfium:2012
    Change-Id: I28817068d50c79f36d12bd50c1910937241194a5

---
+++ core/fpdfapi/parser/object_tree_traversal_util.cpp
@@ -0,0 +1,196 @@
+// Copyright 2023 The PDFium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/fpdfapi/parser/object_tree_traversal_util.h"
+
+#include <stdint.h>
+
+#include <map>
+#include <queue>
+#include <set>
+#include <utility>
+
+#include "core/fpdfapi/parser/cpdf_array.h"
+#include "core/fpdfapi/parser/cpdf_dictionary.h"
+#include "core/fpdfapi/parser/cpdf_document.h"
+#include "core/fpdfapi/parser/cpdf_reference.h"
+#include "core/fpdfapi/parser/cpdf_stream.h"
+#include "third_party/base/check.h"
+#include "third_party/base/containers/contains.h"
+
+namespace {
+
+class TraversalContext {
+ public:
+  explicit TraversalContext(const CPDF_Dictionary* root)
+      : object_queue_({pdfium::WrapRetain(root)}), seen_objects_({root}) {
+    const uint32_t root_object_number = root->GetObjNum();
+    // If `root` is a trailer, then it may not have an object number, as many
+    // trailers are inlined.
+    if (root_object_number) {
+      object_number_map_[root] = root_object_number;
+    }
+  }
+  ~TraversalContext() = default;
+
+  const CPDF_Object* PopCurrentObject() {
+    if (object_queue_.empty()) {
+      return nullptr;
+    }
+
+    const CPDF_Object* current_object = object_queue_.front();
+    object_queue_.pop();
+    return current_object;
+  }
+
+  void PushNewObject(const CPDF_Object* parent_object,
+                     const CPDF_Object* child_object) {
+    CHECK(parent_object);
+    CHECK(child_object);
+    const bool inserted = seen_objects_.insert(child_object).second;
+    if (!inserted) {
+      return;
+    }
+    const uint32_t child_object_number = child_object->GetObjNum();
+    if (child_object_number) {
+      object_number_map_[child_object] = child_object_number;
+    } else {
+      // This search can fail for inlined trailers.
+      auto it = object_number_map_.find(parent_object);
+      if (it != object_number_map_.end()) {
+        object_number_map_[child_object] = it->second;
+      }
+    }
+    object_queue_.push(child_object);
+  }
+
+  // Returns 0 if not found.
+  uint32_t GetObjectNumber(const CPDF_Object* object) const {
+    auto it = object_number_map_.find(object);
+    return it != object_number_map_.end() ? it->second : 0;
+  }
+
+ private:
+  // Queue of objects to traverse.
+  // - Pointers in the queue are non-null.
+  // - The same pointer never enters the queue twice.
+  std::queue<const CPDF_Object*> object_queue_;
+
+  // Map of objects to "top-level" object numbers. For inline objects, this is
+  // the ancestor object with an object number. The keys are non-null and the
+  // values are never 0.
+  // This is used to prevent self-references, as a single PDF object, with
+  // inlined objects, is represented by multiple CPDF_Objects.
+  std::map<const CPDF_Object*, uint32_t> object_number_map_;
+
+  // Tracks traversed objects to prevent duplicates from getting into
+  // `object_queue_` and `object_number_map_`.
+  std::set<const CPDF_Object*> seen_objects_;
+};
+
+std::map<uint32_t, int> GetObjectsReferenceMap(const CPDF_Document* document) {
+  // Set of objects that referenced other objects.
+  std::set<uint32_t> seen_ref_objects;
+  // Map of object number to number of times referenced.
+  std::map<uint32_t, int> referenced_objects;
+
+  const CPDF_Parser* parser = document->GetParser();
+  const CPDF_Dictionary* trailer = parser ? parser->GetTrailer() : nullptr;
+  const CPDF_Dictionary* root = trailer ? trailer : document->GetRoot();
+  const uint32_t root_object_number =
+      trailer ? parser->GetTrailerObjectNumber() : root->GetObjNum();
+  // If `root` is a trailer, then it may not have an object number, as many
+  // trailers are inlined.
+  if (root_object_number) {
+    referenced_objects[root_object_number] = 1;
+  }
+
+  TraversalContext context(root);
+  while (const CPDF_Object* current_object = context.PopCurrentObject()) {
+    switch (current_object->GetType()) {
+      case CPDF_Object::kArray: {
+        CPDF_ArrayLocker locker(current_object->AsArray());
+        for (const auto& it : locker) {
+          context.PushNewObject(current_object, it.Get());
+        }
+        break;
+      }
+      case CPDF_Object::kDictionary: {
+        CPDF_DictionaryLocker locker(current_object->AsDictionary());
+        for (const auto& it : locker) {
+          context.PushNewObject(current_object, it.second.Get());
+        }
+        break;
+      }
+      case CPDF_Object::kReference: {
+        const CPDF_Reference* ref_object = current_object->AsReference();
+        const uint32_t referenced_object_number = ref_object->GetRefObjNum();
+        CHECK(referenced_object_number);
+
+        RetainPtr<const CPDF_Object> referenced_object;
+        if (ref_object->HasIndirectObjectHolder()) {
+          // Calling GetIndirectObject() does not work for normal references.
+          referenced_object = ref_object->GetDirect();
+        } else {
+          // Calling GetDirect() does not work for references from trailers.
+          referenced_object =
+              document->GetIndirectObject(referenced_object_number);
+        }
+        // Unlike the other object types, CPDF_Reference can point at nullptr.
+        if (referenced_object) {
+          // Make sure this is not a self-reference / circular reference.
+          const uint32_t ref_object_number =
+              context.GetObjectNumber(ref_object);
+          if (referenced_object_number != ref_object_number &&
+              (!pdfium::Contains(seen_ref_objects, ref_object_number) ||
+               !pdfium::Contains(seen_ref_objects, referenced_object_number))) {
+            ++referenced_objects[referenced_object_number];
+            if (ref_object_number) {
+              seen_ref_objects.insert(ref_object_number);
+            }
+          }
+          context.PushNewObject(ref_object, referenced_object);
+        }
+        break;
+      }
+      case CPDF_Object::kStream: {
+        RetainPtr<const CPDF_Dictionary> dict =
+            current_object->AsStream()->GetDict();
+        CHECK(dict->IsInline());  // i.e. No object number.
+        CPDF_DictionaryLocker locker(dict);
+        for (const auto& it : locker) {
+          context.PushNewObject(current_object, it.second.Get());
+        }
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  }
+  return referenced_objects;
+}
+
+}  // namespace
+
+std::set<uint32_t> GetObjectsWithReferences(const CPDF_Document* document) {
+  std::set<uint32_t> results;
+  std::map<uint32_t, int> referenced_objects = GetObjectsReferenceMap(document);
+  for (const auto& it : referenced_objects) {
+    results.insert(it.first);
+  }
+  return results;
+}
+
+std::set<uint32_t> GetObjectsWithMultipleReferences(
+    const CPDF_Document* document) {
+  std::set<uint32_t> results;
+  std::map<uint32_t, int> referenced_objects = GetObjectsReferenceMap(document);
+  for (const auto& it : referenced_objects) {
+    if (it.second > 1) {
+      results.insert(it.first);
+    }
+  }
+  return results;
+}
