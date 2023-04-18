e08c7e4b220dd2a6f4e72d0c362177183b7d3bce
Lei Zhang <thestig@chromium.org>
2023-04-18 14:24:26 -0700

    Represnet fxcmap::CMap::MapType with a bool
    Since this enum only has 2 values, use a bool to represent it. Then the
    switch statements that check this enum no longer need the default cases.
    
    Change-Id: I6700dbec0467f2ea820b97ca67b645b2dc829773

--- core/fpdfapi/cmaps/fpdf_cmaps.h
+++ core/fpdfapi/cmaps/fpdf_cmaps.h
@@ -19,7 +19,7 @@ struct DWordCIDMap {
 };
 
 struct CMap {
-  enum class MapType : uint8_t { kSingle, kRange };
+  enum class MapType : bool { kSingle, kRange };
 
   const char* m_Name;              // Raw, POD struct.
   const uint16_t* m_pWordMap;      // Raw, POD struct.
