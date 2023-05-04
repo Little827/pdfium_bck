21a2d02b19f860873140aa72bee3092d9dc4f94a
K. Moon <kmoon@chromium.org>
2023-05-04 02:32:09 -0700

    Handle non-BMP code points for UTF-16 wchar_t
    Correctly encodes supplementary (non-BMP) code points in WideString as
    UTF-16 if wchar_t is 16 bits (as on Windows), rather than 32 bits (as on
    Linux).
    
    Bug: pdfium:2029
    Change-Id: I662ac7fb08bd48267e32da5f36fd1c9c2bb70717

--- core/fxcrt/cfx_utf8encoder.cpp
+++ core/fxcrt/cfx_utf8encoder.cpp
@@ -8,12 +8,36 @@
 
 #include <stdint.h>
 
+#include "build/build_config.h"
+
 CFX_UTF8Encoder::CFX_UTF8Encoder() = default;
 
 CFX_UTF8Encoder::~CFX_UTF8Encoder() = default;
 
 void CFX_UTF8Encoder::Input(wchar_t unicodeAsWchar) {
-  char32_t code_point = static_cast<char32_t>(unicodeAsWchar);
+#if defined(WCHAR_T_IS_UTF16)
+  if (unicodeAsWchar >= 0xd800 && unicodeAsWchar < 0xdc00) {
+    // High surrogate.
+    high_surrogate_ = unicodeAsWchar;
+  } else if (unicodeAsWchar >= 0xdc00 && unicodeAsWchar <= 0xdfff) {
+    // Low surrogate.
+    if (high_surrogate_) {
+      char32_t code_point = unicodeAsWchar & 0x3ff;
+      code_point |= (high_surrogate_ & 0x3ff) << 10;
+      code_point += 0x10000;
+      high_surrogate_ = 0;
+      AppendCodePoint(code_point);
+    }
+  } else {
+    high_surrogate_ = 0;
+    AppendCodePoint(unicodeAsWchar);
+  }
+#else
+  AppendCodePoint(unicodeAsWchar);
+#endif  // defined(WCHAR_T_IS_UTF16)
+}
+
+void CFX_UTF8Encoder::AppendCodePoint(char32_t code_point) {
   if (code_point > 0x10ffff) {
     // Invalid code point above U+10FFFF.
     return;
