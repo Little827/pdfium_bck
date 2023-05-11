0282dec790baebd905e357131cdee11073dabed5
Tom Sepez <tsepez@chromium.org>
2023-05-11 16:06:57 +0000

    Make BRP actually compatible with tests.
    Introduce our own shim while we wait for the generic one to become
    available.
    
    Change-Id: I3ff59f0cd46b9bf5e01e165a70cf995d96830fa5

---
+++ testing/utils/malloc_shim.cpp
@@ -0,0 +1,85 @@
+// Copyright 2023 The PDFium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <new>
+
+#include "base/allocator/partition_allocator/partition_alloc.h"
+#include "build/build_config.h"
+#include "third_party/base/no_destructor.h"
+
+#if !defined(PDF_USE_PARTITION_ALLOC)
+#error "Malloc shim must use partition alloc."
+#endif
+
+namespace {
+
+constexpr partition_alloc::PartitionOptions kOptions = {
+    partition_alloc::PartitionOptions::AlignedAlloc::kDisallowed,
+    partition_alloc::PartitionOptions::ThreadCache::kDisabled,
+    partition_alloc::PartitionOptions::Quarantine::kDisallowed,
+    partition_alloc::PartitionOptions::Cookie::kAllowed,
+#if BUILDFLAG(ENABLE_BACKUP_REF_PTR_SUPPORT)
+    partition_alloc::PartitionOptions::BackupRefPtr::kEnabled,
+    partition_alloc::PartitionOptions::BackupRefPtrZapping::kEnabled,
+#else
+    partition_alloc::PartitionOptions::BackupRefPtr::kDisabled,
+    partition_alloc::PartitionOptions::BackupRefPtrZapping::kDisabled,
+#endif
+    partition_alloc::PartitionOptions::UseConfigurablePool::kNo,
+};
+
+partition_alloc::PartitionAllocator& GetBrpPartitionAllocator() {
+  static bool s_allocator_initialized = false;
+  static pdfium::base::NoDestructor<partition_alloc::PartitionAllocator>
+      s_allocator;
+
+  if (!s_allocator_initialized) {
+    s_allocator_initialized = true;  // Set flag first ...
+    s_allocator->init(kOptions);     // ... may re-enter.
+  }
+  return *s_allocator;
+}
+
+void* MallocShimCppNewOrDie(size_t size) noexcept {
+  return GetBrpPartitionAllocator().root()->AllocWithFlags(0, size,
+                                                           "BRP Partition");
+}
+
+void MallocShimCppDelete(void* p) noexcept {
+  GetBrpPartitionAllocator().root()->Free(p);
+}
+
+}  // namespace
+
+void* operator new(size_t size) {
+  return MallocShimCppNewOrDie(size);
+}
+
+void* operator new[](size_t size) {
+  return MallocShimCppNewOrDie(size);
+}
+
+void operator delete(void* p) {
+  MallocShimCppDelete(p);
+}
+
+void operator delete[](void* p) {
+  MallocShimCppDelete(p);
+}
+
+void* operator new(size_t size, const std::nothrow_t&) noexcept {
+  return MallocShimCppNewOrDie(size);
+}
+
+void* operator new[](size_t size, const std::nothrow_t&) noexcept {
+  return MallocShimCppNewOrDie(size);
+}
+
+void operator delete(void* p, const std::nothrow_t&) noexcept {
+  MallocShimCppDelete(p);
+}
+
+void operator delete[](void* p, const std::nothrow_t&) noexcept {
+  MallocShimCppDelete(p);
+}
