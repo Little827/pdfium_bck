c8c0d86a73275f962829dfd965b51be3cea6017e
Tom Sepez <tsepez@chromium.org>
2023-05-11 16:36:45 +0000

    Make BRP actually compatible with PDFium tests.
    Introduce our own shim while we wait for the generic as supplied
    by ParitionAlloc to become disentangled from base and universally
    available.
    
    Change-Id: I3ff59f0cd46b9bf5e01e165a70cf995d96830fa5

--- core/fxcrt/unowned_ptr_unittest.cpp
+++ core/fxcrt/unowned_ptr_unittest.cpp
@@ -162,7 +162,7 @@ TEST(UnownedPtr, PtrOk) {
 }
 
 TEST(UnownedPtr, PtrNotOk) {
-#if defined(ADDRESS_SANITIZER)
+#if defined(ADDRESS_SANITIZER) || BUILDFLAG(ENABLE_DANGLING_RAW_PTR_CHECKS)
   EXPECT_DEATH(DeleteDangling(), "");
 #else
   DeleteDangling();
@@ -179,7 +179,7 @@ TEST(UnownedPtr, AssignOk) {
 }
 
 TEST(UnownedPtr, AssignNotOk) {
-#if defined(ADDRESS_SANITIZER)
+#if defined(ADDRESS_SANITIZER) || BUILDFLAG(ENABLE_DANGLING_RAW_PTR_CHECKS)
   EXPECT_DEATH(AssignDangling(), "");
 #else
   AssignDangling();
@@ -196,7 +196,7 @@ TEST(UnownedPtr, ReleaseOk) {
 }
 
 TEST(UnownedPtr, ReleaseNotOk) {
-#if defined(ADDRESS_SANITIZER)
+#if defined(ADDRESS_SANITIZER) || BUILDFLAG(ENABLE_DANGLING_RAW_PTR_CHECKS)
   EXPECT_DEATH(ReleaseDangling(), "");
 #else
   ReleaseDangling();
