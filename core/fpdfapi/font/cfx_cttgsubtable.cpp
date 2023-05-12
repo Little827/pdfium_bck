4770ddbc50b25a75926386e581c48ca0a742ca9b
Lei Zhang <thestig@chromium.org>
2023-05-12 16:10:23 -0700

    Fix nits in CFX_CTTGSUBTable
    - Rename TRangeRecord to RangeRecord.
    - Rename RangeRecord fields to follow the style guide.
    - Fix lint errors.
    
    Change-Id: I369f0515299139125c743f3818524cc6e127178e

--- core/fpdfapi/font/cfx_cttgsubtable.cpp
+++ core/fpdfapi/font/cfx_cttgsubtable.cpp
@@ -113,7 +113,7 @@ absl::optional<uint32_t> CFX_CTTGSUBTable::GetVerticalGlyphSub2(
   return absl::nullopt;
 }
 
-int CFX_CTTGSUBTable::GetCoverageIndex(CoverageFormat* coverage,
+int CFX_CTTGSUBTable::GetCoverageIndex(const CoverageFormat* coverage,
                                        uint32_t g) const {
   if (!coverage) {
     return -1;
@@ -131,11 +131,11 @@ int CFX_CTTGSUBTable::GetCoverageIndex(CoverageFormat* coverage,
     return -1;
   }
 
-  const auto& range_records = absl::get<std::vector<TRangeRecord>>(*coverage);
-  for (const auto& rangeRec : range_records) {
-    uint32_t s = rangeRec.Start;
-    uint32_t e = rangeRec.End;
-    uint32_t si = rangeRec.StartCoverageIndex;
+  const auto& range_records = absl::get<std::vector<RangeRecord>>(*coverage);
+  for (const auto& range_rec : range_records) {
+    uint32_t s = range_rec.start;
+    uint32_t e = range_rec.end;
+    uint32_t si = range_rec.start_coverage_index;
     if (s <= g && g <= e) {
       return si + g - s;
     }
@@ -152,7 +152,7 @@ uint8_t CFX_CTTGSUBTable::GetUInt8(FT_Bytes& p) const {
 int16_t CFX_CTTGSUBTable::GetInt16(FT_Bytes& p) const {
   uint16_t ret = FXSYS_UINT16_GET_MSBFIRST(p);
   p += 2;
-  return *(int16_t*)&ret;
+  return *reinterpret_cast<int16_t*>(&ret);
 }
 
 uint16_t CFX_CTTGSUBTable::GetUInt16(FT_Bytes& p) const {
@@ -164,7 +164,7 @@ uint16_t CFX_CTTGSUBTable::GetUInt16(FT_Bytes& p) const {
 int32_t CFX_CTTGSUBTable::GetInt32(FT_Bytes& p) const {
   uint32_t ret = FXSYS_UINT32_GET_MSBFIRST(p);
   p += 4;
-  return *(int32_t*)&ret;
+  return *reinterpret_cast<int32_t*>(&ret);
 }
 
 uint32_t CFX_CTTGSUBTable::GetUInt32(FT_Bytes& p) const {
@@ -275,11 +275,11 @@ CFX_CTTGSUBTable::ParseCoverage(FT_Bytes raw) {
     return std::make_unique<CoverageFormat>(std::move(glyph_array));
   }
 
-  std::vector<TRangeRecord> range_records(GetUInt16(sp));
+  std::vector<RangeRecord> range_records(GetUInt16(sp));
   for (auto& range_rec : range_records) {
-    range_rec.Start = GetUInt16(sp);
-    range_rec.End = GetUInt16(sp);
-    range_rec.StartCoverageIndex = GetUInt16(sp);
+    range_rec.start = GetUInt16(sp);
+    range_rec.end = GetUInt16(sp);
+    range_rec.start_coverage_index = GetUInt16(sp);
   }
   return std::make_unique<CoverageFormat>(std::move(range_records));
 }
@@ -311,7 +311,7 @@ CFX_CTTGSUBTable::FeatureRecord::FeatureRecord() = default;
 
 CFX_CTTGSUBTable::FeatureRecord::~FeatureRecord() = default;
 
-CFX_CTTGSUBTable::TRangeRecord::TRangeRecord() = default;
+CFX_CTTGSUBTable::RangeRecord::RangeRecord() = default;
 
 CFX_CTTGSUBTable::SubTable::SubTable() = default;
 
--- core/fpdfapi/font/cfx_cttgsubtable.h
+++ core/fpdfapi/font/cfx_cttgsubtable.h
@@ -37,18 +37,18 @@ class CFX_CTTGSUBTable {
     DataVector<uint16_t> lookup_list_indices;
   };
 
-  struct TRangeRecord {
-    TRangeRecord();
+  struct RangeRecord {
+    RangeRecord();
 
-    uint16_t Start = 0;
-    uint16_t End = 0;
-    uint16_t StartCoverageIndex = 0;
+    uint16_t start = 0;
+    uint16_t end = 0;
+    uint16_t start_coverage_index = 0;
   };
 
   // GlyphArray for format 1.
   // RangeRecords for format 2.
   using CoverageFormat =
-      absl::variant<DataVector<uint16_t>, std::vector<TRangeRecord>>;
+      absl::variant<DataVector<uint16_t>, std::vector<RangeRecord>>;
 
   struct SubTable {
     SubTable();
@@ -90,7 +90,7 @@ class CFX_CTTGSUBTable {
                                                uint32_t glyphnum) const;
   absl::optional<uint32_t> GetVerticalGlyphSub2(const Lookup& lookup,
                                                 uint32_t glyphnum) const;
-  int GetCoverageIndex(CoverageFormat* coverage, uint32_t g) const;
+  int GetCoverageIndex(const CoverageFormat* coverage, uint32_t g) const;
 
   uint8_t GetUInt8(FT_Bytes& p) const;
   int16_t GetInt16(FT_Bytes& p) const;
