f051437380e335933a19f81534e493d1de5c10c9
K. Moon <kmoon@chromium.org>
2023-07-13 11:05:12 -0700

    Write CAS archive paths for test_runner.py tests
    Adds a GN action that writes out the runtime dependencies for
    test_runner.py tests (javascript, pixel, and corpus) in a format
    compatible with the "cas archive" tool. This will be used to support
    swarming.
    
    Bug: pdfium:1933
    Change-Id: Id44a8ba3d5d607a88e304469eb79de2e0d683891

---
+++ testing/tools/generate_cas_paths.py
@@ -0,0 +1,98 @@
+# Copyright 2023 The PDFium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Tool for converting GN runtime_deps to CAS archive paths."""
+
+import argparse
+from collections import deque
+import filecmp
+import json
+from pathlib import Path
+import os
+
+EXCLUDE_DIRS = {
+    '.git',
+    '__pycache__',
+}
+
+
+def parse_runtime_deps(runtime_deps):
+  """Parses GN's `runtime_deps` format."""
+  with runtime_deps:
+    return [line.rstrip() for line in runtime_deps]
+
+
+def resolve_paths(root, initial_paths):
+  """Converts paths to CAS archive paths format."""
+  absolute_root = os.path.abspath(root)
+
+  resolved_paths = []
+  unvisited_paths = deque(map(Path, initial_paths))
+  while unvisited_paths:
+    path = unvisited_paths.popleft()
+
+    if path.is_dir():
+      # Expand specific children if any are excluded.
+      child_paths = expand_dir(path)
+      if child_paths:
+        unvisited_paths += child_paths
+        continue
+
+    resolved_paths.append(os.path.relpath(path, start=absolute_root))
+
+  return [[absolute_root, path] for path in resolved_paths]
+
+
+def expand_dir(path):
+  """Explicitly expands directory if any children are excluded."""
+  expand = False
+  expanded_paths = []
+
+  for child_path in path.iterdir():
+    if child_path.name in EXCLUDE_DIRS and path.is_dir():
+      expand = True
+      continue
+    expanded_paths.append(child_path)
+
+  return expanded_paths if expand else []
+
+
+def replace_output(resolved, output_path):
+  """Atomically replaces the output with the resolved JSON if changed."""
+  temporary_path = output_path + '.tmp'
+  try:
+    with open(temporary_path, 'w', encoding='ascii') as temporary:
+      json.dump(resolved, temporary)
+
+    if (os.path.exists(output_path) and
+        filecmp.cmp(temporary_path, output_path, shallow=False)):
+      return
+
+    os.replace(temporary_path, output_path)
+    temporary_path = None
+  finally:
+    if temporary_path:
+      os.remove(temporary_path)
+
+
+def main():
+  parser = argparse.ArgumentParser(description=__doc__)
+  parser.add_argument('--root')
+  parser.add_argument(
+      'runtime_deps',
+      help='runtime_deps written by GN',
+      type=argparse.FileType('r', encoding='UTF-8'),
+      metavar='input.runtime_deps')
+  parser.add_argument(
+      'output_json',
+      help='CAS archive paths in JSON format',
+      metavar='output.json')
+  args = parser.parse_args()
+
+  runtime_deps = parse_runtime_deps(args.runtime_deps)
+  resolved_paths = resolve_paths(args.root, runtime_deps)
+  replace_output(resolved_paths, args.output_json)
+
+
+if __name__ == '__main__':
+  main()
