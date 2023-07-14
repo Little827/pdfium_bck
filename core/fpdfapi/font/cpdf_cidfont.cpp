c425533efc5cd128f5819f24b58d80f0068aa5fe
Tom Sepez <tsepez@chromium.org>
2023-07-14 00:02:18 +0000

    Avoid adding std::size() to compute end pointers.
    std::end() is cleaner and avoids ptr arithmetic getting flagged.
    
    Change-Id: Ife2c54b973aecbb9a0698a01d5e2cd5d27d6527e

--- core/fpdfapi/font/cpdf_cidfont.cpp
+++ core/fpdfapi/font/cpdf_cidfont.cpp
@@ -888,7 +888,8 @@ const uint8_t* CPDF_CIDFont::GetCIDTransform(uint16_t cid) const {
   if (m_Charset != CIDSET_JAPAN1 || m_pFontFile)
     return nullptr;
 
-  const auto* pEnd = kJapan1VerticalCIDs + std::size(kJapan1VerticalCIDs);
+  const auto* pBegin = std::begin(kJapan1VerticalCIDs);
+  const auto* pEnd = std::end(kJapan1VerticalCIDs);
   const auto* pTransform = std::lower_bound(
       kJapan1VerticalCIDs, pEnd, cid,
       [](const CIDTransform& entry, uint16_t cid) { return entry.cid < cid; });
