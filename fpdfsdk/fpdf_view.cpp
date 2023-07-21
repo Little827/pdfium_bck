88ef8dfe88a2c1c48265d6fc78387d4ae7119fbc
Tom Sepez <tsepez@chromium.org>
2023-07-21 19:44:47 +0000

    DO NOT CI - check for PA on pdfium startup
    Change-Id: I0caab34609a653ec316757a7b2acea333ce8a213

--- fpdfsdk/fpdf_view.cpp
+++ fpdfsdk/fpdf_view.cpp
@@ -221,6 +221,10 @@ FPDF_InitLibraryWithConfig(const FPDF_LIBRARY_CONFIG* config) {
   if (g_bLibraryInitialized)
     return;
 
+  void* obj = malloc(16);
+  CHECK(partition_alloc::IsManagedByPartitionAllocBRPPool(
+      reinterpret_cast<uintptr_t>(obj)));
+
   FX_InitializeMemoryAllocators();
   CFX_GEModule::Create(config ? config->m_pUserFontPaths : nullptr);
   CPDF_PageModule::Create();
