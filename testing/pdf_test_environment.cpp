a76f35330ee193d90ecfc8869189aeb27332e728
Tom Sepez <tsepez@chromium.org>
2023-07-25 19:57:17 +0000

    Switch to officially supported PA-E malloc shim.
    The tests currently indicate PA support, but not BRP support. BRP
    support will require an explicit step to switch to a BRP pool after
    startup.
    
    Change-Id: I67b4695b8e6cb1b820a18c58640b177436124739

--- testing/pdf_test_environment.cpp
+++ testing/pdf_test_environment.cpp
@@ -4,9 +4,26 @@
 
 #include "testing/pdf_test_environment.h"
 
+#include "build/build_config.h"
 #include "core/fxge/cfx_gemodule.h"
 
-PDFTestEnvironment::PDFTestEnvironment() = default;
+#if defined(PDF_USE_PARTITION_ALLOC)
+#include "base/allocator/partition_allocator/partition_alloc_buildflags.h"
+#include "base/allocator/partition_allocator/shim/allocator_shim.h"
+#endif
+
+PDFTestEnvironment::PDFTestEnvironment() {
+#if defined(PDF_USE_PARTITION_ALLOC)
+#if BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
+  allocator_shim::ConfigurePartitions(
+      allocator_shim::EnableBrp(true),
+      allocator_shim::EnableMemoryTagging(false),
+      allocator_shim::SplitMainPartition(true),
+      allocator_shim::UseDedicatedAlignedPartition(true), 0,
+      allocator_shim::AlternateBucketDistribution::kDefault);
+#endif  // BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
+#endif  // defined(PDF_USE_PARTITION_ALLOC)
+}
 
 PDFTestEnvironment::~PDFTestEnvironment() = default;
 
