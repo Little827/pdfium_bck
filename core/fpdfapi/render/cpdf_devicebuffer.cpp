e1906c8559b97bfd43167f8e96af5675e225cbb5
Lei Zhang <thestig@chromium.org>
2023-10-10 16:44:53 +0000

    M118: Avoid rendering into the device in CPDF_RenderContext::GetBackground()
    Change GetBackground() to render into a bitmap again, and avoid
    rendering directly into the CFX_RenderDevice. This is a partial revert
    of https://pdfium-review.googlesource.com//111431 to fix a performance
    regression when the CFX_RenderDevice is backed by a printer on Windows.
    
    Bug: chromium:1489537
    Change-Id: I01014078a5a438f54a1e21652005601f2fd9b3d8
    Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/112371
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Nigi <nigi@chromium.org>
    (cherry picked from commit e3593d6470b21372772459968aec121dcc6d885d)

--- core/fpdfapi/render/cpdf_devicebuffer.cpp
+++ core/fpdfapi/render/cpdf_devicebuffer.cpp
@@ -82,9 +82,17 @@ void CPDF_DeviceBuffer::OutputToDevice() {
     return;
   }
   auto pBuffer = pdfium::MakeRetain<CFX_DIBitmap>();
+<<<<<<< HEAD   (d74703 Roll Catapult from 220cbb13b5a7 to f16ca3c78e46 (106 revisio)
   m_pDevice->CreateCompatibleBitmap(pBuffer, m_pBitmap->GetWidth(),
                                     m_pBitmap->GetHeight());
   m_pContext->GetBackground(m_pDevice, m_pObject, nullptr, m_Matrix);
+=======
+  if (!m_pDevice->CreateCompatibleBitmap(pBuffer, m_pBitmap->GetWidth(),
+                                         m_pBitmap->GetHeight())) {
+    return;
+  }
+  m_pContext->GetBackground(pBuffer, m_pObject, nullptr, m_Matrix);
+>>>>>>> CHANGE (e3593d Avoid rendering into the device in CPDF_RenderContext::GetBa)
   pBuffer->CompositeBitmap(0, 0, pBuffer->GetWidth(), pBuffer->GetHeight(),
                            m_pBitmap, 0, 0, BlendMode::kNormal, nullptr, false);
   m_pDevice->StretchDIBits(pBuffer, m_Rect.left, m_Rect.top, m_Rect.Width(),
