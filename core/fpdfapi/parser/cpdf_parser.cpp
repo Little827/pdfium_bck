5195d68462876e70643b540176761b5fe9cca2e8
Lei Zhang <thestig@chromium.org>
2023-10-20 14:42:59 -0700

    Merge duplicate CPDF_CrossRefTable creation code
    In CPDF_Parser::LoadCrossRefV5(), two code paths have the same code for
    creating CPDF_CrossRefTable. Merge them into one ctor call and then
    std::move() it as needed.
    
    Change-Id: I1505f0120db8de5660424ab2d29bb56ffbbb5c4e

--- core/fpdfapi/parser/cpdf_parser.cpp
+++ core/fpdfapi/parser/cpdf_parser.cpp
@@ -769,15 +769,14 @@ bool CPDF_Parser::LoadCrossRefV5(FX_FILESIZE* pos, bool bMainXRef) {
   *pos = prev;
 
   RetainPtr<CPDF_Dictionary> pNewTrailer = ToDictionary(pDict->Clone());
+  auto new_cross_ref_table = std::make_unique<CPDF_CrossRefTable>(
+      std::move(pNewTrailer), pStream->GetObjNum());
   if (bMainXRef) {
-    m_CrossRefTable = std::make_unique<CPDF_CrossRefTable>(
-        std::move(pNewTrailer), pStream->GetObjNum());
+    m_CrossRefTable = std::move(new_cross_ref_table);
     m_CrossRefTable->SetObjectMapSize(size);
   } else {
     m_CrossRefTable = CPDF_CrossRefTable::MergeUp(
-        std::make_unique<CPDF_CrossRefTable>(std::move(pNewTrailer),
-                                             pStream->GetObjNum()),
-        std::move(m_CrossRefTable));
+        std::move(new_cross_ref_table), std::move(m_CrossRefTable));
   }
 
   std::vector<CrossRefV5IndexEntry> indices =
