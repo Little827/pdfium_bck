07dc0e592075d86e650252f4352e9a97e60ab43d
Tom Sepez <tsepez@chromium.org>
2023-12-22 17:40:48 +0000

    Enhance FixedSizeDataVector<>.
    Bring in some improvements I've been contemplating from work on the
    chromium side.
    
    -- initialize size_ via std::exchange().
    -- remove writable_span(), overload span() method instead.
    -- introduce subspan(), first(), and last() methods.
    -- adjust callers appropriately.
    
    Change-Id: Icc451b0ddd558dab417e9c8c5636e7028ef506aa

--- core/fxcrt/fixed_uninit_data_vector_unittest.cpp
+++ core/fxcrt/fixed_uninit_data_vector_unittest.cpp
@@ -18,7 +18,7 @@ TEST(FixedUninitDataVector, NoData) {
   EXPECT_EQ(0u, vec.size());
   EXPECT_TRUE(vec.empty());
   EXPECT_TRUE(vec.span().empty());
-  EXPECT_TRUE(vec.writable_span().empty());
+  EXPECT_TRUE(vec.span().empty());
 }
 
 TEST(FixedUninitDataVector, WithData) {
@@ -26,45 +26,45 @@ TEST(FixedUninitDataVector, WithData) {
   EXPECT_FALSE(vec.empty());
   EXPECT_EQ(4u, vec.size());
   EXPECT_EQ(4u, vec.span().size());
-  EXPECT_EQ(4u, vec.writable_span().size());
+  EXPECT_EQ(4u, vec.span().size());
 
   constexpr int kData[] = {1, 2, 3, 4};
-  fxcrt::spancpy(vec.writable_span(), pdfium::make_span(kData));
+  fxcrt::spancpy(vec.span(), pdfium::make_span(kData));
   EXPECT_THAT(vec.span(), testing::ElementsAre(1, 2, 3, 4));
 }
 
 TEST(FixedUninitDataVector, Move) {
   FixedUninitDataVector<int> vec(4);
   constexpr int kData[] = {1, 2, 3, 4};
-  ASSERT_EQ(4u, vec.writable_span().size());
-  fxcrt::spancpy(vec.writable_span(), pdfium::make_span(kData));
+  ASSERT_EQ(4u, vec.span().size());
+  fxcrt::spancpy(vec.span(), pdfium::make_span(kData));
   const int* const original_data_ptr = vec.span().data();
 
   FixedUninitDataVector<int> vec2(std::move(vec));
   EXPECT_FALSE(vec2.empty());
   EXPECT_EQ(4u, vec2.size());
   EXPECT_EQ(4u, vec2.span().size());
-  EXPECT_EQ(4u, vec2.writable_span().size());
+  EXPECT_EQ(4u, vec2.span().size());
   EXPECT_THAT(vec2.span(), testing::ElementsAre(1, 2, 3, 4));
   EXPECT_EQ(vec2.span().data(), original_data_ptr);
 
   EXPECT_EQ(0u, vec.size());
   EXPECT_TRUE(vec.empty());
   EXPECT_TRUE(vec.span().empty());
-  EXPECT_TRUE(vec.writable_span().empty());
+  EXPECT_TRUE(vec.span().empty());
 
   vec = std::move(vec2);
   EXPECT_FALSE(vec.empty());
   EXPECT_EQ(4u, vec.size());
   EXPECT_EQ(4u, vec.span().size());
-  EXPECT_EQ(4u, vec.writable_span().size());
+  EXPECT_EQ(4u, vec.span().size());
   EXPECT_THAT(vec.span(), testing::ElementsAre(1, 2, 3, 4));
   EXPECT_EQ(vec.span().data(), original_data_ptr);
 
   EXPECT_EQ(0u, vec2.size());
   EXPECT_TRUE(vec2.empty());
   EXPECT_TRUE(vec2.span().empty());
-  EXPECT_TRUE(vec2.writable_span().empty());
+  EXPECT_TRUE(vec2.span().empty());
 }
 
 TEST(FixedUninitDataVector, AssignFromFixedZeroedDataVector) {
@@ -72,8 +72,8 @@ TEST(FixedUninitDataVector, AssignFromFixedZeroedDataVector) {
 
   FixedZeroedDataVector<int> vec2(4);
   constexpr int kData[] = {1, 2, 3, 4};
-  ASSERT_EQ(4u, vec2.writable_span().size());
-  fxcrt::spancpy(vec2.writable_span(), pdfium::make_span(kData));
+  ASSERT_EQ(4u, vec2.span().size());
+  fxcrt::spancpy(vec2.span(), pdfium::make_span(kData));
 
   vec = std::move(vec2);
   EXPECT_TRUE(vec2.empty());
@@ -86,11 +86,61 @@ TEST(FixedUninitDataVector, AssignFromFixedTryAllocZeroedDataVector) {
 
   FixedTryAllocZeroedDataVector<int> vec2(4);
   constexpr int kData[] = {1, 2, 3, 4};
-  ASSERT_EQ(4u, vec2.writable_span().size());
-  fxcrt::spancpy(vec2.writable_span(), pdfium::make_span(kData));
+  ASSERT_EQ(4u, vec2.span().size());
+  fxcrt::spancpy(vec2.span(), pdfium::make_span(kData));
 
   vec = std::move(vec2);
   EXPECT_TRUE(vec2.empty());
   EXPECT_EQ(4u, vec.span().size());
   EXPECT_THAT(vec.span(), testing::ElementsAre(1, 2, 3, 4));
+}
+
+TEST(FixedUninitDataVector, Subspan) {
+  FixedUninitDataVector<uint32_t> vec(4);
+  for (size_t i = 0; i < vec.size(); ++i) {
+    vec.span()[i] = i;
+  }
+  pdfium::span<uint32_t> empty = vec.subspan(2, 0);
+  EXPECT_TRUE(empty.empty());
+
+  pdfium::span<uint32_t> first = vec.subspan(0, 1);
+  EXPECT_EQ(first.size(), 1u);
+  EXPECT_EQ(first[0], 0u);
+
+  pdfium::span<uint32_t> mids = vec.subspan(1, 2);
+  EXPECT_EQ(mids.size(), 2u);
+  EXPECT_EQ(mids[0], 1u);
+  EXPECT_EQ(mids[1], 2u);
+
+  pdfium::span<uint32_t> rest = vec.subspan(3);
+  EXPECT_EQ(rest.size(), 1u);
+  EXPECT_EQ(rest[0], 3u);
+}
+
+TEST(FixedUninitDataVector, First) {
+  FixedUninitDataVector<uint32_t> vec(4);
+  for (size_t i = 0; i < vec.size(); ++i) {
+    vec.span()[i] = i;
+  }
+  pdfium::span<uint32_t> empty = vec.first(0);
+  EXPECT_TRUE(empty.empty());
+
+  pdfium::span<uint32_t> some = vec.first(2);
+  EXPECT_EQ(some.size(), 2u);
+  EXPECT_EQ(some[0], 0u);
+  EXPECT_EQ(some[1], 1u);
+}
+
+TEST(FixedUninitDataVector, Last) {
+  FixedUninitDataVector<uint32_t> vec(4);
+  for (size_t i = 0; i < vec.size(); ++i) {
+    vec.span()[i] = i;
+  }
+  pdfium::span<uint32_t> empty = vec.first(0);
+  EXPECT_TRUE(empty.empty());
+
+  pdfium::span<uint32_t> some = vec.first(2);
+  EXPECT_EQ(some.size(), 2u);
+  EXPECT_EQ(some[0], 0u);
+  EXPECT_EQ(some[1], 1u);
 }
