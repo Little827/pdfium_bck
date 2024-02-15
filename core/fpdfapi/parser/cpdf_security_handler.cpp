587825b4038104f1a503a9e0b93603349b275f97
Lei Zhang <thestig@chromium.org>
2024-02-14 17:49:11 -0800

    Get rid of FX_GET_32WORD macro.
    Replace its usage with a call to FXSYS_GetDwordMsbFirst().
    
    Change-Id: I73e1b546d86b840103ab0e0a41d10270922f80cc

--- core/fpdfapi/parser/cpdf_security_handler.cpp
+++ core/fpdfapi/parser/cpdf_security_handler.cpp
@@ -91,22 +91,15 @@ bool IsValidKeyLengthForCipher(CPDF_CryptoHandler::Cipher cipher,
   }
 }
 
-#define FX_GET_32WORD(n, b, i)                                        \
-  {                                                                   \
-    (n) = (uint32_t)(                                                 \
-        ((uint64_t)(b)[(i)] << 24) | ((uint64_t)(b)[(i) + 1] << 16) | \
-        ((uint64_t)(b)[(i) + 2] << 8) | ((uint64_t)(b)[(i) + 3]));    \
-  }
-int BigOrder64BitsMod3(uint8_t* data) {
+int BigOrder64BitsMod3(pdfium::span<const uint8_t> data) {
   uint64_t ret = 0;
   for (int i = 0; i < 4; ++i) {
-    uint32_t value;
-    FX_GET_32WORD(value, data, 4 * i);
     ret <<= 32;
-    ret |= value;
+    ret |= fxcrt::GetUInt32MSBFirst(data);
     ret %= 3;
+    data = data.subspan(4);
   }
-  return (int)ret;
+  return static_cast<int>(ret);
 }
 
 void Revision6_Hash(const ByteString& password,
@@ -155,7 +148,7 @@ void Revision6_Hash(const ByteString& password,
     CRYPT_AESEncrypt(&aes, encrypted_output_span.data(), content.data(),
                      iBufLen);
     int iHash = 0;
-    switch (BigOrder64BitsMod3(encrypted_output_span.data())) {
+    switch (BigOrder64BitsMod3(encrypted_output_span)) {
       case 0:
         iHash = 0;
         iBlockSize = 32;
