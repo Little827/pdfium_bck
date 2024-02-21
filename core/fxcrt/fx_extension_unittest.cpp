37475ac0a461751efaeb0fc7239b55fdda07faf1
Tom Sepez <tsepez@chromium.org>
2024-02-21 22:59:40 +0000

    Introduce FXSYS_CStringAdvance().
    Provide a big hammer for intra-string pointer arithmetic.
    
    Change-Id: I17e80557b1ce64ae76cd6041e4781031f502a764

--- core/fxcrt/fx_extension_unittest.cpp
+++ core/fxcrt/fx_extension_unittest.cpp
@@ -244,3 +244,25 @@ TEST(fxcrt, FXSYS_SafeOps) {
     }
   }
 }
+
+TEST(fxcrt, CStringAdvance) {
+  const char* empty = "";
+  EXPECT_EQ(nullptr, FXSYS_CStringAdvance(empty));
+
+  const char* fred = "fred";
+  EXPECT_EQ('f', *fred);
+  fred = FXSYS_CStringAdvance(fred);
+  ASSERT_NE(nullptr, fred);
+  EXPECT_EQ('r', *fred);
+  fred = FXSYS_CStringAdvance(fred);
+  ASSERT_NE(nullptr, fred);
+  EXPECT_EQ('e', *fred);
+  fred = FXSYS_CStringAdvance(fred);
+  ASSERT_NE(nullptr, fred);
+  EXPECT_EQ('d', *fred);
+  fred = FXSYS_CStringAdvance(fred);
+  ASSERT_NE(nullptr, fred);
+  EXPECT_EQ('\0', *fred);
+  fred = FXSYS_CStringAdvance(fred);
+  EXPECT_EQ(nullptr, fred);
+}
