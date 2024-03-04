1f4eecd3176479ab65a02ff1cdca6aa64b64b4fa
Tom Sepez <tsepez@chromium.org>
2024-03-04 21:38:08 +0000

    Downgrade raw_span<> to span<> in StringViewTemplate.
    Trade some small amount of temporal safety for performance gains,
    say, when making repeated Substr() calls in a loop.
    
    Change-Id: I637cd8c5f5eb13ecd272369d8988edb851293915

--- core/fxcrt/string_view_template.h
+++ core/fxcrt/string_view_template.h
@@ -16,7 +16,6 @@
 
 #include "core/fxcrt/fx_memcpy_wrappers.h"
 #include "core/fxcrt/fx_system.h"
-#include "core/fxcrt/raw_span.h"
 #include "core/fxcrt/span.h"
 
 namespace fxcrt {
@@ -267,7 +266,7 @@ class StringViewTemplate {
   }
 
  protected:
-  pdfium::raw_span<const UnsignedType> m_Span;
+  pdfium::span<const UnsignedType> m_Span;
 
  private:
   void* operator new(size_t) throw() { return nullptr; }
