558c0369a0890a7a8d16a5a9c4fa3cd2e1538ae0
Wang Chuan <jdyaomo@gmail.com>
2024-05-05 15:58:22 +0800

    Correctly handle image transparency in path objects
    When displaying an embedded image in a path object, the image's alpha
    value needs to be multiplied with the alpha value defined in the path
    object to ensure transparency is rendered correctly.
    
    Bug: pdfium:2106
    Change-Id: Ibaa8786b8a4e522c258590aed26a6779712d921d

--- core/fpdfapi/render/cpdf_renderstatus.cpp
+++ core/fpdfapi/render/cpdf_renderstatus.cpp
@@ -373,7 +373,34 @@ bool CPDF_RenderStatus::ProcessForm(const CPDF_FormObject* pFormObj,
     status.RenderObjectList(pFormObj->form(), matrix);
     m_bStopped = status.m_bStopped;
   }
+
+  uint8_t alpha = static_cast<uint8_t>(
+      m_InitialStates.general_state().GetFillAlpha() * 255);
+  if (alpha < 255) {
+    auto rect = pFormObj->GetTransformedBBox(mtObj2Device);
+    rect.Intersect(m_pDevice->GetClipBox());
+    ApplyAlphaToRenderDevice(rect, alpha);
+  }
+
   return true;
+}
+
+void CPDF_RenderStatus::ApplyAlphaToRenderDevice(const FX_RECT& rect,
+                                                 uint8_t alpha) {
+  if (rect.IsEmpty()) {
+    return;
+  }
+  auto mask = pdfium::MakeRetain<CFX_DIBitmap>();
+  if (!mask->Create(m_pDevice->GetWidth(), m_pDevice->GetHeight(),
+                    FXDIB_Format::k8bppMask)) {
+    return;
+  }
+  mask->Clear(0xff000000);
+  for (int row = rect.top; row < rect.bottom; row++) {
+    uint8_t* dest = mask->GetWritableScanline(row).data();
+    memset(dest + rect.left, alpha, rect.Width());
+  }
+  m_pDevice->MultiplyAlphaMask(mask);
 }
 
 bool CPDF_RenderStatus::ProcessPath(CPDF_PathObject* path_obj,
