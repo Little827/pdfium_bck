0ac6c470d1ad06de4ca282dba7f25595c3de0f3b
Tom Sepez <tsepez@chromium.org>
2024-05-07 18:43:28 +0000

    Add fxcrt::Fill
    This is replacement for C++20 std::ranges:fill(), which itself is a
    replacement for memset() in many cases.
    
    Avoid future unsafe_buffers annotations.
    
    Change-Id: I37891a721d61364a7af523512ae45b076a78d26a

--- core/fxcodec/gif/cfx_gifcontext_unittest.cpp
+++ core/fxcodec/gif/cfx_gifcontext_unittest.cpp
@@ -16,6 +16,7 @@
 #include "core/fxcodec/cfx_codec_memory.h"
 #include "core/fxcrt/data_vector.h"
 #include "core/fxcrt/span_util.h"
+#include "core/fxcrt/stl_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace fxcodec {
