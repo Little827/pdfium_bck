27534ac94b41f20f19ec06bf3e8d9cb9e3bbdca8
Lei Zhang <thestig@chromium.org>
2024-05-20 15:38:33 -0700

    Add a 2-parameter CPDF_String ctor
    Prepare for removing the 3-parameter CPDF_String ctor by adding a
    2-param ctor that covers the majority of the use cases, where the `bHex`
    argument is false.
    
    Change-Id: I6b4f99c87201e71712303b8c9be2f2406597edb1

--- core/fpdfapi/parser/fpdf_parser_decode_unittest.cpp
+++ core/fpdfapi/parser/fpdf_parser_decode_unittest.cpp
@@ -100,7 +100,7 @@ TEST(ParserDecodeTest, ValidateDecoderPipeline) {
   {
     // Invalid 1 decoder pipeline due to wrong type.
     auto decoders = pdfium::MakeRetain<CPDF_Array>();
-    decoders->AppendNew<CPDF_String>("FlateEncode", false);
+    decoders->AppendNew<CPDF_String>("FlateEncode");
     EXPECT_FALSE(ValidateDecoderPipeline(decoders.Get()));
   }
   {
@@ -120,7 +120,7 @@ TEST(ParserDecodeTest, ValidateDecoderPipeline) {
   {
     // Invalid 2 decoder pipeline due to wrong type.
     auto decoders = pdfium::MakeRetain<CPDF_Array>();
-    decoders->AppendNew<CPDF_String>("AHx", false);
+    decoders->AppendNew<CPDF_String>("AHx");
     decoders->AppendNew<CPDF_Name>("LZWDecode");
     EXPECT_FALSE(ValidateDecoderPipeline(decoders.Get()));
   }
@@ -141,7 +141,7 @@ TEST(ParserDecodeTest, ValidateDecoderPipeline) {
     decoders->AppendNew<CPDF_Name>("A85");
     decoders->AppendNew<CPDF_Name>("RunLengthDecode");
     decoders->AppendNew<CPDF_Name>("FlateDecode");
-    decoders->AppendNew<CPDF_String>("RL", false);
+    decoders->AppendNew<CPDF_String>("RL");
     EXPECT_FALSE(ValidateDecoderPipeline(decoders.Get()));
   }
 }
@@ -178,8 +178,7 @@ TEST(ParserDecodeTest, ValidateDecoderPipelineWithIndirectObjects) {
   {
     // Invalid 2 decoder pipeline due to wrong type indirect object.
     CPDF_IndirectObjectHolder objects_holder;
-    auto decoder =
-        pdfium::MakeRetain<CPDF_String>(nullptr, "FlateDecode", false);
+    auto decoder = pdfium::MakeRetain<CPDF_String>(nullptr, "FlateDecode");
     uint32_t decoder_number =
         objects_holder.AddIndirectObject(std::move(decoder));
 
@@ -214,7 +213,7 @@ TEST(ParserDecodeTest, GetDecoderArray) {
   {
     // Wrong filter type.
     auto dict = pdfium::MakeRetain<CPDF_Dictionary>();
-    dict->SetNewFor<CPDF_String>("Filter", "RL", false);
+    dict->SetNewFor<CPDF_String>("Filter", "RL");
     std::optional<DecoderArray> decoder_array = GetDecoderArray(dict);
     EXPECT_FALSE(decoder_array.has_value());
   }
