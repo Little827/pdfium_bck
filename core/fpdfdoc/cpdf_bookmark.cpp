24613a94d1d94f056608026031ceabac97d5a3a1
Tom Sepez <tsepez@chromium.org>
2024-06-11 18:13:36 +0000

    Avoid UNSAFE_TODO() in CPDF_Bookmark.
    Construct a string as a string rather than an initial vector.
    
    Bug: 42271176
    Change-Id: I9136cf25533a7c2d941de42e9f0885bcce5c0cb0

--- core/fpdfdoc/cpdf_bookmark.cpp
+++ core/fpdfdoc/cpdf_bookmark.cpp
@@ -24,25 +24,20 @@ CPDF_Bookmark::CPDF_Bookmark(RetainPtr<const CPDF_Dictionary> pDict)
 CPDF_Bookmark::~CPDF_Bookmark() = default;
 
 WideString CPDF_Bookmark::GetTitle() const {
-  if (!m_pDict)
+  if (!m_pDict) {
     return WideString();
-
+  }
   RetainPtr<const CPDF_String> pString =
       ToString(m_pDict->GetDirectObjectFor("Title"));
-  if (!pString)
+  if (!pString) {
     return WideString();
-
-  WideString title = pString->GetUnicodeText();
-  size_t len = title.GetLength();
-  if (!len)
-    return WideString();
-
-  DataVector<wchar_t> buf(len);
-  for (size_t i = 0; i < len; i++) {
-    wchar_t w = title[i];
-    buf[i] = w > 0x20 ? w : 0x20;
   }
-  return UNSAFE_TODO(WideString::Create(buf.data(), len));
+  WideString result;
+  WideString title = pString->GetUnicodeText();
+  for (const wchar_t wc : title) {
+    result += wc > 0x20 ? wc : 0x20;
+  }
+  return result;
 }
 
 CPDF_Dest CPDF_Bookmark::GetDest(CPDF_Document* pDocument) const {
