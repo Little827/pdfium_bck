8b8f086d1b149327c9da76e5a0060c920fbe8904
Lei Zhang <thestig@chromium.org>
2024-06-11 15:10:55 -0700

    Rewrite comments in CJPX_Decoder::StartDecode()
    The existing comments are out of date, especially after CL [1]. Rewrite
    the comments to explain what is going on.
    
    [1] https://pdfium-review.googlesource.com/50750
    
    Change-Id: I7681670859af08e599030238ca48c485f65b59fd

--- core/fxcodec/jpx/cjpx_decoder.cpp
+++ core/fxcodec/jpx/cjpx_decoder.cpp
@@ -517,13 +517,14 @@ bool CJPX_Decoder::StartDecode() {
     color_sycc_to_rgb(m_Image);
 
   if (m_Image->icc_profile_buf) {
-    // TODO(palmer): Using |opj_free| here resolves the crash described in
-    // https://crbug.com/737033, but ultimately we need to harmonize the
-    // memory allocation strategy across OpenJPEG and its PDFium callers.
-#if !defined(USE_SYSTEM_LIBOPENJPEG2)
-    opj_free(m_Image->icc_profile_buf);
-#else
+#if defined(USE_SYSTEM_LIBOPENJPEG2)
+    // Since opj_free() is an internal function within OpenJPEG, do not assume
+    // it exists and call free() here.
     free(m_Image->icc_profile_buf);
+#else
+    // Memory is allocated with opj_malloc() inside OpenJPEG, so call opj_free()
+    // to match that. The bundled copy of OpenJPEG is known to have opj_free().
+    opj_free(m_Image->icc_profile_buf);
 #endif
     m_Image->icc_profile_buf = nullptr;
     m_Image->icc_profile_len = 0;
